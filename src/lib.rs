0d3869d478549f413fdd5da2857419fa00b83323
Lee Clagett <code@leeclagett.com>
2015-04-06 14:27:24 -0400

    adding basic account creation/deletion (no network support)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,64 @@
+/*  Copyright 2015 MaidSafe.net limited
+
+    This MaidSafe Software is licensed to you under (1) the MaidSafe.net Commercial License,
+    version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+    licence you accepted on initial access to the Software (the "Licences").
+
+    By contributing code to the MaidSafe Software, or to this project generally, you agree to be
+    bound by the terms of the MaidSafe Contributor Agreement, version 1.0, found in the root
+    directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also
+    available at: http://www.maidsafe.net/licenses
+
+    Unless required by applicable law or agreed to in writing, the MaidSafe Software distributed
+    under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied.
+
+    See the Licences for the specific language governing permissions and limitations relating to
+    use of the MaidSafe Software.                                                                 */
 #![crate_name = "maidsafe_client"]
 #![crate_type = "lib"]
 #![doc(html_logo_url = "http://maidsafe.net/img/Resources/branding/maidsafe_logo.fab2.png",
        html_favicon_url = "http://maidsafe.net/img/favicon.ico",
               html_root_url = "http://dirvine.github.io/dirvine/maidsafe_client/")]
-//! Placeholder
 
+extern crate cbor;
+extern crate crypto;
+extern crate maidsafe_types;
 
-/// Placeholder doc test
-pub fn always_true() -> bool { true }
+pub mod account;
 
-#[test]
-fn it_works() {
- assert_eq!(always_true(), true);
+pub enum CryptoError {
+    SymmetricCryptoError(crypto::symmetriccipher::SymmetricCipherError),
+    BadBuffer
 }
+
+impl From<crypto::symmetriccipher::SymmetricCipherError> for CryptoError {
+    fn from(error : crypto::symmetriccipher::SymmetricCipherError) -> CryptoError {
+        return CryptoError::SymmetricCryptoError(error);
+    }
+}
+
+
+pub enum MaidsafeError {
+    CryptoError(CryptoError),
+    EncodingError(cbor::CborError),
+}
+
+impl From<CryptoError> for MaidsafeError {
+    fn from(error : CryptoError) -> MaidsafeError {
+        return MaidsafeError::CryptoError(error);
+    }
+}
+
+impl From<cbor::CborError> for MaidsafeError {
+    fn from(error : cbor::CborError) -> MaidsafeError {
+        return MaidsafeError::EncodingError(error);
+    }
+}
+
+impl From<crypto::symmetriccipher::SymmetricCipherError> for MaidsafeError {
+    fn from(error : crypto::symmetriccipher::SymmetricCipherError) -> MaidsafeError {
+        return MaidsafeError::CryptoError(CryptoError::SymmetricCryptoError(error));
+    }
+}
+
