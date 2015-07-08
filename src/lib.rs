79dcd6b004e00653c060735035bdcb060dfb9ecf
krishna <krishnaof1988@gmail.com>
2015-07-08 14:48:04 +0530

    NFS API removed

--- src/lib.rs
+++ src/lib.rs
@@ -25,13 +25,13 @@
 //               LINT
 ///////////////////////////////////////////////////
 
-#![forbid(bad_style, warnings)] 
+#![forbid(bad_style, warnings)]
 
 #![deny(deprecated, improper_ctypes, missing_docs, non_shorthand_field_patterns,
 overflowing_literals, plugin_as_library, private_no_mangle_fns, private_no_mangle_statics,
 raw_pointer_derive, stable_features, unconditional_recursion, unknown_lints, unsafe_code,
 unsigned_negation, unused, unused_allocation, unused_attributes, unused_comparisons,
-unused_features, unused_parens, while_true)] 
+unused_features, unused_parens, while_true)]
 
 #![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
 unused_qualifications, variant_size_differences)]
@@ -45,18 +45,12 @@ unused_qualifications, variant_size_differences)]
 
 extern crate cbor;
 extern crate rand;
-extern crate time;
 extern crate crypto;
 extern crate routing;
 extern crate sodiumoxide;
 extern crate rustc_serialize;
-
 extern crate maidsafe_types;
 extern crate lru_time_cache;
-extern crate self_encryption;
-
-/// NFS module
-pub mod nfs;
 /// Self-Auth and Gateway Module
 pub mod client;
 /// Parse incoming data
