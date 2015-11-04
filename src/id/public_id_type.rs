cdce16351491f700fb55b68aaa236e41c7cb0783
krishna <krishnaof1988@gmail.com>
2015-11-04 17:37:32 +0530

    using slice_equal

--- src/id/public_id_type.rs
+++ src/id/public_id_type.rs
@@ -165,7 +165,6 @@ mod test {
         }
 
         let signature = ::sodiumoxide::crypto::sign::Signature(signature_arr);
-        assert_eq!(&signature.0.iter().map(|x| x).collect::<Vec<_>>(),
-                   &public_maid.signature().0.iter().map(|x| x).collect::<Vec<_>>());
+        assert!(::utility::slice_equal(&signature.0, &public_maid.signature().0));
     }
 }
