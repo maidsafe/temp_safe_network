41d362d9877226ddd50a90523949588c2b66e228
krishna <krishnaof1988@gmail.com>
2016-08-05 13:28:48 +0530

    fix/ffi_move_api: fetch the moved or copied directory
    Create the directory moved or copied in the network. Previously the metadat was updated, and when the directory was fetched it returned error.
    Similarly in copy file the copied file must generate a new id, instead of using the same id from the source

--- src/ffi/nfs/move_dir.rs
+++ src/ffi/nfs/move_dir.rs
@@ -38,12 +38,12 @@ impl MoveDirectory {
                      -> Result<DirectoryListing, FfiError> {
         let start_dir_key = if shared {
             try!(params.clone()
-                .safe_drive_dir_key
-                .ok_or(FfiError::from("Safe Drive directory key is not present")))
+                       .safe_drive_dir_key
+                       .ok_or(FfiError::from("Safe Drive directory key is not present")))
         } else {
             try!(params.clone()
-                .app_root_dir_key
-                .ok_or(FfiError::from("Application directory key is not present")))
+                       .app_root_dir_key
+                       .ok_or(FfiError::from("Application directory key is not present")))
         };
 
         let tokens = helper::tokenise_path(path, false);
@@ -61,60 +61,59 @@ impl Action for MoveDirectory {
             return Err(FfiError::PermissionDenied);
         }
         let directory_helper = DirectoryHelper::new(params.client.clone());
-        let mut src_dir =
-            try!(self.get_directory(&params, self.is_src_path_shared, &self.src_path));
-        let mut dest_dir =
-            try!(self.get_directory(&params, self.is_dest_path_shared, &self.dest_path));
+        let mut src_dir = try!(self.get_directory(&params,
+                                                  self.is_src_path_shared,
+                                                  &self.src_path));
+        let mut dest_dir = try!(self.get_directory(&params,
+                                                   self.is_dest_path_shared,
+                                                   &self.dest_path));
         if dest_dir.find_sub_directory(src_dir.get_metadata().get_name()).is_some() {
             return Err(FfiError::from(DirectoryAlreadyExistsWithSameName));
         }
         let org_parent_of_src_dir = try!(src_dir.get_metadata()
-            .get_parent_dir_key()
-            .cloned()
-            .ok_or(FfiError::from("Parent directory not found")));
+                                                .get_parent_dir_key()
+                                                .cloned()
+                                                .ok_or(FfiError::from("Parent directory not \
+                                                                       found")));
         if self.retain_source {
             let name = src_dir.get_metadata().get_name().to_owned();
             let user_metadata = src_dir.get_metadata().get_user_metadata().to_owned();
             let access_level = src_dir.get_metadata().get_access_level().clone();
             let created_time = *src_dir.get_metadata().get_created_time();
             let modified_time = *src_dir.get_metadata().get_modified_time();
-            let mut dir = try!(DirectoryListing::new(name,
-                                                     src_dir.get_metadata()
-                                                         .get_key()
-                                                         .get_type_tag(),
-                                                     user_metadata,
-                                                     src_dir.get_metadata()
-                                                         .get_key()
-                                                         .is_versioned(),
-                                                     access_level,
-                                                     src_dir.get_metadata()
-                                                         .get_parent_dir_key()
-                                                         .cloned()));
+            let (mut dir, _) = try!(directory_helper.create(name,
+                                                            src_dir.get_metadata()
+                                                                   .get_key()
+                                                                   .get_type_tag(),
+                                                            user_metadata,
+                                                            src_dir.get_metadata()
+                                                                   .get_key()
+                                                                   .is_versioned(),
+                                                            access_level,
+                                                            Some(&mut dest_dir)));
             src_dir.get_files().iter().all(|file| {
                 dir.get_mut_files().push(file.clone());
                 true
             });
             src_dir.get_sub_directories()
-                .iter()
-                .all(|sub_dir| {
-                    dir.get_mut_sub_directories().push(sub_dir.clone());
-                    true
-                });
+                   .iter()
+                   .all(|sub_dir| {
+                       dir.get_mut_sub_directories().push(sub_dir.clone());
+                       true
+                   });
             dir.get_mut_metadata().set_created_time(created_time);
             dir.get_mut_metadata().set_modified_time(modified_time);
-            src_dir = dir;
+            let _ = try!(directory_helper.update(&dir));
         } else {
             src_dir.get_mut_metadata()
-                .set_parent_dir_key(Some(dest_dir.get_metadata().get_key().clone()));
-        }
-        dest_dir.upsert_sub_directory(src_dir.get_metadata().clone());
-        let _ = try!(directory_helper.update(&dest_dir));
-        if !self.retain_source {
+                   .set_parent_dir_key(Some(dest_dir.get_metadata().get_key().clone()));
+            dest_dir.upsert_sub_directory(src_dir.get_metadata().clone());
+            let _ = try!(directory_helper.update(&dest_dir));
             let _ = try!(directory_helper.update(&src_dir));
             let mut parent_of_src_dir = try!(directory_helper.get(&org_parent_of_src_dir));
             // TODO (Spandan) - Fetch and issue a DELETE on the removed directory.
             let _dir_meta = try!(parent_of_src_dir.remove_sub_directory(src_dir.get_metadata()
-                .get_name()));
+                                                                               .get_name()));
             let _ = try!(directory_helper.update(&parent_of_src_dir));
         }
         Ok(None)
