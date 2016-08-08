9e4ce3366e33905901cbc41b8b6a698825736e38
ustulation <ustulation@gmail.com>
2016-08-08 11:40:41 +0100

    refactor/log: revert to creating a null file Also new log4rs will remove the previous file so no need to manually delete it.

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -134,9 +134,7 @@ impl Drop for FfiHandle {
 #[no_mangle]
 pub extern "C" fn init_logging() -> int32_t {
     catch_unwind_i32(|| {
-        let _ = fs::remove_file("Client.log");
         ffi_try!(safe_log::init(false).map_err(CoreError::Unexpected));
-
         0
     })
 }
@@ -152,7 +150,7 @@ pub unsafe extern "C" fn output_log_path(c_output_flie_name: *const c_char,
                                          -> *const u8 {
     catch_unwind_ptr(|| {
         let op_file = ffi_ptr_try!(helper::c_char_ptr_to_string(c_output_flie_name), c_result);
-        let fh = ffi_ptr_try!(FileHandler::<()>::open(&op_file, true)
+        let fh = ffi_ptr_try!(FileHandler::<()>::new(&op_file, true)
                                   .map_err(|e| CoreError::Unexpected(format!("{:?}", e))),
                               c_result);
         let op_file_path =
