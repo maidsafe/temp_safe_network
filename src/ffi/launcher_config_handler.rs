80f273a248991c73a5b45c5b9c6beb20760f1406
Nikita Baksalyar <nikita.baksalyar@maidsafe.net>
2016-10-18 02:15:16 +0500

    refactor/ffi: update launcher_config_handler to async

--- src/ffi/launcher_config_handler.rs
+++ src/ffi/launcher_config_handler.rs
@@ -16,20 +16,19 @@
 // relating to use of the SAFE Network Software.
 
 
-use core::client::Client;
+use core::Client;
+use core::futures::FutureExt;
+use ffi::FfiFuture;
 use ffi::config::{LAUNCHER_GLOBAL_CONFIG_FILE_NAME, LAUNCHER_GLOBAL_DIRECTORY_NAME};
 use ffi::errors::FfiError;
+use futures::Future;
 use maidsafe_utilities::serialisation::{deserialise, serialise};
-use nfs::AccessLevel;
-use nfs::directory_listing::DirectoryListing;
-use nfs::helper::directory_helper::DirectoryHelper;
-use nfs::helper::file_helper::FileHelper;
+use nfs::{AccessLevel, Dir, DirMetadata};
+use nfs::helper::{dir_helper, file_helper};
 use nfs::helper::writer::Mode::Overwrite;
-use nfs::metadata::directory_key::DirectoryKey;
 use routing::XorName;
 use rust_sodium::crypto::{box_, secretbox};
 use rust_sodium::crypto::hash::sha256;
-use std::sync::{Arc, Mutex};
 
 #[derive(RustcEncodable, RustcDecodable, Debug)]
 pub struct LauncherConfiguration {
@@ -39,168 +38,178 @@ pub struct LauncherConfiguration {
 
 #[derive(RustcEncodable, RustcDecodable, Debug, Clone)]
 pub struct AppInfo {
-    pub app_root_dir_key: DirectoryKey,
+    pub app_root_dir: Dir,
     pub asym_keys: (box_::PublicKey, box_::SecretKey),
     pub sym_key: secretbox::Key,
 }
 
-pub struct ConfigHandler {
-    client: Arc<Mutex<Client>>,
-}
+pub fn app_info(client: Client,
+                app_name: String,
+                app_key: String,
+                vendor: String)
+                -> Box<FfiFuture<AppInfo>> {
+    let app_id = app_id(&app_key, &vendor);
+    let c2 = client.clone();
 
-impl ConfigHandler {
-    pub fn new(client: Arc<Mutex<Client>>) -> ConfigHandler {
-        ConfigHandler { client: client }
-    }
-
-    pub fn get_app_info(&self,
-                        app_name: String,
-                        app_key: String,
-                        vendor: String)
-                        -> Result<AppInfo, FfiError> {
-        let app_id = self.get_app_id(&app_key, &vendor);
-
-        let (configs, _) = try!(self.get_launcher_global_config_and_dir());
-        let app_info = match configs.into_iter()
-            .find(|config| config.app_id == app_id)
-            .map(|config| config) {
-            Some(config) => config.app_info,
-            None => {
-                trace!("App's exclusive directory is not mapped inside Launcher's config. This \
-                        must imply it's not present inside user-root-dir also - creating one.");
-
-                let dir_helper = DirectoryHelper::new(self.client.clone());
-                let mut root_dir_listing = try!(dir_helper.get_user_root_directory_listing());
-                let app_dir_name = self.get_app_dir_name(&app_name, &root_dir_listing);
-                let dir_key = *try!(dir_helper.create(app_dir_name,
-                                                      Vec::new(),
-                                                      false,
-                                                      AccessLevel::Private,
-                                                      Some(&mut root_dir_listing)))
-                    .0
-                    .get_key();
-
-                let app_info = AppInfo {
-                    app_root_dir_key: dir_key,
-                    asym_keys: box_::gen_keypair(),
-                    sym_key: secretbox::gen_key(),
-                };
-                let app_config = LauncherConfiguration {
-                    app_id: app_id,
-                    app_info: app_info.clone(),
-                };
-                try!(self.upsert_to_launcher_global_config(app_config));
-
-                app_info
-            }
-        };
-
-        Ok(app_info)
-    }
-
-    fn get_app_id(&self, app_key: &str, vendor: &str) -> XorName {
-        let mut id_str = String::new();
-        id_str.push_str(app_key);
-        id_str.push_str(vendor);
-        XorName(sha256::hash(id_str.as_bytes()).0)
-    }
-
-    fn get_app_dir_name(&self, app_name: &str, directory_listing: &DirectoryListing) -> String {
-        let mut dir_name = format!("{}-Root-Dir", app_name);
-        if directory_listing.find_sub_directory(&dir_name).is_some() {
-            let mut index = 1u8;
-            loop {
-                dir_name = format!("{}-{}-Root-Dir", app_name, index);
-                if directory_listing.find_sub_directory(&dir_name).is_some() {
-                    index += 1;
-                } else {
-                    break;
-                }
-            }
-        }
-
-        dir_name
-    }
-
-    fn upsert_to_launcher_global_config(&self,
-                                        config: LauncherConfiguration)
-                                        -> Result<(), FfiError> {
-        trace!("Update (by overwriting) Launcher's config file by appending a new config.");
-
-        let (mut global_configs, dir_listing) = try!(self.get_launcher_global_config_and_dir());
-
-        // (Spandan)
-        // Unable to use `if let Some() .. else` logic to upsert to a vector due to a language bug.
-        // Once the bug is resolved
-        // - https://github.com/rust-lang/rust/issues/28449
-        // then modify the following to use it.
-        if let Some(pos) = global_configs.iter()
-            .position(|existing_config| existing_config.app_id == config.app_id) {
-            let existing_config = unwrap!(global_configs.get_mut(pos));
-            *existing_config = config;
-        } else {
-            global_configs.push(config);
-        }
-
-        let file = unwrap!(dir_listing.get_files()
-                               .iter()
-                               .find(|file| file.get_name() == LAUNCHER_GLOBAL_CONFIG_FILE_NAME),
-                           "Logic Error - Launcher start-up should ensure the file must be \
-                            present at this stage - Report bug.")
-            .clone();
-
-        let mut file_helper = FileHelper::new(self.client.clone());
-        let mut writer = try!(file_helper.update_content(file, Overwrite, dir_listing));
-        try!(writer.write(&try!(serialise(&global_configs))));
-        let _ = try!(writer.close());
-
-        Ok(())
-    }
-
-    fn get_launcher_global_config_and_dir
-        (&self)
-         -> Result<(Vec<LauncherConfiguration>, DirectoryListing), FfiError> {
-        trace!("Get Launcher's config directory.");
-
-        let dir_helper = DirectoryHelper::new(self.client.clone());
-        let mut dir_listing = try!(dir_helper.get_configuration_directory_listing(
-            LAUNCHER_GLOBAL_DIRECTORY_NAME.to_string()));
-
-        let global_configs = {
-            let mut file_helper = FileHelper::new(self.client.clone());
-            let file = match dir_listing.get_files()
-                .iter()
-                .find(|file| file.get_name() == LAUNCHER_GLOBAL_CONFIG_FILE_NAME)
-                .cloned() {
-                Some(file) => file,
+    launcher_global_config_and_dir(&client)
+        .and_then(move |(configs, _, metadata)| {
+            match configs.into_iter()
+                .find(|config| config.app_id == app_id)
+                .map(|config| config) {
+                Some(config) => ok!(config.app_info),
                 None => {
-                    trace!("Launcher's config file does not exist inside it's config dir - \
+                    trace!("App's exclusive directory is not mapped inside Launcher's config. \
+                            This must imply it's not present inside user-root-dir also - \
                             creating one.");
 
-                    dir_listing =
-                        try!(try!(file_helper.create(LAUNCHER_GLOBAL_CONFIG_FILE_NAME.to_string(),
-                                            Vec::new(),
-                                            dir_listing))
-                                .close())
-                            .0;
-                    unwrap!(dir_listing.get_files()
-                            .iter()
-                            .find(|file| file.get_name() == LAUNCHER_GLOBAL_CONFIG_FILE_NAME)
-                            .cloned())
-                        .clone()
+                    let client = c2.clone();
+                    let c2 = c2.clone();
+
+                    dir_helper::user_root_dir(c2.clone())
+                        .and_then(move |root_dir| {
+                            let app_dir_name = app_dir_name(&app_name, &root_dir);
+
+                            dir_helper::create_sub_dir(client.clone(),
+                                                       app_dir_name,
+                                                       None,
+                                                       Vec::new(),
+                                                       &root_dir,
+                                                       &metadata.id())
+                        })
+                        .map_err(FfiError::from)
+                        .and_then(move |(_, dir, metadata)| {
+                            let app_info = AppInfo {
+                                app_root_dir: dir,
+                                asym_keys: box_::gen_keypair(),
+                                sym_key: secretbox::gen_key(),
+                            };
+
+                            let app_config = LauncherConfiguration {
+                                app_id: app_id,
+                                app_info: app_info.clone(),
+                            };
+
+                            upsert_to_launcher_global_config(&c2, app_config).map(move |_| {
+                                app_info
+                            })
+                        })
+                        .into_box()
+                }
+            }
+        })
+        .into_box()
+}
+
+fn app_id(app_key: &str, vendor: &str) -> XorName {
+    let mut id_str = String::new();
+    id_str.push_str(app_key);
+    id_str.push_str(vendor);
+    XorName(sha256::hash(id_str.as_bytes()).0)
+}
+
+fn app_dir_name(app_name: &str, directory: &Dir) -> String {
+    let mut dir_name = format!("{}-Root-Dir", app_name);
+    if directory.find_sub_dir(&dir_name).is_some() {
+        let mut index = 1u8;
+        loop {
+            dir_name = format!("{}-{}-Root-Dir", app_name, index);
+            if directory.find_sub_dir(&dir_name).is_some() {
+                index += 1;
+            } else {
+                break;
+            }
+        }
+    }
+    dir_name
+}
+
+fn upsert_to_launcher_global_config(client: &Client,
+                                    config: LauncherConfiguration)
+                                    -> Box<FfiFuture<()>> {
+    trace!("Update (by overwriting) Launcher's config file by appending a new config.");
+
+    let client_clone = client.clone();
+
+    launcher_global_config_and_dir(&client)
+        .map_err(FfiError::from)
+        .and_then(move |(mut global_configs, dir, metadata)| {
+            // (Spandan)
+            // Unable to use `if let Some() .. else` logic to upsert to a vector due to
+            // a language bug. Once the bug is resolved
+            // - https://github.com/rust-lang/rust/issues/28449
+            // then modify the following to use it.
+            if let Some(pos) = global_configs.iter()
+                .position(|existing_config| existing_config.app_id == config.app_id) {
+                let existing_config = unwrap!(global_configs.get_mut(pos));
+                *existing_config = config;
+            } else {
+                global_configs.push(config);
+            }
+
+            let file = unwrap!(dir.find_file(LAUNCHER_GLOBAL_CONFIG_FILE_NAME),
+                               "Logic Error - Launcher start-up should ensure the file must be \
+                                present at this stage - Report bug.")
+                .clone();
+
+            file_helper::update_content(client_clone, file, Overwrite, metadata.id(), dir)
+                .map_err(FfiError::from)
+                .and_then(move |writer| {
+                    let ser = fry!(serialise(&global_configs));
+                    writer.write(&ser)
+                        .and_then(move |_| writer.close())
+                        .map_err(FfiError::from)
+                        .into_box()
+                })
+        })
+        .map(|_| ())
+        .into_box()
+}
+
+fn launcher_global_config_and_dir
+    (client: &Client)
+     -> Box<FfiFuture<(Vec<LauncherConfiguration>, Dir, DirMetadata)>> {
+    trace!("Get Launcher's config directory.");
+
+    let c2 = client.clone();
+
+    dir_helper::configuration_dir(client.clone(), LAUNCHER_GLOBAL_DIRECTORY_NAME.to_string())
+        .map_err(FfiError::from)
+        .and_then(move |(dir, metadata)| {
+            let file_fut = match dir.find_file(LAUNCHER_GLOBAL_CONFIG_FILE_NAME).cloned() {
+                Some(file) => ok!(file),
+                None => {
+                    trace!("Launcher's config file does not exist inside its config dir - \
+                            creating one.");
+
+                    file_helper::create(c2.clone(),
+                                        LAUNCHER_GLOBAL_CONFIG_FILE_NAME.to_string(),
+                                        Vec::new(),
+                                        metadata.id(),
+                                        dir.clone())
+                        .and_then(move |writer| writer.close())
+                        .map_err(FfiError::from)
+                        .map(move |updated_dir| {
+                            unwrap!(updated_dir.find_file(LAUNCHER_GLOBAL_CONFIG_FILE_NAME)).clone()
+                        })
+                        .into_box()
                 }
             };
-            let mut reader = try!(file_helper.read(&file));
 
-            let size = reader.size();
-
-            if size == 0 {
-                Vec::new()
-            } else {
-                try!(deserialise(&try!(reader.read(0, size))))
-            }
-        };
-
-        Ok((global_configs, dir_listing))
-    }
+            file_fut.and_then(move |file| {
+                    let reader = fry!(file_helper::read(c2, &file).map_err(FfiError::from));
+                    let size = reader.size();
+                    if size == 0 {
+                        ok!(Vec::new())
+                    } else {
+                        reader.read(0, size)
+                            .map_err(FfiError::from)
+                            .and_then(move |data| deserialise(&data).map_err(FfiError::from))
+                            .into_box()
+                    }
+                })
+                .map(move |global_configs| (global_configs, dir, metadata))
+                .map_err(FfiError::from)
+                .into_box()
+        })
+        .into_box()
 }
