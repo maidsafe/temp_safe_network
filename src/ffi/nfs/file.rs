cd13eb64e89f3e4e4b7f195ed8d84398b5132390
Nikita Baksalyar <nikita.baksalyar@maidsafe.net>
2016-10-24 01:49:02 +0500

    refactor/ffi: implement async delete and move for NFS files

--- src/ffi/nfs/file.rs
+++ src/ffi/nfs/file.rs
@@ -33,28 +33,53 @@ use ffi::helper;
 use ffi::object_cache::AppHandle;
 use futures::Future;
 use libc::{c_void, int32_t};
-use nfs::helper::file_helper;
+use nfs::NfsError;
+use nfs::helper::{dir_helper, file_helper};
 use nfs::helper::writer::Mode;
 use std::ptr;
 use time;
 
 
-// /// Delete a file.
-// #[no_mangle]
-// pub unsafe extern "C" fn nfs_delete_file(session: *const Session,
-//                                          app_handle: AppHandle,
-//                                          file_path: *const u8,
-//                                          file_path_len: usize,
-//                                          is_shared: bool)
-//                                          -> int32_t {
-//     helper::catch_unwind_i32(|| {
-//         trace!("FFI delete file, given the path.");
-// let file_path = ffi_try!(helper::c_utf8_to_str(file_path,
-// file_path_len));
-//         ffi_try!(delete_file(&*app_handle, file_path, is_shared));
-//         0
-//     })
-// }
+/// Delete a file.
+#[no_mangle]
+pub unsafe extern "C" fn nfs_delete_file(session: *const Session,
+                                         app_handle: AppHandle,
+                                         file_path: *const u8,
+                                         file_path_len: usize,
+                                         is_shared: bool,
+                                         user_data: *mut c_void,
+                                         o_cb: extern "C" fn(int32_t, *mut c_void))
+                                         -> int32_t {
+    helper::catch_unwind_i32(|| {
+        trace!("FFI delete file, given the path.");
+        let file_path = ffi_try!(helper::c_utf8_to_str(file_path, file_path_len));
+
+        let session = (*session).clone();
+        let s2 = session.clone();
+        let user_data = OpaqueCtx(user_data);
+
+        ffi_try!(session.send(CoreMsg::new(move |client| {
+            let mut obj_cache = unwrap!(s2.object_cache());
+            match obj_cache.get_app(app_handle) {
+                Ok(app) => {
+                    let fut = delete_file(&client, &app, file_path, is_shared)
+                        .then(move |res| {
+                            o_cb(ffi_result_code!(res), user_data.0);
+                            Ok(())
+                        })
+                        .into_box();
+                    Some(fut)
+                }
+                Err(e) => {
+                    o_cb(ffi_error_code!(e), user_data.0);
+                    None
+                }
+            }
+        })));
+
+        0
+    })
+}
 
 /// Get file. The returned FileDetails pointer must be disposed of by calling
 /// `file_details_drop` when no longer needed.
@@ -168,34 +193,58 @@ pub unsafe extern "C" fn nfs_modify_file(session: *const Session,
     })
 }
 
-// /// Move or copy a file.
-// #[no_mangle]
-// pub unsafe extern "C" fn nfs_move_file(app_handle: *const App,
-//                                        src_path: *const u8,
-//                                        src_path_len: usize,
-//                                        is_src_path_shared: bool,
-//                                        dst_path: *const u8,
-//                                        dst_path_len: usize,
-//                                        is_dst_path_shared: bool,
-//                                        retain_src: bool)
-//                                        -> int32_t {
-//     helper::catch_unwind_i32(|| {
-//         trace!("FFI move file, from {:?} to {:?}.", src_path, dst_path);
+/// Move or copy a file.
+#[no_mangle]
+pub unsafe extern "C" fn nfs_move_file(session: *const Session,
+                                       app_handle: AppHandle,
+                                       src_path: *const u8,
+                                       src_path_len: usize,
+                                       is_src_path_shared: bool,
+                                       dst_path: *const u8,
+                                       dst_path_len: usize,
+                                       is_dst_path_shared: bool,
+                                       retain_src: bool,
+                                       user_data: *mut c_void,
+                                       o_cb: extern "C" fn(int32_t, *mut c_void))
+                                       -> int32_t {
+    helper::catch_unwind_i32(|| {
+        trace!("FFI move file, from {:?} to {:?}.", src_path, dst_path);
 
-// let src_path = ffi_try!(helper::c_utf8_to_str(src_path,
-// src_path_len));
-// let dst_path = ffi_try!(helper::c_utf8_to_str(dst_path,
-// dst_path_len));
+        let src_path = ffi_try!(helper::c_utf8_to_str(src_path, src_path_len));
+        let dst_path = ffi_try!(helper::c_utf8_to_str(dst_path, dst_path_len));
 
-//         ffi_try!(move_file(&*app_handle,
-//                            src_path,
-//                            is_src_path_shared,
-//                            dst_path,
-//                            is_dst_path_shared,
-//                            retain_src));
-//         0
-//     })
-// }
+        let session = (*session).clone();
+        let s2 = session.clone();
+        let user_data = OpaqueCtx(user_data);
+
+        ffi_try!(session.send(CoreMsg::new(move |client| {
+            let mut obj_cache = unwrap!(s2.object_cache());
+            match obj_cache.get_app(app_handle) {
+                Ok(app) => {
+                    let fut = move_file(&client,
+                                        &app,
+                                        src_path,
+                                        is_src_path_shared,
+                                        dst_path,
+                                        is_dst_path_shared,
+                                        retain_src)
+                        .then(move |res| {
+                            o_cb(ffi_result_code!(res), user_data.0);
+                            Ok(())
+                        })
+                        .into_box();
+                    Some(fut)
+                }
+                Err(e) => {
+                    o_cb(ffi_error_code!(e), user_data.0);
+                    None
+                }
+            }
+        })));
+
+        0
+    })
+}
 
 /// Get file metadata. The returned pointer must be disposed of by calling
 /// `file_metadata_drop` when no longer needed.
@@ -243,16 +292,14 @@ pub unsafe extern "C" fn nfs_get_file_metadata(session: *const Session,
     })
 }
 
-// fn delete_file(app: &App, file_path: &str, is_shared: bool) -> Result<(),
-// FfiError> {
-//     let (mut directory, file_name) =
-//         try!(helper::dir_and_file(app, file_path, is_shared));
-
-// let _ = try!(file_helper::delete(app.client(), file_name, &mut
-// directory));
-
-//     Ok(())
-// }
+fn delete_file(client: &Client, app: &App, file_path: &str, is_shared: bool) -> Box<FfiFuture<()>> {
+    let c2 = client.clone();
+    helper::dir_and_file(&client.clone(), app, file_path, is_shared)
+        .and_then(move |(mut dir, dir_meta, filename)| {
+            file_helper::delete(c2, &filename, &dir_meta.id(), &mut dir).map_err(FfiError::from)
+        })
+        .into_box()
+}
 
 fn get_file(client: &Client,
             app: &App,
@@ -339,45 +386,47 @@ fn modify_file(client: &Client,
     }
 }
 
-// fn move_file(client: &Client,
-//              app: &App,
-//              src_path: &str,
-//              is_src_path_shared: bool,
-//              dst_path: &str,
-//              is_dst_path_shared: bool,
-//              retain_src: bool)
-//              -> Result<(), FfiError> {
-//     let (mut src_dir, src_file_name) =
-// try!(helper::get_directory_and_file(app, src_path,
-// is_src_path_shared));
-// let mut dst_dir = try!(helper::get_directory(app, dst_path,
-// is_dst_path_shared));
+fn move_file(client: &Client,
+             app: &App,
+             src_path: &str,
+             is_src_path_shared: bool,
+             dst_path: &str,
+             is_dst_path_shared: bool,
+             retain_src: bool)
+             -> Box<FfiFuture<()>> {
+    let c2 = client.clone();
+    let c3 = client.clone();
 
-//     if dst_dir.find_file(&src_file_name).is_some() {
-//         return Err(FfiError::from(NfsError::FileAlreadyExistsWithSameName));
-//     }
+    helper::dir_and_file(&client, app, src_path, is_src_path_shared)
+        .join(helper::dir(&client, app, dst_path, is_dst_path_shared))
+        .and_then(move |((mut src_dir, src_dir_meta, src_filename), (mut dst_dir, dst_dir_meta))| {
+            if dst_dir.find_file(&src_filename).is_some() {
+                return err!(FfiError::from(NfsError::FileAlreadyExistsWithSameName));
+            }
 
-//     let mut file = match src_dir.find_file(&src_file_name).cloned() {
-//         Some(file) => file,
-//         None => return Err(FfiError::PathNotFound),
-//     };
+            let file = match src_dir.find_file(&src_filename).cloned() {
+                Some(file) => file,
+                None => return err!(FfiError::PathNotFound),
+            };
 
-//     if retain_src {
-// file = try!(File::new(file.get_metadata().clone(),
-// file.get_datamap().clone()));
-//     }
+            let _ = fry!(dst_dir.upsert_file(file));
 
-//     dst_dir.upsert_file(file);
+            let fut = dir_helper::update(c2.clone(), &dst_dir_meta.id(), &dst_dir)
+                .map_err(FfiError::from);
 
-//     let _ = try!(dir_helper::update(&dst_dir));
+            if !retain_src {
+                let _ = fry!(src_dir.remove_file(&src_filename));
 
-//     if !retain_src {
-//         let _ = try!(src_dir.remove_file(&src_file_name));
-//         let _ = try!(dir_helper::update(&src_dir));
-//     }
-
-//     Ok(())
-// }
+                fut.and_then(move |_| {
+                        dir_helper::update(c3, &src_dir_meta.id(), &src_dir).map_err(FfiError::from)
+                    })
+                    .into_box()
+            } else {
+                fut.into_box()
+            }
+        })
+        .into_box()
+}
 
 fn get_file_metadata(client: &Client,
                      app: &App,
@@ -427,24 +476,56 @@ mod tests {
             .into_box()
     }
 
-    // #[test]
-    // fn delete_file() {
-    //     let app = test_utils::create_app(false);
-    //     let app_root_dir_key = unwrap!(app.get_app_dir_key());
+    #[test]
+    fn delete_file() {
+        let sess = test_utils::create_session();
+        let app = test_utils::create_app(&sess, false);
+        let app2 = app.clone();
+        let app_dir_id = unwrap!(app.app_dir());
+        let app_dir_id2 = app_dir_id.clone();
 
-    //     create_test_file(&app, "test_file.txt");
+        let (tx, rx) = mpsc::channel::<()>();
+        let tx2 = tx.clone();
 
-    //     let app_root_dir = unwrap!(dir_helper::get(&app_root_dir_key));
-    //     assert_eq!(app_root_dir.get_files().len(), 1);
-    //     assert!(app_root_dir.find_file("test_file.txt").is_some());
+        unwrap!(sess.send(CoreMsg::new(move |client| {
+            let c2 = client.clone();
+            let c3 = client.clone();
+            let c4 = client.clone();
+            let c5 = client.clone();
 
-    //     assert!(super::delete_file(&app, "/test_file.txt", false).is_ok());
+            let fut = create_test_file(&client, &app, "test_file.txt")
+                .then(move |res| {
+                    let _ = unwrap!(res, "can't create file test_file.txt");
+                    dir_helper::get(c2, &app_dir_id)
+                })
+                .then(move |res| {
+                    let app_root_dir = unwrap!(res);
+                    assert_eq!(app_root_dir.files().len(), 1);
+                    assert!(app_root_dir.find_file("test_file.txt").is_some());
+                    super::delete_file(&c3, &app, "/test_file.txt", false)
+                })
+                .then(move |res| {
+                    assert!(res.is_ok(), "can't delete file test_file.txt");
+                    dir_helper::get(c4, &app_dir_id2)
+                })
+                .then(move |res| {
+                    let app_root_dir = unwrap!(res);
+                    assert_eq!(app_root_dir.files().len(), 0);
+                    super::delete_file(&c5, &app2, "/test_file.txt", false)
+                })
+                .then(move |res| {
+                    assert!(res.is_err(),
+                            "deleting file /test_file.txt should return an error");
+                    unwrap!(tx2.send(()));
+                    Ok(())
+                })
+                .into_box();
+            Some(fut)
+        })));
 
-    //     let app_root_dir = unwrap!(dir_helper::get(&app_root_dir_key));
-    //     assert_eq!(app_root_dir.get_files().len(), 0);
-
-    //     assert!(super::delete_file(&app, "/test_file.txt", false).is_err());
-    // }
+        let _ = unwrap!(rx.recv_timeout(Duration::from_secs(15)));
+        unwrap!(sess.send(CoreMsg::build_terminator()));
+    }
 
     #[test]
     fn get_file() {
