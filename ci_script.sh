fff6b1bde4af803109221eb0eecbbe21e7b1c4c4
ustulation <ustulation@gmail.com>
2016-11-21 17:54:50 +0000

    fix/ci: combine travis and appeyor script into one

--- ci_script.sh
+++ ci_script.sh
@@ -8,15 +8,9 @@ set -o errtrace
 trap 'exit' ERR
 
 if [[ $TRAVIS = true ]]; then
-  echo "Into Travis ----"
   cd $TRAVIS_BUILD_DIR
 else
-  echo "Appveyor Folder ----"
-  echo $APPVEYOR_BUILD_FOLDER
   cd $APPVEYOR_BUILD_FOLDER
-  echo "ABCD Folder ----"
-  echo $ABCD
-  echo "ASDF Folder ${ASDF}----"
 fi
 
 if [[ ! $TRAVIS_RUST_VERSION = nightly ]]; then
