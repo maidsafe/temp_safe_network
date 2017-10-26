c6eac352d9bb43d0dc414a12bf6ce0b58a97047c
Adam Cigánek <adam.ciganek@gmail.com>
2017-10-26 16:50:03 +0200

    test/serialisation: expand the serialisation compatibility tests

--- safe_authenticator/src/tests/serialisation.rs
+++ safe_authenticator/src/tests/serialisation.rs
@@ -23,10 +23,14 @@ use AuthError;
 use AuthFuture;
 use Authenticator;
 use access_container;
+use app_auth;
+use config;
 use futures::{Future, future};
 use rand::{Rng, SeedableRng, XorShiftRng};
 use safe_core::{Client, FutureExt, MDataInfo};
 use safe_core::{config_handler, mock_vault_path};
+use safe_core::ipc::{AccessContainerEntry, AppExchangeInfo, AuthReq, Permission};
+use safe_core::ipc::req::ContainerPermissions;
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
@@ -39,7 +43,7 @@ fn write_data() {
     // Clear the vault store.
     unwrap!(fs::remove_file(vault_path));
 
-    let _auth =
+    let auth =
         unwrap!(Authenticator::create_acc(
         stash.locator.clone(),
         stash.password.clone(),
@@ -47,6 +51,15 @@ fn write_data() {
         || (),
     ));
 
+    unwrap!(auth.send(move |client| {
+        app_auth::authenticate(client, stash.auth_req.clone())
+            .then(|res| {
+                let _ = unwrap!(res);
+                Ok(())
+            })
+            .into_box()
+            .into()
+    }));
 }
 
 #[test]
@@ -61,6 +74,8 @@ fn read_data() {
 
     unwrap!(auth.send(move |client| {
         let c0 = client.clone();
+        let c1 = client.clone();
+        let c2 = client.clone();
 
         // Read access container and ensure all standard containers exists.
         access_container::fetch_authenticator_entry(client)
@@ -68,8 +83,22 @@ fn read_data() {
                 let (_, containers) = unwrap!(res);
                 verify_std_dirs(&c0, &containers)
             })
-            .then(|res| {
+            .then(move |res| {
                 unwrap!(res);
+                config::get_app(&c1, &stash.auth_req.app.id).map(move |app_info| (app_info, stash))
+            })
+            .then(move |res| {
+                let (app_info, stash) = unwrap!(res);
+                assert_eq!(app_info.info, stash.auth_req.app);
+
+                access_container::fetch_entry(&c2, &app_info.info.id, app_info.keys)
+                    .map(move |(_, ac_entry)| (ac_entry, stash))
+            })
+            .then(move |res| {
+                let (ac_entry, stash) = unwrap!(res);
+                let ac_entry = unwrap!(ac_entry);
+                verify_access_container_entry(&ac_entry, &stash.auth_req.containers);
+
                 Ok(())
             })
             .into_box()
@@ -99,10 +128,23 @@ fn verify_std_dirs(
         .into_box()
 }
 
+fn verify_access_container_entry(
+    actual_entry: &AccessContainerEntry,
+    requested_containers: &HashMap<String, ContainerPermissions>,
+) {
+    assert_eq!(actual_entry.len(), requested_containers.len());
+
+    for (name, expected_perms) in requested_containers {
+        let &(_, ref actual_perms) = unwrap!(actual_entry.get(name));
+        assert_eq!(actual_perms, expected_perms);
+    }
+}
+
 struct Stash {
     locator: String,
     password: String,
     invitation: String,
+    auth_req: AuthReq,
 }
 
 fn setup() -> (Stash, PathBuf) {
@@ -121,11 +163,41 @@ fn setup() -> (Stash, PathBuf) {
 
     // IMPORTANT: Use constant seed for repeatability.
     let mut rng = XorShiftRng::from_seed([0, 1, 2, 3]);
+
+    let app_exchange_info = AppExchangeInfo {
+        id: random_string(&mut rng, 16),
+        scope: None,
+        name: "test-app-0".to_string(),
+        vendor: "test-vendor-0".to_string(),
+    };
+
+    let mut containers = HashMap::new();
+    let _ = containers.insert(
+        "_documents".to_string(),
+        btree_set![
+            Permission::Read,
+            Permission::Insert,
+            Permission::Update,
+            Permission::Delete,
+        ],
+    );
+
+    let auth_req = AuthReq {
+        app: app_exchange_info,
+        app_container: false,
+        containers,
+    };
+
     let stash = Stash {
-        locator: rng.gen_ascii_chars().take(16).collect(),
-        password: rng.gen_ascii_chars().take(16).collect(),
+        locator: random_string(&mut rng, 16),
+        password: random_string(&mut rng, 16),
         invitation: String::new(),
+        auth_req,
     };
 
     (stash, mock_vault_path(&config))
+}
+
+fn random_string<R: Rng>(rng: &mut R, len: usize) -> String {
+    rng.gen_ascii_chars().take(len).collect()
 }
