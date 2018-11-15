8fd0d3878fdd0ef2dc964382ddb8380dd05b6594
Chris O'Neil <chris.oneil@gmail.com>
2018-11-15 14:38:09 +0000

    chore/ci: move packaging to script file
    The inline script wouldn't work on AppVeyor because AppVeyor treats
    a process printing anything to stderr as a failure. Cargo script
    definitely writes to stderr and hence was causing the build to fail. The
    issue is documented here:
    https://github.com/appveyor/ci/issues/1873
    
    You also need to use the special `PowerShell` notation (as opposed to
    `ps`) to get it to work correctly.

---
+++ scripts/package.ps1
@@ -0,0 +1,20 @@
+$cargo_script_path = $pwd.path + "\cargo-script.exe"
+Remove-Item -Path .\target\deploy -Recurse -Force -ErrorAction SilentlyContinue
+$url = "https://s3.eu-west-2.amazonaws.com/download-native-libs/cargo-script.exe"
+Invoke-WebRequest $url -OutFile "cargo-script.exe"
+New-Item -Path ".\target\deploy" -ItemType directory
+$COMMIT_MESSAGE = "$env:APPVEYOR_REPO_COMMIT_MESSAGE $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED"
+if ($COMMIT_MESSAGE -match "[Vv]ersion change.*safe_authenticator to ([^;]+)") {
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_app -d target\deploy --mock
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_app -d target\deploy
+} else {
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_app -d target\deploy --mock --commit
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_app -d target\deploy --commit
+}
+if ($COMMIT_MESSAGE -match "[Vv]ersion change.*safe_app to ([^;]+)") {
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator -d target\deploy --mock
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator -d target\deploy
+} else {
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator -d target\deploy --mock --commit
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator -d target\deploy --commit
+}
