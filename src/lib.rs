5aa17fd2ffba71c7a3214a31046ad95a16e49336
Marcin S <scatman@bu.edu>
2019-10-22 21:02:37 +0100

    chore/lints: remove explicitly-listed non-warn lints
    See https://hackmd.io/8odS50_AQV6U1wCHBfbeuw for the motivation.
    
    - Removed all `forbid` items: most are already `deny` by the compiler, not clear
    what explicitly forbidding them gives us.
    
    - `unsafe_code` stayed `deny`, other `deny` items removed as they are all `warn`
    and will be caught by CI. The `bad_style` lint was deprecated in favor of
    `nonstandard_style`, which is also `warn`.
    
    - Existing `warn` items mostly unchanged, except that a couple were added. These
    are all `allow` by the compiler, but useful to get warnings about.
    
    - Removed all `allow` items: they are already `allow` by the compiler.

--- src/lib.rs
+++ src/lib.rs
@@ -13,46 +13,18 @@
     html_favicon_url = "https://maidsafe.net/img/favicon.ico",
     test(attr(forbid(warnings)))
 )]
-#![forbid(
-    exceeding_bitshifts,
-    mutable_transmutes,
-    no_mangle_const_items,
-    unknown_crate_types,
-    warnings
-)]
-#![deny(
-    bad_style,
-    deprecated,
-    improper_ctypes,
-    missing_docs,
-    non_shorthand_field_patterns,
-    overflowing_literals,
-    plugin_as_library,
-    stable_features,
-    unconditional_recursion,
-    unknown_lints,
-    unsafe_code,
-    unused,
-    unused_allocation,
-    unused_attributes,
-    unused_comparisons,
-    unused_features,
-    unused_parens,
-    while_true
-)]
+// For explanation of lint checks, run `rustc -W help`.
+#![deny(unsafe_code)]
 #![warn(
+    // TODO: add missing debug implementations for structs?
+    // missing_debug_implementations,
+    missing_docs,
     trivial_casts,
     trivial_numeric_casts,
     unused_extern_crates,
     unused_import_braces,
     unused_qualifications,
     unused_results
-)]
-#![allow(
-    box_pointers,
-    missing_copy_implementations,
-    missing_debug_implementations,
-    variant_size_differences
 )]
 // For quick_error
 #![recursion_limit = "128"]
