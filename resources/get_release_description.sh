3d4f7848a118cf3b7ed7391493e8ffafdf828a53
Gabriel Viganotti <gabrielviganotti@gmail.com>
2019-12-11 16:39:25 -0300

    chore(ci): update release scripts to publish authd links, and remove scripts that are not necessary anymore

--- resources/get_release_description.sh
+++ resources/get_release_description.sh
@@ -15,30 +15,17 @@ if [[ -z "$ffi_version" ]]; then
     echo "You must supply a version number for safe-ffi."
     exit 1
 fi
+authd_version=$4
+if [[ -z "$authd_version" ]]; then
+    echo "You must supply a version number for safe-authd."
+    exit 1
+fi
 
 # The single quotes around EOF is to stop attempted variable and backtick expansion.
 read -r -d '' release_description << 'EOF'
-Command line interface for the SAFE Network.
+Command line interface for the SAFE Network. Refer to [SAFE CLI User Guide](https://github.com/maidsafe/safe-api/blob/master/safe-cli/README.md) for detailed instructions.
 
-## Development Builds
-
-There are also development versions of this release:
-[Linux](S3_LINUX_DEPLOY_URL)
-[macOS](S3_MACOS_DEPLOY_URL)
-[Windows](S3_WIN_DEPLOY_URL)
-
-The development version uses a mocked SAFE network, allowing you to work against a file that mimics the network, where SafeCoins are created for local use.
-
-## Bash Autocompletion
-
-If you are using Bash on Linux, you can get autocompletion for `safe` by doing the following:
-```
-curl -L SAFE_COMPLETION_URL > ~/.safe_completion
-chmod +x ~/.safe_completion
-echo "source ~/.safe_completion" >> ~/.bashrc
-```
-
-## SHA-256 checksums for release versions:
+## SHA-256 checksums for CLI binaries:
 ```
 Linux
 zip: ZIP_LINUX_CHECKSUM
@@ -53,29 +40,39 @@ zip: ZIP_WIN_CHECKSUM
 tar.gz: TAR_WIN_CHECKSUM
 ```
 
-## FFI
+### SAFE Authenticator daemon
+
+The Authenticator daemon exposes services which allow applications and users to create SAFE Network accounts, log in using an existing account's credentials (passphrase and password), authorise applications which need to store data on the network on behalf of the user, as well as revoke permissions previously granted to applications.
+The SAFE Authenticator, which runs as a daemon or as a service in Windows platforms, can be started and managed with the SAFE CLI if the `safe-authd`/`safe-authd.exe` binary is in the system's PATH with execution permissions (please refer to [Authenticator section in CLI User Guide](https://github.com/maidsafe/safe-api/blob/master/safe-cli/README.md#the-authenticator-daemon-authd) for detailed instructions).
+
+| OS | Download link | SHA-256 checksum |
+| --- | --- |
+| Linux | [Download](S3_AUTHD_LINUX_DEPLOY_URL) | ZIP_LINUX_CHECKSUM_AUTHD |
+| macOS | [Download](S3_AUTHD_MACOS_DEPLOY_URL) | ZIP_MACOS_CHECKSUM_AUTHD |
+| Windows | [Download](S3_AUTHD_WIN_DEPLOY_URL) | ZIP_WIN_CHECKSUM_AUTHD |
+
+### FFI
 
 FFI is used to generate the native libraries which can be used by other high level languages to consume the Rust API. The development versions use a mocked version of the SAFE Network.
 
-| OS | Production | Development |
-| --- | --- | --- |
-| Linux | [Download](S3_FFI_LINUX_DEPLOY_URL) | [Download](S3_FFI_DEV_LINUX_DEPLOY_URL) |
-| Windows | [Download](S3_FFI_WIN_DEPLOY_URL) | [Download](S3_FFI_DEV_WIN_DEPLOY_URL) |
-| macOS | [Download](S3_FFI_MACOS_DEPLOY_URL) | [Download](S3_FFI_DEV_MACOS_DEPLOY_URL) |
-| iOS | [Download](S3_FFI_IOS_DEPLOY_URL) | [Download](S3_FFI_DEV_IOS_DEPLOY_URL) |
-| Android ARMv7 | [Download](S3_FFI_ANDROID_ARMV7_DEPLOY_URL) | [Download](S3_FFI_DEV_ANDROID_ARMV7_DEPLOY_URL) |
-| Android x86_64 | [Download](S3_FFI_ANDROID_X86_64_DEPLOY_URL) | [Download](S3_FFI_DEV_ANDROID_X86_64_DEPLOY_URL) |
+| OS | Download link |
+| --- | --- |
+| Linux | [Download](S3_FFI_LINUX_DEPLOY_URL) |
+| macOS | [Download](S3_FFI_MACOS_DEPLOY_URL) |
+| Windows | [Download](S3_FFI_WIN_DEPLOY_URL) |
+| iOS | [Download](S3_FFI_IOS_DEPLOY_URL) |
+| Android ARMv7 | [Download](S3_FFI_ANDROID_ARMV7_DEPLOY_URL) |
+| Android x86_64 | [Download](S3_FFI_ANDROID_X86_64_DEPLOY_URL) |
 
 ## Related Links
-* [SAFE Authenticator CLI](https://github.com/maidsafe/safe-authenticator-cli/releases/latest/)
-* [SAFE Browser PoC](https://github.com/maidsafe/safe_browser/releases/)
+* [SAFE CLI User Guide](https://github.com/maidsafe/safe-api/blob/master/safe-cli/README.md)
+* [SAFE Browser](https://github.com/maidsafe/safe_browser/releases/)
 * [SAFE Vault](https://github.com/maidsafe/safe_vault/releases/latest/)
 EOF
 
-safe_completion_url="https:\/\/github.com\/maidsafe\/safe-api\/releases\/download\/$api_version\/safe_completion.sh"
-s3_linux_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-api-$api_version-x86_64-unknown-linux-gnu-dev.zip"
-s3_win_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-api-$api_version-x86_64-pc-windows-gnu-dev.zip"
-s3_macos_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-api-$api_version-x86_64-apple-darwin-dev.zip"
+s3_authd_linux_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-authd-$authd_version-x86_64-unknown-linux-gnu.zip"
+s3_authd_win_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-authd-$authd_version-x86_64-pc-windows-gnu.zip"
+s3_authd_macos_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-authd-$authd_version-x86_64-apple-darwin.zip"
 
 zip_linux_checksum=$(sha256sum \
     "./deploy/prod/safe-cli-$cli_version-x86_64-unknown-linux-gnu.zip" | \
@@ -96,12 +93,16 @@ tar_win_checksum=$(sha256sum \
     "./deploy/prod/safe-cli-$cli_version-x86_64-pc-windows-gnu.tar.gz" | \
     awk '{ print $1 }')
 
-s3_ffi_dev_linux_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-x86_64-unknown-linux-gnu-dev.zip"
-s3_ffi_dev_win_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-x86_64-pc-windows-gnu-dev.zip"
-s3_ffi_dev_macos_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-x86_64-apple-darwin-dev.zip"
-s3_ffi_dev_android_x86_64_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-x86_64-linux-android-dev.zip"
-s3_ffi_dev_android_armv7_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-armv7-linux-androideabi-dev.zip"
-s3_ffi_dev_ios_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-apple-ios-dev.zip"
+zip_linux_checksum_authd=$(sha256sum \
+    "./deploy/prod/safe-authd-$authd_version-x86_64-unknown-linux-gnu.zip" | \
+    awk '{ print $1 }')
+zip_macos_checksum_authd=$(sha256sum \
+    "./deploy/prod/safe-authd-$authd_version-x86_64-apple-darwin.zip" | \
+    awk '{ print $1 }')
+zip_win_checksum_authd=$(sha256sum \
+    "./deploy/prod/safe-authd-$authd_version-x86_64-pc-windows-gnu.zip" | \
+    awk '{ print $1 }')
+
 s3_ffi_linux_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-x86_64-unknown-linux-gnu.zip"
 s3_ffi_win_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-x86_64-pc-windows-gnu.zip"
 s3_ffi_macos_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-x86_64-apple-darwin.zip"
@@ -109,10 +110,10 @@ s3_ffi_android_x86_64_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-
 s3_ffi_android_armv7_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-armv7-linux-androideabi.zip"
 s3_ffi_ios_deploy_url="https:\/\/safe-api.s3.amazonaws.com\/safe-ffi-$ffi_version-apple-ios.zip"
 
-release_description=$(sed "s/S3_LINUX_DEPLOY_URL/$s3_linux_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/S3_MACOS_DEPLOY_URL/$s3_macos_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/S3_WIN_DEPLOY_URL/$s3_win_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/SAFE_COMPLETION_URL/$safe_completion_url/g" <<< "$release_description")
+release_description=$(sed "s/S3_AUTHD_LINUX_DEPLOY_URL/$s3_authd_linux_deploy_url/g" <<< "$release_description")
+release_description=$(sed "s/S3_AUTHD_MACOS_DEPLOY_URL/$s3_authd_macos_deploy_url/g" <<< "$release_description")
+release_description=$(sed "s/S3_AUTHD_WIN_DEPLOY_URL/$s3_authd_win_deploy_url/g" <<< "$release_description")
+
 release_description=$(sed "s/ZIP_LINUX_CHECKSUM/$zip_linux_checksum/g" <<< "$release_description")
 release_description=$(sed "s/ZIP_MACOS_CHECKSUM/$zip_macos_checksum/g" <<< "$release_description")
 release_description=$(sed "s/ZIP_WIN_CHECKSUM/$zip_win_checksum/g" <<< "$release_description")
@@ -120,17 +121,15 @@ release_description=$(sed "s/TAR_LINUX_CHECKSUM/$tar_linux_checksum/g" <<< "$rel
 release_description=$(sed "s/TAR_MACOS_CHECKSUM/$tar_macos_checksum/g" <<< "$release_description")
 release_description=$(sed "s/TAR_WIN_CHECKSUM/$tar_win_checksum/g" <<< "$release_description")
 
+release_description=$(sed "s/ZIP_LINUX_CHECKSUM_AUTHD/$zip_linux_checksum_authd/g" <<< "$release_description")
+release_description=$(sed "s/ZIP_MACOS_CHECKSUM_AUTHD/$zip_macos_checksum_authd/g" <<< "$release_description")
+release_description=$(sed "s/ZIP_WIN_CHECKSUM_AUTHD/$zip_win_checksum_authd/g" <<< "$release_description")
+
 release_description=$(sed "s/S3_FFI_LINUX_DEPLOY_URL/$s3_ffi_linux_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/S3_FFI_DEV_LINUX_DEPLOY_URL/$s3_ffi_dev_linux_deploy_url/g" <<< "$release_description")
 release_description=$(sed "s/S3_FFI_WIN_DEPLOY_URL/$s3_ffi_win_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/S3_FFI_DEV_WIN_DEPLOY_URL/$s3_ffi_dev_win_deploy_url/g" <<< "$release_description")
 release_description=$(sed "s/S3_FFI_MACOS_DEPLOY_URL/$s3_ffi_macos_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/S3_FFI_DEV_MACOS_DEPLOY_URL/$s3_ffi_dev_macos_deploy_url/g" <<< "$release_description")
 release_description=$(sed "s/S3_FFI_IOS_DEPLOY_URL/$s3_ffi_ios_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/S3_FFI_DEV_IOS_DEPLOY_URL/$s3_ffi_dev_ios_deploy_url/g" <<< "$release_description")
 release_description=$(sed "s/S3_FFI_ANDROID_ARMV7_DEPLOY_URL/$s3_ffi_android_armv7_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/S3_FFI_DEV_ANDROID_ARMV7_DEPLOY_URL/$s3_ffi_dev_android_armv7_deploy_url/g" <<< "$release_description")
 release_description=$(sed "s/S3_FFI_ANDROID_X86_64_DEPLOY_URL/$s3_ffi_android_x86_64_deploy_url/g" <<< "$release_description")
-release_description=$(sed "s/S3_FFI_DEV_ANDROID_X86_64_DEPLOY_URL/$s3_ffi_dev_android_x86_64_deploy_url/g" <<< "$release_description")
 
 echo "$release_description"
