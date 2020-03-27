0228c160437baa5ea8ed06cc72820db6eb905b7c
Ravinder Jangra <ravinderjangra@live.com>
2020-03-27 11:53:08 +0530

    refactor/ci: remove safe_app builds and refactor builds

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -4,17 +4,10 @@ $url = "https://s3.eu-west-2.amazonaws.com/download-native-libs/cargo-script.exe
 Invoke-WebRequest $url -OutFile "cargo-script.exe"
 New-Item -Path ".\target\deploy" -ItemType directory
 $COMMIT_MESSAGE = "$env:APPVEYOR_REPO_COMMIT_MESSAGE $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED"
-if ($COMMIT_MESSAGE -match "[Vv]ersion change.*safe_authenticator to ([^;]+)") {
-  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_app -d target\deploy --mock --strip --rebuild
-  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_app -d target\deploy --strip --rebuild
+if ($COMMIT_MESSAGE -match "[Vv]ersion change.*safe_authenticator_ffi to ([^;]+)") {
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator_ffi -d target\deploy --mock --strip --rebuild
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator_ffi -d target\deploy --strip --rebuild
 } else {
-  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_app -d target\deploy --mock --commit --strip --rebuild
-  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_app -d target\deploy --commit --strip --rebuild
-}
-if ($COMMIT_MESSAGE -match "[Vv]ersion change.*safe_app to ([^;]+)") {
-  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator -d target\deploy --mock --strip --rebuild
-  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator -d target\deploy --strip --rebuild
-} else {
-  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator -d target\deploy --mock --commit --strip --rebuild
-  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator -d target\deploy --commit --strip --rebuild
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator_ffi -d target\deploy --mock --commit --strip --rebuild
+  & $cargo_script_path script -- .\scripts\package.rs --lib --name safe_authenticator_ffi -d target\deploy --commit --strip --rebuild
 }
