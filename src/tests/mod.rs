d7939f4d153b667d619848a29db8f58433ef1461
Lionel Faber <lionel1704@gmail.com>
2020-08-13 14:59:09 +0530

    feat/logs: add thread name to the logs

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,6 +1,5 @@
 use crate::config_handler::write_connection_info;
 use crate::{network::Network as NetworkLayer, Command, Config, Node};
-use crate::{Command, Config, Node};
 use crossbeam_channel::Sender;
 use flexi_logger::{DeferredNow, Logger};
 use log::{self, Record};
@@ -15,16 +14,18 @@ use std::thread::{self, JoinHandle};
 
 mod client;
 
-struct InProcNetwork {
+struct Network {
     vaults: Vec<(Sender<Command>, JoinHandle<()>)>,
 }
 
 fn init_logging(log_dir: Option<&PathBuf>) {
     // Custom formatter for logs
     let do_format = move |writer: &mut dyn Write, clock: &mut DeferredNow, record: &Record| {
+        let thread = std::thread::current();
         write!(
             writer,
-            "{} {} [{}:{}] {}",
+            "{} {} {} [{}:{}] {}",
+            thread.name().unwrap_or("Untitled"),
             record.level(),
             clock.now().to_rfc3339(),
             record.file().unwrap_or_default(),
@@ -57,63 +58,73 @@ impl Network {
         node_config.listen_on_loopback();
         let (command_tx, command_rx) = crossbeam_channel::bounded(1);
         let mut genesis_config = node_config.clone();
-        let handle = thread::spawn(move || {
-            genesis_config.set_flag("first", 1);
-            let path = path.join("genesis-vault");
-            genesis_config.set_root_dir(&path);
-            genesis_config.listen_on_loopback();
+        let handle = std::thread::Builder::new()
+            .name("vault-genesis".to_string())
+            .spawn(move || {
+                genesis_config.set_flag("first", 1);
+                let path = path.join("genesis-vault");
+                genesis_config.set_root_dir(&path);
+                genesis_config.listen_on_loopback();
 
-            let mut routing_config = RoutingConfig::default();
-            routing_config.first = genesis_config.is_first();
-            routing_config.transport_config = genesis_config.network_config().clone();
+                let mut routing_config = RoutingConfig::default();
+                routing_config.first = genesis_config.is_first();
+                routing_config.transport_config = genesis_config.network_config().clone();
 
-            let (routing, routing_rx, client_rx) = Routing::new(routing_config);
-            let routing_layer = Rc::new(RefCell::new(routing));
-            let routing = NetworkLayer::new(routing_layer.clone());
-            let receiver =
-                crate::Receiver::new(routing_rx, client_rx, command_rx, routing_layer.clone());
-            let mut node = Node::new(
-                receiver,
-                routing.clone(),
-                &genesis_config,
-                rand::thread_rng(),
-            )
-            .expect("Unable to start vault Node");
-            let our_conn_info = node
-                .our_connection_info()
-                .expect("Could not get genesis info");
-            let _ = write_connection_info(&our_conn_info).unwrap();
-            node.run();
-        });
+                let (routing, routing_rx, client_rx) = Routing::new(routing_config);
+                let routing_layer = Rc::new(RefCell::new(routing));
+                let routing = NetworkLayer::new(routing_layer.clone());
+                let receiver =
+                    crate::Receiver::new(routing_rx, client_rx, command_rx, routing_layer.clone());
+                let mut node = Node::new(
+                    receiver,
+                    routing.clone(),
+                    &genesis_config,
+                    rand::thread_rng(),
+                )
+                .expect("Unable to start vault Node");
+                let our_conn_info = node
+                    .our_connection_info()
+                    .expect("Could not get genesis info");
+                let _ = write_connection_info(&our_conn_info).unwrap();
+                node.run();
+            })
+            .unwrap();
         vaults.push((command_tx, handle));
         for i in 1..no_of_vaults {
             thread::sleep(std::time::Duration::from_secs(30));
             let (command_tx, command_rx) = crossbeam_channel::bounded(1);
             let mut vault_config = node_config.clone();
-            let handle = thread::spawn(move || {
-                let vault_path = path.join(format!("vault-{}", i));
-                println!("Starting new vault: {:?}", &vault_path);
-                vault_config.set_root_dir(&vault_path);
+            let handle = std::thread::Builder::new()
+                .name(format!("Vault-{}", i))
+                .spawn(move || {
+                    let vault_path = path.join(format!("vault-{}", i));
+                    println!("Starting new vault: {:?}", &vault_path);
+                    vault_config.set_root_dir(&vault_path);
 
-                let mut network_config = NetworkConfig::default();
-                let _ = network_config
-                    .hard_coded_contacts
-                    .insert(genesis_info.clone());
-                vault_config.set_network_config(network_config);
-                vault_config.listen_on_loopback();
+                    let mut network_config = NetworkConfig::default();
+                    let _ = network_config
+                        .hard_coded_contacts
+                        .insert(genesis_info.clone());
+                    vault_config.set_network_config(network_config);
+                    vault_config.listen_on_loopback();
 
-                let mut routing_config = RoutingConfig::default();
-                routing_config.transport_config = vault_config.network_config().clone();
+                    let mut routing_config = RoutingConfig::default();
+                    routing_config.transport_config = vault_config.network_config().clone();
 
-                let (routing, routing_rx, client_rx) = Routing::new(routing_config);
-                let routing_layer = Rc::new(RefCell::new(routing));
-                let routing = Network::new(routing_layer.clone());
-                let receiver =
-                    crate::Receiver::new(routing_rx, client_rx, command_rx, routing_layer.clone());
-                let mut node = Node::new(receiver, routing, &vault_config, rand::thread_rng())
-                    .expect("Unable to start vault Node");
-                node.run();
-            });
+                    let (routing, routing_rx, client_rx) = Routing::new(routing_config);
+                    let routing_layer = Rc::new(RefCell::new(routing));
+                    let routing = NetworkLayer::new(routing_layer.clone());
+                    let receiver = crate::Receiver::new(
+                        routing_rx,
+                        client_rx,
+                        command_rx,
+                        routing_layer.clone(),
+                    );
+                    let mut node = Node::new(receiver, routing, &vault_config, rand::thread_rng())
+                        .expect("Unable to start vault Node");
+                    node.run();
+                })
+                .unwrap();
             vaults.push((command_tx, handle));
         }
         Self { vaults }
