maidsafe/safe-vault

refactor(all): add async/await to the rest of the vault code

src/node/elder_duties/data_section/mod.rs
@@ -49,7 +49,7 @@ impl DataSection {
         let metadata = Metadata::new(info, &total_used_space, network.clone())?;
 
         // Rewards
-        let keypair = utils::key_pair(network.clone())?;
+        let keypair = utils::key_pair(network.clone()).await?;
         let public_key_set = network.public_key_set().await?;
         let actor = TransferActor::new(keypair, public_key_set, Validator {});
         let reward_calc = RewardCalc::new(network.clone());
@@ -66,16 +66,16 @@ impl DataSection {
         use DataSectionDuty::*;
         match duty {
             RunAsMetadata(duty) => self.metadata.process(duty).await,
-            RunAsRewards(duty) => self.rewards.process(duty),
+            RunAsRewards(duty) => self.rewards.process(duty).await,
         }
     }
 
     // Transition the section funds account to the new key.
     pub async fn elders_changed(&mut self) -> Option<NodeOperation> {
         let pub_key_set = self.network.public_key_set().await.ok()?;
-        let keypair = utils::key_pair(self.network.clone()).ok()?;
+        let keypair = utils::key_pair(self.network.clone()).await.ok()?;
         let actor = TransferActor::new(keypair, pub_key_set, Validator {});
-        self.rewards.transition(actor)
+        self.rewards.transition(actor).await
     }
 
     // At section split, all Elders get their reward payout.
@@ -91,12 +91,12 @@ impl DataSection {
 
         // Then payout rewards to all the Elders.
         let elders = self.network.our_elder_names().await;
-        self.rewards.payout_rewards(elders)
+        self.network.payout_rewards(elders).await
     }
 
     /// When a new node joins, it is registered for receiving rewards.
-    pub fn new_node_joined(&mut self, id: XorName) -> Option<NodeOperation> {
-        self.rewards.process(RewardDuty::AddNewNode(id))
+    pub async fn new_node_joined(&mut self, id: XorName) -> Option<NodeOperation> {
+        self.rewards.process(RewardDuty::AddNewNode(id)).await
     }
 
     /// When a relocated node joins, a DataSection
@@ -113,7 +113,7 @@ impl DataSection {
             old_node_id,
             new_node_id,
             age,
-        });
+        }).await;
         let second = self.metadata.trigger_chunk_duplication(new_node_id).await;
         Some(vec![first, second].into())
     }
@@ -123,7 +123,7 @@ impl DataSection {
     pub async fn member_left(&mut self, node_id: XorName, _age: u8) -> Option<NodeOperation> {
         // marks the reward account as
         // awaiting claiming of the counter
-        let first = self.rewards.process(RewardDuty::DeactivateNode(node_id));
+        let first = self.rewards.process(RewardDuty::DeactivateNode(node_id)).await;
         let second = self.metadata.trigger_chunk_duplication(node_id).await;
         Some(vec![first, second].into())
     }

src/node/elder_duties/data_section/rewards/section_funds.rs
@@ -62,7 +62,7 @@ impl SectionFunds {
     }
 
     /// At Elder churn, we must transition to a new account.
-    pub fn transition(&mut self, to: TransferActor<Validator>) -> Option<MessagingDuty> {
+    pub async fn transition(&mut self, to: TransferActor<Validator>) -> Option<MessagingDuty> {
         if self.is_transitioning() {
             // hm, could be tricky edge cases here, but
             // we'll start by assuming there will only be
@@ -104,7 +104,7 @@ impl SectionFunds {
                     self.wrapping.send(Message::NodeCmd {
                         cmd: Transfers(ValidateSectionPayout(event.signed_transfer)),
                         id: MessageId::new(),
-                    })
+                    }).await
                 }
             }
             Ok(None) => None, // Would indicate that this apparently has already been done, so no change.
@@ -114,7 +114,7 @@ impl SectionFunds {
 
     /// Will validate and sign the payout, and ask of the replicas to
     /// do the same, and await their responses as to accumulate the result.
-    pub fn initiate_reward_payout(&mut self, payout: Payout) -> Option<MessagingDuty> {
+    pub async fn initiate_reward_payout(&mut self, payout: Payout) -> Option<MessagingDuty> {
         if self.state.finished.contains(&payout.node_id) {
             return None;
         }
@@ -141,7 +141,7 @@ impl SectionFunds {
                     self.wrapping.send(Message::NodeCmd {
                         cmd: Transfers(ValidateSectionPayout(event.signed_transfer)),
                         id: MessageId::new(),
-                    })
+                    }).await
                 }
             }
             Ok(None) => None, // Would indicate that this apparently has already been done, so no change.
@@ -152,7 +152,7 @@ impl SectionFunds {
     /// As all Replicas have accumulated the distributed
     /// actor cmds and applied them, they'll send out the
     /// result, which each actor instance accumulates locally.
-    pub fn receive(&mut self, validation: TransferValidated) -> Option<NodeOperation> {
+    pub async fn receive(&mut self, validation: TransferValidated) -> Option<NodeOperation> {
         use NodeCmd::*;
         use NodeTransferCmd::*;
         match self.actor.receive(validation) {
@@ -183,7 +183,7 @@ impl SectionFunds {
 
                     // If there are queued payouts,
                     // the first in queue will be executed.
-                    let queued_op = self.try_pop_queue();
+                    let queued_op = self.try_pop_queue().await;
 
                     // We ask of our Replicas to register this transfer.
                     let reg_op = self
@@ -191,7 +191,7 @@ impl SectionFunds {
                         .send(Message::NodeCmd {
                             cmd: Transfers(RegisterSectionPayout(proof)),
                             id: MessageId::new(),
-                        })?
+                        }).await?
                         .into();
 
                     if let Some(queued) = queued_op {
@@ -210,14 +210,14 @@ impl SectionFunds {
 
     // Can safely be called without overwriting any
     // payout in flight, since validations for that are made.
-    fn try_pop_queue(&mut self) -> Option<NodeOperation> {
+    async fn try_pop_queue(&mut self) -> Option<NodeOperation> {
         if let Some(payout) = self.state.queued_payouts.pop_front() {
             // Validation logic when inititating rewards prevents enqueueing a payout that is already
             // in the finished set. Therefore, calling initiate here cannot return None because of
             // the payout already being finished.
             // For that reason it is safe to enqueue it again, if this call returns None.
             // (we will not loop on that payout)
-            if let Some(msg) = self.initiate_reward_payout(payout.clone()) {
+            if let Some(msg) = self.initiate_reward_payout(payout.clone()).await {
                 return Some(msg.into());
             } else if !self.state.finished.contains(&payout.node_id) {
                 // buut.. just to prevent any future changes to

src/node/elder_duties/key_section/mod.rs
@@ -49,7 +49,7 @@ pub struct KeySection<R: CryptoRng + Rng> {
 
 impl<R: CryptoRng + Rng> KeySection<R> {
     pub async fn new(info: &NodeInfo, routing: Network, rng: R) -> Result<Self> {
-        let gateway = ClientGateway::new(info, routing.clone(), rng)?;
+        let gateway = ClientGateway::new(info, routing.clone(), rng).await?;
         let replica_manager = Self::new_replica_manager(info, routing.clone()).await?;
         let payments = Payments::new(info.keys.clone(), replica_manager.clone());
         let transfers = Transfers::new(info.keys.clone(), replica_manager.clone());
@@ -68,9 +68,9 @@ impl<R: CryptoRng + Rng> KeySection<R> {
     /// Issues queries to Elders of the section
     /// as to catch up with shares state and
     /// start working properly in the group.
-    pub fn catchup_with_section(&mut self) -> Option<NodeOperation> {
+    pub async fn catchup_with_section(&mut self) -> Option<NodeOperation> {
         // currently only at2 replicas need to catch up
-        self.transfers.catchup_with_replicas()
+        self.transfers.catchup_with_replicas().await
     }
 
     // Update our replica with the latest keys
@@ -121,7 +121,7 @@ impl<R: CryptoRng + Rng> KeySection<R> {
             EvaluateClientMsg(msg) => self.msg_analysis.evaluate(&msg).await,
             RunAsGateway(duty) => self.gateway.process(duty).await,
             RunAsPayment(duty) => self.payments.process(&duty).await,
-            RunAsTransfers(duty) => self.transfers.process(&duty),
+            RunAsTransfers(duty) => self.transfers.process(&duty).await,
         }
     }
 

src/node/node_duties/network_events.rs
@@ -26,7 +26,7 @@ impl NetworkEvents {
         Self { analysis }
     }
 
-    pub fn process(&mut self, event: RoutingEvent) -> Option<NodeOperation> {
+    pub async fn process(&mut self, event: RoutingEvent) -> Option<NodeOperation> {
         use ElderDuty::*;
         use NodeDuty::*;
 
@@ -80,7 +80,7 @@ impl NetworkEvents {
                     src,
                     dst
                 );
-                self.evaluate_msg(content)
+                self.evaluate_msg(content).await
             }
             RoutingEvent::EldersChanged {
                 key,
@@ -99,11 +99,11 @@ impl NetworkEvents {
         }
     }
 
-    fn evaluate_msg(&mut self, content: Bytes) -> Option<NodeOperation> {
+    async fn evaluate_msg(&mut self, content: Bytes) -> Option<NodeOperation> {
         match bincode::deserialize::<MsgEnvelope>(&content) {
             Ok(msg) => {
                 warn!("Message Envelope received. Contents: {:?}", &msg);
-                self.analysis.evaluate(&msg)
+                self.analysis.evaluate(&msg).await
             }
             Err(e) => {
                 error!(

maidsafe/temp_safe_network

src/node/elder_duties/data_section/mod.rs
@@ -51,7 +51,7 @@ impl DataSection {
         let metadata = Metadata::new(info, dbs, &total_used_space, network.clone())?;
 
         // Rewards
-        let keypair = utils::key_pair(network.clone())?;
+        let keypair = utils::key_pair(network.clone()).await?;
         let public_key_set = network.public_key_set().await?;
         let actor = TransferActor::new(keypair, public_key_set, Validator {});
         let reward_calc = RewardCalc::new(network.clone());
@@ -68,16 +68,16 @@ impl DataSection {
         use DataSectionDuty::*;
         match duty {
             RunAsMetadata(duty) => self.metadata.process_metadata_duty(duty).await,
-            RunAsRewards(duty) => self.rewards.process_reward_duty(duty),
+            RunAsRewards(duty) => self.rewards.process_reward_duty(duty).await,
         }
     }
 
     // Transition the section funds account to the new key.
     pub async fn elders_changed(&mut self) -> Option<NodeOperation> {
         let pub_key_set = self.network.public_key_set().await.ok()?;
-        let keypair = utils::key_pair(self.network.clone()).ok()?;
+        let keypair = utils::key_pair(self.network.clone()).await.ok()?;
         let actor = TransferActor::new(keypair, pub_key_set, Validator {});
-        self.rewards.transition(actor)
+        self.rewards.transition(actor).await
     }
 
     // At section split, all Elders get their reward payout.
@@ -93,12 +93,12 @@ impl DataSection {
 
         // Then payout rewards to all the Elders.
         let elders = self.network.our_elder_names().await;
-        self.rewards.payout_rewards(elders)
+        self.network.payout_rewards(elders).await
     }
 
     /// When a new node joins, it is registered for receiving rewards.
-    pub fn new_node_joined(&mut self, id: XorName) -> Option<NodeOperation> {
-        self.rewards.process_reward_duty(RewardDuty::AddNewNode(id))
+    pub async fn new_node_joined(&mut self, id: XorName) -> Option<NodeOperation> {
+        self.rewards.process_reward_duty(RewardDuty::AddNewNode(id)).await
     }
 
     /// When a relocated node joins, a DataSection
@@ -117,7 +117,7 @@ impl DataSection {
                 old_node_id,
                 new_node_id,
                 age,
-            });
+            }).await;
         let second = self.metadata.trigger_chunk_duplication(new_node_id).await;
         Some(vec![first, second].into())
     }
@@ -129,7 +129,7 @@ impl DataSection {
         // awaiting claiming of the counter
         let first = self
             .rewards
-            .process_reward_duty(RewardDuty::DeactivateNode(node_id));
+            .process_reward_duty(RewardDuty::DeactivateNode(node_id)).await;
         let second = self.metadata.trigger_chunk_duplication(node_id).await;
         Some(vec![first, second].into())
     }

src/node/elder_duties/data_section/rewards/section_funds.rs
@@ -62,7 +62,7 @@ impl SectionFunds {
     }
 
     /// At Elder churn, we must transition to a new account.
-    pub fn transition(&mut self, to: TransferActor<Validator>) -> Option<NodeMessagingDuty> {
+    pub async fn transition(&mut self, to: TransferActor<Validator>) -> Option<NodeMessagingDuty> {
         if self.is_transitioning() {
             // hm, could be tricky edge cases here, but
             // we'll start by assuming there will only be
@@ -104,7 +104,7 @@ impl SectionFunds {
                     self.wrapping.send(Message::NodeCmd {
                         cmd: Transfers(ValidateSectionPayout(event.signed_transfer)),
                         id: MessageId::new(),
-                    })
+                    }).await
                 }
             }
             Ok(None) => None, // Would indicate that this apparently has already been done, so no change.
@@ -114,7 +114,7 @@ impl SectionFunds {
 
     /// Will validate and sign the payout, and ask of the replicas to
     /// do the same, and await their responses as to accumulate the result.
-    pub fn initiate_reward_payout(&mut self, payout: Payout) -> Option<NodeMessagingDuty> {
+    pub async fn initiate_reward_payout(&mut self, payout: Payout) -> Option<NodeMessagingDuty> {
         if self.state.finished.contains(&payout.node_id) {
             return None;
         }
@@ -141,7 +141,7 @@ impl SectionFunds {
                     self.wrapping.send(Message::NodeCmd {
                         cmd: Transfers(ValidateSectionPayout(event.signed_transfer)),
                         id: MessageId::new(),
-                    })
+                    }).await
                 }
             }
             Ok(None) => None, // Would indicate that this apparently has already been done, so no change.
@@ -152,7 +152,7 @@ impl SectionFunds {
     /// As all Replicas have accumulated the distributed
     /// actor cmds and applied them, they'll send out the
     /// result, which each actor instance accumulates locally.
-    pub fn receive(&mut self, validation: TransferValidated) -> Option<NodeOperation> {
+    pub async fn receive(&mut self, validation: TransferValidated) -> Option<NodeOperation> {
         use NodeCmd::*;
         use NodeTransferCmd::*;
         match self.actor.receive(validation) {
@@ -183,7 +183,7 @@ impl SectionFunds {
 
                     // If there are queued payouts,
                     // the first in queue will be executed.
-                    let queued_op = self.try_pop_queue();
+                    let queued_op = self.try_pop_queue().await;
 
                     // We ask of our Replicas to register this transfer.
                     let reg_op = self
@@ -191,7 +191,7 @@ impl SectionFunds {
                         .send(Message::NodeCmd {
                             cmd: Transfers(RegisterSectionPayout(proof)),
                             id: MessageId::new(),
-                        })?
+                        }).await?
                         .into();
 
                     if let Some(queued) = queued_op {
@@ -210,14 +210,14 @@ impl SectionFunds {
 
     // Can safely be called without overwriting any
     // payout in flight, since validations for that are made.
-    fn try_pop_queue(&mut self) -> Option<NodeOperation> {
+    async fn try_pop_queue(&mut self) -> Option<NodeOperation> {
         if let Some(payout) = self.state.queued_payouts.pop_front() {
             // Validation logic when inititating rewards prevents enqueueing a payout that is already
             // in the finished set. Therefore, calling initiate here cannot return None because of
             // the payout already being finished.
             // For that reason it is safe to enqueue it again, if this call returns None.
             // (we will not loop on that payout)
-            if let Some(msg) = self.initiate_reward_payout(payout.clone()) {
+            if let Some(msg) = self.initiate_reward_payout(payout.clone()).await {
                 return Some(msg.into());
             } else if !self.state.finished.contains(&payout.node_id) {
                 // buut.. just to prevent any future changes to

src/node/elder_duties/key_section/mod.rs
@@ -50,7 +50,7 @@ pub struct KeySection<R: CryptoRng + Rng> {
 
 impl<R: CryptoRng + Rng> KeySection<R> {
     pub async fn new(info: &NodeInfo, rate_limit: RateLimit, routing: Network, rng: R) -> Result<Self> {
-        let gateway = ClientGateway::new(info, routing.clone(), rng)?;
+        let gateway = ClientGateway::new(info, routing.clone(), rng).await?;
         let replica_manager = Self::new_replica_manager(info, routing.clone()).await?;
         let payments = Payments::new(info.keys.clone(), rate_limit, replica_manager.clone());
         let transfers = Transfers::new(info.keys.clone(), replica_manager.clone());
@@ -69,9 +69,9 @@ impl<R: CryptoRng + Rng> KeySection<R> {
     /// Issues queries to Elders of the section
     /// as to catch up with shares state and
     /// start working properly in the group.
-    pub fn catchup_with_section(&mut self) -> Option<NodeOperation> {
+    pub async fn catchup_with_section(&mut self) -> Option<NodeOperation> {
         // currently only at2 replicas need to catch up
-        self.transfers.catchup_with_replicas()
+        self.transfers.catchup_with_replicas().await
     }
 
     // Update our replica with the latest keys
@@ -122,7 +122,7 @@ impl<R: CryptoRng + Rng> KeySection<R> {
             EvaluateClientMsg(msg) => self.msg_analysis.evaluate(&msg).await,
             RunAsGateway(duty) => self.gateway.process_as_gateway(duty).await,
             RunAsPayment(duty) => self.payments.process_payment_duty(&duty).await,
-            RunAsTransfers(duty) => self.transfers.process_transfer_duty(&duty),
+            RunAsTransfers(duty) => self.transfers.process_transfer_duty(&duty).await,
         }
     }
 

src/node/node_duties/network_events.rs
@@ -26,7 +26,7 @@ impl NetworkEvents {
         Self { analysis }
     }
 
-    pub fn process_network_event(&mut self, event: RoutingEvent) -> Option<NodeOperation> {
+    pub async fn process_network_event(&mut self, event: RoutingEvent) -> Option<NodeOperation> {
         use ElderDuty::*;
         use NodeDuty::*;
 
@@ -80,7 +80,7 @@ impl NetworkEvents {
                     src,
                     dst
                 );
-                self.evaluate_msg(content)
+                self.evaluate_msg(content).await
             }
             RoutingEvent::EldersChanged {
                 key,
@@ -99,11 +99,11 @@ impl NetworkEvents {
         }
     }
 
-    fn evaluate_msg(&mut self, content: Bytes) -> Option<NodeOperation> {
+    async fn evaluate_msg(&mut self, content: Bytes) -> Option<NodeOperation> {
         match bincode::deserialize::<MsgEnvelope>(&content) {
             Ok(msg) => {
                 warn!("Message Envelope received. Contents: {:?}", &msg);
-                self.analysis.evaluate(&msg)
+                self.analysis.evaluate(&msg).await
             }
             Err(e) => {
                 error!(