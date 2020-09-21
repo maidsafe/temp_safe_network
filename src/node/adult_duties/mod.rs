maidsafe/safe-vault

feat(async): introduce async logging and make functions async/await to remove the use of block_on(...)

src/node/adult_duties/mod.rs
@@ -15,9 +15,7 @@ use crate::{
     Result,
 };
 use std::{
-    cell::Cell,
-    fmt::{self, Display, Formatter},
-    rc::Rc,
+    fmt::{self, Display, Formatter}
 };
 use std::sync::{Arc, Mutex};
 
@@ -33,12 +31,12 @@ impl AdultDuties {
         Ok(Self { chunks })
     }
 
-    pub fn process(&mut self, duty: &AdultDuty) -> Option<NodeOperation> {
+    pub async fn process(&mut self, duty: &AdultDuty) -> Option<NodeOperation> {
         use AdultDuty::*;
         use ChunkDuty::*;
         let RunAsChunks(chunk_duty) = duty;
         let result = match chunk_duty {
-            ReadChunk(msg) | WriteChunk(msg) => self.chunks.receive_msg(msg),
+            ReadChunk(msg) | WriteChunk(msg) => self.chunks.receive_msg(msg).await,
         };
 
         result.map(|c| c.into())

src/node/elder_duties/data_section/metadata/mod.rs
@@ -26,11 +26,7 @@ use elder_stores::ElderStores;
 use map_storage::MapStorage;
 use sequence_storage::SequenceStorage;
 use sn_data_types::{ElderDuties, Message, MsgEnvelope};
-use std::{
-    cell::Cell,
-    fmt::{self, Display, Formatter},
-    rc::Rc,
-};
+use std::fmt::{self, Display, Formatter};
 use std::sync::{Arc, Mutex};
 use xor_name::XorName;
 
@@ -69,16 +65,16 @@ impl Metadata {
         })
     }
 
-    pub fn process(&mut self, duty: MetadataDuty) -> Option<NodeOperation> {
+    pub async fn process(&mut self, duty: MetadataDuty) -> Option<NodeOperation> {
         use MetadataDuty::*;
         match duty {
-            ProcessRead(msg) | ProcessWrite(msg) => self.process_msg(msg),
+            ProcessRead(msg) | ProcessWrite(msg) => self.process_msg(msg).await,
         }
     }
 
-    fn process_msg(&mut self, msg: MsgEnvelope) -> Option<NodeOperation> {
+    async fn process_msg(&mut self, msg: MsgEnvelope) -> Option<NodeOperation> {
         match &msg.message {
-            Message::Cmd { .. } => writing::get_result(msg, &mut self.elder_stores),
+            Message::Cmd { .. } => writing::get_result(msg, &mut self.elder_stores).await,
             Message::Query { .. } => reading::get_result(msg, &self.elder_stores).map(|c| c.into()),
             _ => None, // only Queries and Cmds from client is handled at Metadata
         }
@@ -87,8 +83,8 @@ impl Metadata {
     // This should be called whenever a node leaves the section. It fetches the list of data that was
     // previously held by the node and requests the other holders to store an additional copy.
     // The list of holders is also updated by removing the node that left.
-    pub fn trigger_chunk_duplication(&mut self, node: XorName) -> Option<NodeOperation> {
-        self.elder_stores.blob_register_mut().duplicate_chunks(node)
+    pub async fn trigger_chunk_duplication(&mut self, node: XorName) -> Option<NodeOperation> {
+        self.elder_stores.blob_register_mut().duplicate_chunks(node).await
     }
 
     // trace!(

src/node/elder_duties/data_section/mod.rs
@@ -21,7 +21,6 @@ use crate::{
 };
 use sn_routing::Prefix;
 use sn_transfers::TransferActor;
-use std::{cell::Cell, rc::Rc};
 use xor_name::XorName;
 use std::sync::{Arc, Mutex};
 
@@ -41,7 +40,7 @@ pub struct DataSection {
 
 impl DataSection {
     ///
-    pub fn new(
+    pub async fn new(
         info: &NodeInfo,
         total_used_space: &Arc<Mutex<u64>>,
         network: Network,
@@ -51,7 +50,7 @@ impl DataSection {
 
         // Rewards
         let keypair = utils::key_pair(network.clone())?;
-        let public_key_set = network.public_key_set()?;
+        let public_key_set = network.public_key_set().await?;
         let actor = TransferActor::new(keypair, public_key_set, Validator {});
         let reward_calc = RewardCalc::new(network.clone());
         let rewards = Rewards::new(info.keys.clone(), actor, reward_calc);
@@ -63,24 +62,24 @@ impl DataSection {
         })
     }
 
-    pub fn process(&mut self, duty: DataSectionDuty) -> Option<NodeOperation> {
+    pub async fn process(&mut self, duty: DataSectionDuty) -> Option<NodeOperation> {
         use DataSectionDuty::*;
         match duty {
-            RunAsMetadata(duty) => self.metadata.process(duty),
+            RunAsMetadata(duty) => self.metadata.process(duty).await,
             RunAsRewards(duty) => self.rewards.process(duty),
         }
     }
 
     // Transition the section funds account to the new key.
-    pub fn elders_changed(&mut self) -> Option<NodeOperation> {
-        let pub_key_set = self.network.public_key_set().ok()?;
+    pub async fn elders_changed(&mut self) -> Option<NodeOperation> {
+        let pub_key_set = self.network.public_key_set().await.ok()?;
         let keypair = utils::key_pair(self.network.clone()).ok()?;
         let actor = TransferActor::new(keypair, pub_key_set, Validator {});
         self.rewards.transition(actor)
     }
 
     // At section split, all Elders get their reward payout.
-    pub fn section_split(&mut self, prefix: Prefix) -> Option<NodeOperation> {
+    pub async fn section_split(&mut self, prefix: Prefix) -> Option<NodeOperation> {
         // First remove nodes that are no longer in our section.
         let to_remove = self
             .rewards
@@ -91,7 +90,7 @@ impl DataSection {
         self.rewards.remove(to_remove);
 
         // Then payout rewards to all the Elders.
-        let elders = self.network.our_elder_names();
+        let elders = self.network.our_elder_names().await;
         self.rewards.payout_rewards(elders)
     }
 
@@ -103,7 +102,7 @@ impl DataSection {
     /// When a relocated node joins, a DataSection
     /// has a few different things to do, such as
     /// pay out rewards and trigger chunk duplication.
-    pub fn relocated_node_joined(
+    pub async fn relocated_node_joined(
         &mut self,
         old_node_id: XorName,
         new_node_id: XorName,
@@ -115,17 +114,17 @@ impl DataSection {
             new_node_id,
             age,
         });
-        let second = self.metadata.trigger_chunk_duplication(new_node_id);
+        let second = self.metadata.trigger_chunk_duplication(new_node_id).await;
         Some(vec![first, second].into())
     }
 
     /// Name of the node
     /// Age of the node
-    pub fn member_left(&mut self, node_id: XorName, _age: u8) -> Option<NodeOperation> {
+    pub async fn member_left(&mut self, node_id: XorName, _age: u8) -> Option<NodeOperation> {
         // marks the reward account as
         // awaiting claiming of the counter
         let first = self.rewards.process(RewardDuty::DeactivateNode(node_id));
-        let second = self.metadata.trigger_chunk_duplication(node_id);
+        let second = self.metadata.trigger_chunk_duplication(node_id).await;
         Some(vec![first, second].into())
     }
 }

src/node/elder_duties/key_section/payment/mod.rs
@@ -17,11 +17,7 @@ use sn_data_types::{
     Cmd, CmdError, ElderDuties, Error, Message, Money, MsgEnvelope, PublicKey, Result,
     TransferError,
 };
-use std::{
-    cell::{RefCell, RefMut},
-    fmt::{self, Display, Formatter},
-    rc::Rc,
-};
+use std::fmt::{self, Display, Formatter};
 use std::sync::{Arc, Mutex};
 
 /// An Elder in a KeySection is responsible for
@@ -44,14 +40,14 @@ impl Payments {
     }
 
     // The code in this method is a bit messy, needs to be cleaned up.
-    pub fn process(&mut self, duty: &PaymentDuty) -> Option<NodeOperation> {
+    pub async fn process(&mut self, duty: &PaymentDuty) -> Option<NodeOperation> {
         use PaymentDuty::*;
         match duty {
-            ProcessPayment(msg) => self.process_payment(msg),
+            ProcessPayment(msg) => self.process_payment(msg).await,
         }
     }
 
-    fn process_payment(&mut self, msg: &MsgEnvelope) -> Option<NodeOperation> {
+    async fn process_payment(&mut self, msg: &MsgEnvelope) -> Option<NodeOperation> {
         let (payment, num_bytes) = match &msg.message {
             Message::Cmd {
                 cmd: Cmd::Data { payment, cmd },
@@ -109,7 +105,7 @@ impl Payments {
                 }
                 // consider having the section actor be
                 // informed of this transfer as well..
-                self.wrapping.forward(msg)
+                self.wrapping.forward(msg).await
             }
             Err(error) => self.wrapping.error(
                 CmdError::Transfer(TransferRegistration(error)),

src/node/mod.rs
@@ -107,20 +107,21 @@ impl<R: CryptoRng + Rng> Node<R> {
     }
 
     /// Returns our connection info.
-    pub fn our_connection_info(&mut self) -> Result<SocketAddr> {
-        self.network_api.our_connection_info().map_err(From::from)
+    pub async fn our_connection_info(&mut self) -> Result<SocketAddr> {
+        self.network_api.our_connection_info().await.map_err(From::from)
     }
 
     /// Returns whether routing node is in elder state.
-    pub fn is_elder(&mut self) -> bool {
-        self.network_api.is_elder()
+    pub async fn is_elder(&mut self) -> bool {
+        self.network_api.is_elder().await
     }
 
     /// Starts the node, and runs the main event loop.
     /// Blocks until the node is terminated, which is done
     /// by client sending in a `Command` to free it.
     pub async fn run(&mut self) -> Result<()> {
-        let info = self.network_api.our_connection_info().unwrap();
+        let mut event_stream = self.network_api.listen_events().await?;
+        let info = self.network_api.our_connection_info().await.unwrap();
         info!("Listening for routing events at: {}", info);
         while let Some(event) = self.network_events.next().await {
             info!("New event received from the Network: {:?}", event);
@@ -163,11 +164,11 @@ impl<R: CryptoRng + Rng> Node<R> {
         match duty {
             RunAsAdult(duty) => {
                 info!("Running as Adult: {:?}", duty);
-                self.duties.adult_duties()?.process(&duty)
+                self.duties.adult_duties()?.process(&duty).await
             }
             RunAsElder(duty) => {
                 info!("Running as Elder: {:?}", duty);
-                self.duties.elder_duties()?.process(duty)
+                self.duties.elder_duties()?.process(duty).await
             }
             RunAsNode(duty) => {
                 info!("Running as Node: {:?}", duty);

src/node/msg_wrapping.rs
@@ -51,8 +51,8 @@ impl NodeMsgWrapping {
         Self { inner }
     }
 
-    pub fn send(&self, message: Message) -> Option<MessagingDuty> {
-        self.inner.send(message)
+    pub async fn send(&self, message: Message) -> Option<MessagingDuty> {
+        self.inner.send(message).await
     }
 }
 
@@ -62,8 +62,8 @@ impl AdultMsgWrapping {
         Self { inner }
     }
 
-    pub fn send(&self, message: Message) -> Option<MessagingDuty> {
-        self.inner.send(message)
+    pub async fn send(&self, message: Message) -> Option<MessagingDuty> {
+        self.inner.send(message).await
     }
 
     pub fn error(
@@ -82,21 +82,21 @@ impl ElderMsgWrapping {
         Self { inner }
     }
 
-    pub fn forward(&self, msg: &MsgEnvelope) -> Option<MessagingDuty> {
-        let msg = self.inner.set_proxy(&msg);
+    pub async fn forward(&self, msg: &MsgEnvelope) -> Option<MessagingDuty> {
+        let msg = self.inner.set_proxy(&msg).await;
         Some(MessagingDuty::SendToSection(msg))
     }
 
-    pub fn send(&self, message: Message) -> Option<MessagingDuty> {
-        self.inner.send(message)
+    pub async fn send(&self, message: Message) -> Option<MessagingDuty> {
+        self.inner.send(message).await
     }
 
-    pub fn send_to_adults(
+    pub async fn send_to_adults(
         &self,
         targets: BTreeSet<XorName>,
         msg: &MsgEnvelope,
     ) -> Option<MessagingDuty> {
-        self.inner.send_to_adults(targets, msg)
+        self.inner.send_to_adults(targets, msg).await
     }
 
     pub fn error(
@@ -114,8 +114,8 @@ impl MsgWrapping {
         Self { keys, duty }
     }
 
-    pub fn send(&self, message: Message) -> Option<MessagingDuty> {
-        let origin = self.sign(&message);
+    pub async fn send(&self, message: Message) -> Option<MessagingDuty> {
+        let origin = self.sign(&message).await;
         let msg = MsgEnvelope {
             message,
             origin,
@@ -124,12 +124,12 @@ impl MsgWrapping {
         Some(MessagingDuty::SendToSection(msg))
     }
 
-    pub fn send_to_adults(
+    pub async fn send_to_adults(
         &self,
         targets: BTreeSet<XorName>,
         msg: &MsgEnvelope,
     ) -> Option<MessagingDuty> {
-        let msg = self.set_proxy(&msg);
+        let msg = self.set_proxy(&msg).await;
         Some(MessagingDuty::SendToAdults { targets, msg })
     }
 
@@ -148,17 +148,17 @@ impl MsgWrapping {
         })
     }
 
-    fn sign<T: Serialize>(&self, data: &T) -> MsgSender {
+    async fn sign<T: Serialize>(&self, data: &T) -> MsgSender {
         MsgSender::Node {
             duty: self.duty,
-            proof: self.keys.produce_proof(&utils::serialise(data)),
+            proof: self.keys.produce_proof(&utils::serialise(data)).await,
         }
     }
 
-    fn set_proxy(&self, msg: &MsgEnvelope) -> MsgEnvelope {
+    async fn set_proxy(&self, msg: &MsgEnvelope) -> MsgEnvelope {
         // origin signs the message, while proxies sign the envelope
         let mut msg = msg.clone();
-        msg.add_proxy(self.sign(&msg));
+        msg.add_proxy(self.sign(&msg).await);
 
         msg
     }

maidsafe/temp_safe_network

src/node/adult_duties/mod.rs
@@ -15,9 +15,7 @@ use crate::{
     Result,
 };
 use std::{
-    cell::Cell,
-    fmt::{self, Display, Formatter},
-    rc::Rc,
+    fmt::{self, Display, Formatter}
 };
 use std::sync::{Arc, Mutex};
 
@@ -33,12 +31,12 @@ impl AdultDuties {
         Ok(Self { chunks })
     }
 
-    pub fn process_adult_duty(&mut self, duty: &AdultDuty) -> Option<NodeOperation> {
+    pub async fn process_adult_duty(&mut self, duty: &AdultDuty) -> Option<NodeOperation> {
         use AdultDuty::*;
         use ChunkDuty::*;
         let RunAsChunks(chunk_duty) = duty;
         let result = match chunk_duty {
-            ReadChunk(msg) | WriteChunk(msg) => self.chunks.receive_msg(msg),
+            ReadChunk(msg) | WriteChunk(msg) => self.chunks.receive_msg(msg).await,
         };
 
         result.map(|c| c.into())

src/node/elder_duties/data_section/metadata/mod.rs
@@ -27,11 +27,7 @@ use elder_stores::ElderStores;
 use map_storage::MapStorage;
 use sequence_storage::SequenceStorage;
 use sn_data_types::{ElderDuties, Message, MsgEnvelope};
-use std::{
-    cell::Cell,
-    fmt::{self, Display, Formatter},
-    rc::Rc,
-};
+use std::fmt::{self, Display, Formatter};
 use std::sync::{Arc, Mutex};
 use xor_name::XorName;
 
@@ -71,16 +67,16 @@ impl Metadata {
         })
     }
 
-    pub fn process_metadata_duty(&mut self, duty: MetadataDuty) -> Option<NodeOperation> {
+    pub async fn process_metadata_duty(&mut self, duty: MetadataDuty) -> Option<NodeOperation> {
         use MetadataDuty::*;
         match duty {
-            ProcessRead(msg) | ProcessWrite(msg) => self.process_msg(msg),
+            ProcessRead(msg) | ProcessWrite(msg) => self.process_msg(msg).await,
         }
     }
 
-    fn process_msg(&mut self, msg: MsgEnvelope) -> Option<NodeOperation> {
+    async fn process_msg(&mut self, msg: MsgEnvelope) -> Option<NodeOperation> {
         match &msg.message {
-            Message::Cmd { .. } => writing::get_result(msg, &mut self.elder_stores),
+            Message::Cmd { .. } => writing::get_result(msg, &mut self.elder_stores).await,
             Message::Query { .. } => reading::get_result(msg, &self.elder_stores).map(|c| c.into()),
             _ => None, // only Queries and Cmds from client is handled at Metadata
         }
@@ -89,8 +85,8 @@ impl Metadata {
     // This should be called whenever a node leaves the section. It fetches the list of data that was
     // previously held by the node and requests the other holders to store an additional copy.
     // The list of holders is also updated by removing the node that left.
-    pub fn trigger_chunk_duplication(&mut self, node: XorName) -> Option<NodeOperation> {
-        self.elder_stores.blob_register_mut().duplicate_chunks(node)
+    pub async fn trigger_chunk_duplication(&mut self, node: XorName) -> Option<NodeOperation> {
+        self.elder_stores.blob_register_mut().duplicate_chunks(node).await
     }
 
     // trace!(

src/node/elder_duties/data_section/mod.rs
@@ -22,7 +22,6 @@ use crate::{
 };
 use sn_routing::Prefix;
 use sn_transfers::TransferActor;
-use std::{cell::Cell, rc::Rc};
 use xor_name::XorName;
 use std::sync::{Arc, Mutex};
 
@@ -42,7 +41,7 @@ pub struct DataSection {
 
 impl DataSection {
     ///
-    pub fn new(
+    pub async fn new(
         info: &NodeInfo,
         dbs: ChunkHolderDbs,
         total_used_space: &Arc<Mutex<u64>>,
@@ -53,7 +52,7 @@ impl DataSection {
 
         // Rewards
         let keypair = utils::key_pair(network.clone())?;
-        let public_key_set = network.public_key_set()?;
+        let public_key_set = network.public_key_set().await?;
         let actor = TransferActor::new(keypair, public_key_set, Validator {});
         let reward_calc = RewardCalc::new(network.clone());
         let rewards = Rewards::new(info.keys.clone(), actor, reward_calc);
@@ -65,24 +64,24 @@ impl DataSection {
         })
     }
 
-    pub fn process_data_section_duty(&mut self, duty: DataSectionDuty) -> Option<NodeOperation> {
+    pub async fn process_data_section_duty(&mut self, duty: DataSectionDuty) -> Option<NodeOperation> {
         use DataSectionDuty::*;
         match duty {
-            RunAsMetadata(duty) => self.metadata.process_metadata_duty(duty),
+            RunAsMetadata(duty) => self.metadata.process_metadata_duty(duty).await,
             RunAsRewards(duty) => self.rewards.process_reward_duty(duty),
         }
     }
 
     // Transition the section funds account to the new key.
-    pub fn elders_changed(&mut self) -> Option<NodeOperation> {
-        let pub_key_set = self.network.public_key_set().ok()?;
+    pub async fn elders_changed(&mut self) -> Option<NodeOperation> {
+        let pub_key_set = self.network.public_key_set().await.ok()?;
         let keypair = utils::key_pair(self.network.clone()).ok()?;
         let actor = TransferActor::new(keypair, pub_key_set, Validator {});
         self.rewards.transition(actor)
     }
 
     // At section split, all Elders get their reward payout.
-    pub fn section_split(&mut self, prefix: Prefix) -> Option<NodeOperation> {
+    pub async fn section_split(&mut self, prefix: Prefix) -> Option<NodeOperation> {
         // First remove nodes that are no longer in our section.
         let to_remove = self
             .rewards
@@ -93,7 +92,7 @@ impl DataSection {
         self.rewards.remove(to_remove);
 
         // Then payout rewards to all the Elders.
-        let elders = self.network.our_elder_names();
+        let elders = self.network.our_elder_names().await;
         self.rewards.payout_rewards(elders)
     }
 
@@ -105,7 +104,7 @@ impl DataSection {
     /// When a relocated node joins, a DataSection
     /// has a few different things to do, such as
     /// pay out rewards and trigger chunk duplication.
-    pub fn relocated_node_joined(
+    pub async fn relocated_node_joined(
         &mut self,
         old_node_id: XorName,
         new_node_id: XorName,
@@ -119,19 +118,19 @@ impl DataSection {
                 new_node_id,
                 age,
             });
-        let second = self.metadata.trigger_chunk_duplication(new_node_id);
+        let second = self.metadata.trigger_chunk_duplication(new_node_id).await;
         Some(vec![first, second].into())
     }
 
     /// Name of the node
     /// Age of the node
-    pub fn member_left(&mut self, node_id: XorName, _age: u8) -> Option<NodeOperation> {
+    pub async fn member_left(&mut self, node_id: XorName, _age: u8) -> Option<NodeOperation> {
         // marks the reward account as
         // awaiting claiming of the counter
         let first = self
             .rewards
             .process_reward_duty(RewardDuty::DeactivateNode(node_id));
-        let second = self.metadata.trigger_chunk_duplication(node_id);
+        let second = self.metadata.trigger_chunk_duplication(node_id).await;
         Some(vec![first, second].into())
     }
 }

src/node/elder_duties/key_section/payment/mod.rs
@@ -17,11 +17,7 @@ use crate::{
 use sn_data_types::{
     Cmd, CmdError, ElderDuties, Error, Message, MsgEnvelope, PublicKey, Result, TransferError,
 };
-use std::{
-    cell::{RefCell, RefMut},
-    fmt::{self, Display, Formatter},
-    rc::Rc,
-};
+use std::fmt::{self, Display, Formatter};
 use std::sync::{Arc, Mutex};
 
 /// An Elder in a KeySection is responsible for
@@ -53,14 +49,14 @@ impl Payments {
     }
 
     // The code in this method is a bit messy, needs to be cleaned up.
-    pub fn process_payment_duty(&mut self, duty: &PaymentDuty) -> Option<NodeOperation> {
+    pub async fn process_payment_duty(&mut self, duty: &PaymentDuty) -> Option<NodeOperation> {
         use PaymentDuty::*;
         match duty {
-            ProcessPayment(msg) => self.process_payment(msg),
+            ProcessPayment(msg) => self.process_payment(msg).await,
         }
     }
 
-    fn process_payment(&mut self, msg: &MsgEnvelope) -> Option<NodeOperation> {
+    async fn process_payment(&mut self, msg: &MsgEnvelope) -> Option<NodeOperation> {
         let (payment, num_bytes) = match &msg.message {
             Message::Cmd {
                 cmd: Cmd::Data { payment, cmd },
@@ -118,7 +114,7 @@ impl Payments {
                 }
                 // consider having the section actor be
                 // informed of this transfer as well..
-                self.wrapping.forward(msg)
+                self.wrapping.forward(msg).await
             }
             Err(error) => self.wrapping.error(
                 CmdError::Transfer(TransferRegistration(error)),

src/node/mod.rs
@@ -115,20 +115,21 @@ impl<R: CryptoRng + Rng> Node<R> {
     }
 
     /// Returns our connection info.
-    pub fn our_connection_info(&mut self) -> Result<SocketAddr> {
-        self.network_api.our_connection_info().map_err(From::from)
+    pub async fn our_connection_info(&mut self) -> Result<SocketAddr> {
+        self.network_api.our_connection_info().await.map_err(From::from)
     }
 
     /// Returns whether routing node is in elder state.
-    pub fn is_elder(&mut self) -> bool {
-        self.network_api.is_elder()
+    pub async fn is_elder(&mut self) -> bool {
+        self.network_api.is_elder().await
     }
 
     /// Starts the node, and runs the main event loop.
     /// Blocks until the node is terminated, which is done
     /// by client sending in a `Command` to free it.
     pub async fn run(&mut self) -> Result<()> {
-        let info = self.network_api.our_connection_info().unwrap();
+        let mut event_stream = self.network_api.listen_events().await?;
+        let info = self.network_api.our_connection_info().await.unwrap();
         info!("Listening for routing events at: {}", info);
         while let Some(event) = self.network_events.next().await {
             info!("New event received from the Network: {:?}", event);
@@ -171,11 +172,11 @@ impl<R: CryptoRng + Rng> Node<R> {
         match duty {
             RunAsAdult(duty) => {
                 info!("Running as Adult: {:?}", duty);
-                self.duties.adult_duties()?.process_adult_duty(&duty)
+                self.duties.adult_duties()?.process_adult_duty(&duty).await
             }
             RunAsElder(duty) => {
                 info!("Running as Elder: {:?}", duty);
-                self.duties.elder_duties()?.process_elder_duty(duty)
+                self.duties.elder_duties()?.process_elder_duty(duty).await
             }
             RunAsNode(duty) => {
                 info!("Running as Node: {:?}", duty);

src/node/msg_wrapping.rs
@@ -51,7 +51,7 @@ impl NodeMsgWrapping {
         Self { inner }
     }
 
-    pub fn send(&self, message: Message) -> Option<NodeMessagingDuty> {
+    pub async fn send(&self, message: Message) -> Option<NodeMessagingDuty> {
         self.inner.send(message)
     }
 }
@@ -62,8 +62,8 @@ impl AdultMsgWrapping {
         Self { inner }
     }
 
-    pub fn send(&self, message: Message) -> Option<NodeMessagingDuty> {
-        self.inner.send(message)
+    pub async fn send(&self, message: Message) -> Option<NodeMessagingDuty> {
+        self.inner.send(message).await
     }
 
     pub fn error(
@@ -83,20 +83,20 @@ impl ElderMsgWrapping {
     }
 
     pub fn forward(&self, msg: &MsgEnvelope) -> Option<NodeMessagingDuty> {
-        let msg = self.inner.set_proxy(&msg);
+        let msg = self.inner.set_proxy(&msg).await;
         Some(NodeMessagingDuty::SendToSection(msg))
     }
 
-    pub fn send(&self, message: Message) -> Option<NodeMessagingDuty> {
-        self.inner.send(message)
+    pub async fn send(&self, message: Message) -> Option<NodeMessagingDuty> {
+        self.inner.send(message).await
     }
 
-    pub fn send_to_adults(
+    pub async fn send_to_adults(
         &self,
         targets: BTreeSet<XorName>,
         msg: &MsgEnvelope,
     ) -> Option<NodeMessagingDuty> {
-        self.inner.send_to_adults(targets, msg)
+        self.inner.send_to_adults(targets, msg).await
     }
 
     pub fn error(
@@ -114,8 +114,8 @@ impl MsgWrapping {
         Self { keys, duty }
     }
 
-    pub fn send(&self, message: Message) -> Option<NodeMessagingDuty> {
-        let origin = self.sign(&message);
+    pub async fn send(&self, message: Message) -> Option<NodeMessagingDuty> {
+        let origin = self.sign(&message).await;
         let msg = MsgEnvelope {
             message,
             origin,
@@ -124,13 +124,13 @@ impl MsgWrapping {
         Some(NodeMessagingDuty::SendToSection(msg))
     }
 
-    pub fn send_to_adults(
+    pub async fn send_to_adults(
         &self,
         targets: BTreeSet<XorName>,
         msg: &MsgEnvelope,
     ) -> Option<NodeMessagingDuty> {
-        let msg = self.set_proxy(&msg);
-        Some(NodeMessagingDuty::SendToAdults { targets, msg })
+        let msg = self.set_proxy(&msg).await;
+        Some(MessagingDuty::SendToAdults { targets, msg })
     }
 
     pub fn error(
@@ -148,17 +148,17 @@ impl MsgWrapping {
         })
     }
 
-    fn sign<T: Serialize>(&self, data: &T) -> MsgSender {
+    async fn sign<T: Serialize>(&self, data: &T) -> MsgSender {
         MsgSender::Node {
             duty: self.duty,
-            proof: self.keys.produce_proof(&utils::serialise(data)),
+            proof: self.keys.produce_proof(&utils::serialise(data)).await,
         }
     }
 
-    fn set_proxy(&self, msg: &MsgEnvelope) -> MsgEnvelope {
+    async fn set_proxy(&self, msg: &MsgEnvelope) -> MsgEnvelope {
         // origin signs the message, while proxies sign the envelope
         let mut msg = msg.clone();
-        msg.add_proxy(self.sign(&msg));
+        msg.add_proxy(self.sign(&msg).await);
 
         msg
     }