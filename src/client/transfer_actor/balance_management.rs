ad67e08ebdb981c9558d6b37c39503641defdbd0
Yoga07 <yogeshwar_1997@hotmail.com>
2020-10-09 01:20:11 +0530

    fix(tests): fix actor and Money transfer tests

--- src/client/transfer_actor/balance_management.rs
+++ src/client/transfer_actor/balance_management.rs
@@ -248,9 +248,11 @@ mod tests {
             .send_money(*full_id2.public_key(), Money::from_str("1")?)
             .await?;
 
-        // initial 10 on creation from farming simulation minus 1
+        // Initial 10 Money on creation from farming simulation minus 1
+        // Assert locally
         assert_eq!(client.get_local_balance().await, Money::from_str("9")?);
 
+        // Fetch balance from network and assert the same.
         assert_eq!(
             client.get_balance_from_network(None).await?,
             Money::from_str("9")?
@@ -260,8 +262,15 @@ mod tests {
             .send_money(*full_id2.public_key(), Money::from_str("2")?)
             .await?;
 
-        // initial 10 on creation from farming simulation minus 3
+        // Initial 10 on creation from farming simulation minus 3
         assert_eq!(client.get_local_balance().await, Money::from_str("7")?);
+
+        // Fetch balance from network and assert the same.
+        assert_eq!(
+            client.get_balance_from_network(None).await?,
+            Money::from_str("7")?
+        );
+
         Ok(())
     }
 
@@ -277,9 +286,7 @@ mod tests {
             .send_money(*full_id2.public_key(), Money::from_str("0")?)
             .await?;
 
-        println!("res to send 0: {:?}", res);
-
-        // initial 10 on creation from farming simulation minus 1
+        // Initial 10 Money on creation from farming simulation minus 1.
         assert_eq!(client.get_local_balance().await, Money::from_str("10")?);
 
         assert_eq!(client.get_balance().await?, Money::from_str("10")?);
@@ -287,12 +294,11 @@ mod tests {
         Ok(())
     }
 
-    // 1. Create a client A and allocate some test safecoin to it.
+    // 1. Create a client A and allocate 100 Money to it. (Clients start with 10 Money by default on simulated-farming)
     // 2. Get the balance and verify it.
-    // 3. Create another client B with a wallet holding some safecoin.
-    // 4. Transfer some money from client B to client A and verify the new balance.
+    // 3. Create another client B with a wallet holding 10 Money on start.
+    // 4. Transfer 5 Money from client A to client B and verify the new balance.
     // 5. Try to do a coin transfer without enough funds, it should return `InsufficientBalance`
-    // 6. Try to do a coin transfer with the amount set to 0, it should return `InvalidOperation`
     #[tokio::test]
     #[cfg(feature = "simulated-payouts")]
     pub async fn balance_transfers_between_clients() -> Result<(), ClientError> {
@@ -307,30 +313,36 @@ mod tests {
 
         let balance = client.get_balance().await?;
         assert_eq!(balance, Money::from_str("110")?); // 10 coins added automatically w/ farming sim on client init.
-        let init_bal = Money::from_str("10")?;
+
         let orig_balance = client.get_balance().await?;
         let _ = client.send_money(wallet1, Money::from_str("5.0")?).await?;
+
+        // Assert sender is debited.
         let new_balance = client.get_balance().await?;
         assert_eq!(
             new_balance,
-            orig_balance
-                .checked_sub(Money::from_str("5.0")?)
-                .ok_or_else(|| ClientError::from("Invalid checked sub in test"))?,
+            calculate_new_balance(balance, None, Some(Money::from_str("5.0")?))
         );
 
+        // Assert that the receiver has been credited.
+        let mut receiving_bal = receiving_client.get_balance().await?;
+        assert_eq!(receiving_bal, Money::from_str("15.0")?);
+
+        // Try transferring money exceeding our balance.
         let res = client.send_money(wallet1, Money::from_str("5000")?).await;
         match res {
             Err(ClientError::DataError(SndError::InsufficientBalance)) => (),
             res => panic!("Unexpected result: {:?}", res),
         };
-        // Check if money is refunded
-        let balance = client.get_balance().await?;
-        let receiving_balance = receiving_client.get_balance().await?;
 
-        let expected = calculate_new_balance(init_bal, Some(1), Some(Money::from_str("5")?));
+        // Assert if money is refunded.
+        let balance = client.get_balance().await?;
+        let expected = calculate_new_balance(orig_balance, None, Some(Money::from_str("5")?));
         assert_eq!(balance, expected);
 
-        assert_eq!(receiving_balance, Money::from_str("5015")?); // 500 + 5 + initial 10
+        // Assert no money is credited accidentally.
+        receiving_bal = receiving_client.get_balance().await?;
+        assert_eq!(receiving_bal, Money::from_str("15")?);
 
         Ok(())
     }
