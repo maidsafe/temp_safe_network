7a263795f7a5cb49690aebbefadc3aa739dd1e45
Josh Wilson <joshuef@gmail.com>
2020-11-30 17:53:15 +0100

    chore: wrap client tests in feature flag

--- src/tests/client.rs
+++ src/tests/client.rs
@@ -6,6 +6,8 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
+// ------------- Client Tests -----------------
+// --------------------------------------------
 use super::Network;
 use sn_client::client::{
     exported_tests as client_tests, map_apis::exported_tests as map_tests,
@@ -26,181 +28,182 @@ fn start_network() {
 }
 
 // --------------------------------------------
-// ------------- Client Tests -----------------
-// --------------------------------------------
+#[cfg(feature = "client-tests")]
+mod test {
+    use super::*;
+    #[tokio::test]
+    async fn client_creation() {
+        start_network();
+        assert!(client_tests::client_creation().await.is_ok());
+    }
 
-#[tokio::test]
-async fn client_creation() {
-    start_network();
-    assert!(client_tests::client_creation().await.is_ok());
-}
-
-#[tokio::test]
-async fn client_creation_for_existing_sk() {
-    start_network();
-    assert!(client_tests::client_creation_with_existing_keypair()
-        .await
-        .is_ok());
-}
-
-// --------------------------------------------
-// --------- Transfer Actor Tests -------------
-// --------------------------------------------
-
-#[tokio::test]
-async fn transfer_actor_creation_hydration_for_nonexistant_balance() {
-    start_network();
-    assert!(
-        transfer_actor_tests::transfer_actor_creation_hydration_for_nonexistant_balance()
+    #[tokio::test]
+    async fn client_creation_for_existing_sk() {
+        start_network();
+        assert!(client_tests::client_creation_with_existing_keypair()
             .await
-            .is_ok()
-    );
-}
+            .is_ok());
+    }
 
-#[tokio::test]
-async fn transfer_actor_creation_hydration_for_existing_balance() {
-    start_network();
-    assert!(
-        transfer_actor_tests::transfer_actor_creation_hydration_for_existing_balance()
+    // --------------------------------------------
+    // --------- Transfer Actor Tests -------------
+    // --------------------------------------------
+
+    #[tokio::test]
+    async fn transfer_actor_creation_hydration_for_nonexistant_balance() {
+        start_network();
+        assert!(
+            transfer_actor_tests::transfer_actor_creation_hydration_for_nonexistant_balance()
+                .await
+                .is_ok()
+        );
+    }
+
+    #[tokio::test]
+    async fn transfer_actor_creation_hydration_for_existing_balance() {
+        start_network();
+        assert!(
+            transfer_actor_tests::transfer_actor_creation_hydration_for_existing_balance()
+                .await
+                .is_ok()
+        );
+    }
+
+    // --------------------------------------------
+    // ------------ Transfer Tests ----------------
+    // --------------------------------------------
+
+    #[tokio::test]
+    async fn cannot_write_with_insufficient_balance() {
+        start_network();
+        assert!(transfer_tests::cannot_write_with_insufficient_balance()
             .await
-            .is_ok()
-    );
-}
+            .is_ok());
+    }
 
-// --------------------------------------------
-// ------------ Transfer Tests ----------------
-// --------------------------------------------
-
-#[tokio::test]
-async fn cannot_write_with_insufficient_balance() {
-    start_network();
-    assert!(transfer_tests::cannot_write_with_insufficient_balance()
-        .await
-        .is_ok());
-}
-
-#[tokio::test]
-async fn insufficient_balance_transfers() {
-    start_network();
-    assert!(transfer_tests::insufficient_balance_transfers()
-        .await
-        .is_ok());
-}
-
-#[tokio::test]
-async fn transfer_actor_cannot_send_0_money_req() {
-    start_network();
-    assert!(transfer_tests::transfer_actor_cannot_send_0_money_req()
-        .await
-        .is_ok());
-}
-
-#[tokio::test]
-async fn transfer_actor_can_send_several_transfers_and_thats_reflected_locally() {
-    start_network();
-    assert!(
-        transfer_tests::transfer_actor_can_send_several_transfers_and_thats_reflected_locally()
+    #[tokio::test]
+    async fn insufficient_balance_transfers() {
+        start_network();
+        assert!(transfer_tests::insufficient_balance_transfers()
             .await
-            .is_ok()
-    );
-}
+            .is_ok());
+    }
 
-#[tokio::test]
-async fn transfer_actor_can_send_money_and_thats_reflected_locally() {
-    start_network();
-    assert!(
-        transfer_tests::transfer_actor_can_send_money_and_thats_reflected_locally()
+    #[tokio::test]
+    async fn transfer_actor_cannot_send_0_money_req() {
+        start_network();
+        assert!(transfer_tests::transfer_actor_cannot_send_0_money_req()
             .await
-            .is_ok()
-    );
-}
+            .is_ok());
+    }
 
-#[tokio::test]
-async fn balance_transfers_between_clients() {
-    start_network();
-    assert!(transfer_tests::balance_transfers_between_clients()
-        .await
-        .is_ok());
-}
+    #[tokio::test]
+    async fn transfer_actor_can_send_several_transfers_and_thats_reflected_locally() {
+        start_network();
+        assert!(
+            transfer_tests::transfer_actor_can_send_several_transfers_and_thats_reflected_locally()
+                .await
+                .is_ok()
+        );
+    }
 
-// --------------------------------------------
-// ---------- Sequence Data Tests -------------
-// --------------------------------------------
+    #[tokio::test]
+    async fn transfer_actor_can_send_money_and_thats_reflected_locally() {
+        start_network();
+        assert!(
+            transfer_tests::transfer_actor_can_send_money_and_thats_reflected_locally()
+                .await
+                .is_ok()
+        );
+    }
 
-#[tokio::test]
-async fn append_to_sequence_test() {
-    start_network();
-    assert!(sequence_tests::append_to_sequence_test().await.is_ok());
-}
-
-#[tokio::test]
-async fn sequence_basics_test() {
-    start_network();
-    assert!(sequence_tests::sequence_basics_test().await.is_ok());
-}
-
-#[tokio::test]
-async fn sequence_cannot_delete_public_test() {
-    start_network();
-    assert!(sequence_tests::sequence_cannot_delete_public_test()
-        .await
-        .is_ok());
-}
-
-#[tokio::test]
-async fn sequence_deletions_should_cost_put_price() {
-    start_network();
-    assert!(sequence_tests::sequence_deletions_should_cost_put_price()
-        .await
-        .is_ok());
-}
-
-#[tokio::test]
-async fn sequence_private_permissions_test() {
-    start_network();
-    assert!(sequence_tests::sequence_private_permissions_test()
-        .await
-        .is_ok());
-}
-
-#[tokio::test]
-async fn sequence_pub_permissions_test() {
-    start_network();
-    assert!(sequence_tests::sequence_pub_permissions_test()
-        .await
-        .is_ok());
-}
-
-// --------------------------------------------
-// ------------ Map Data Tests ----------------
-// --------------------------------------------
-
-#[tokio::test]
-async fn del_seq_map_test() {
-    start_network();
-    assert!(map_tests::del_seq_map_test().await.is_ok());
-}
-
-#[tokio::test]
-async fn map_cannot_initially_put_data_with_another_owner_than_current_client() {
-    start_network();
-    assert!(
-        map_tests::map_cannot_initially_put_data_with_another_owner_than_current_client()
+    #[tokio::test]
+    async fn balance_transfers_between_clients() {
+        start_network();
+        assert!(transfer_tests::balance_transfers_between_clients()
             .await
-            .is_ok()
-    );
-}
+            .is_ok());
+    }
 
-#[tokio::test]
-async fn del_unseq_map_test() {
-    start_network();
-    assert!(map_tests::del_unseq_map_test().await.is_ok());
-}
+    // --------------------------------------------
+    // ---------- Sequence Data Tests -------------
+    // --------------------------------------------
 
-#[tokio::test]
-async fn map_deletions_should_cost_put_price() {
-    start_network();
-    assert!(map_tests::map_deletions_should_cost_put_price()
-        .await
-        .is_ok());
+    #[tokio::test]
+    async fn append_to_sequence_test() {
+        start_network();
+        assert!(sequence_tests::append_to_sequence_test().await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn sequence_basics_test() {
+        start_network();
+        assert!(sequence_tests::sequence_basics_test().await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn sequence_cannot_delete_public_test() {
+        start_network();
+        assert!(sequence_tests::sequence_cannot_delete_public_test()
+            .await
+            .is_ok());
+    }
+
+    #[tokio::test]
+    async fn sequence_deletions_should_cost_put_price() {
+        start_network();
+        assert!(sequence_tests::sequence_deletions_should_cost_put_price()
+            .await
+            .is_ok());
+    }
+
+    #[tokio::test]
+    async fn sequence_private_permissions_test() {
+        start_network();
+        assert!(sequence_tests::sequence_private_permissions_test()
+            .await
+            .is_ok());
+    }
+
+    #[tokio::test]
+    async fn sequence_pub_permissions_test() {
+        start_network();
+        assert!(sequence_tests::sequence_pub_permissions_test()
+            .await
+            .is_ok());
+    }
+
+    // --------------------------------------------
+    // ------------ Map Data Tests ----------------
+    // --------------------------------------------
+
+    #[tokio::test]
+    async fn del_seq_map_test() {
+        start_network();
+        assert!(map_tests::del_seq_map_test().await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn map_cannot_initially_put_data_with_another_owner_than_current_client() {
+        start_network();
+        assert!(
+            map_tests::map_cannot_initially_put_data_with_another_owner_than_current_client()
+                .await
+                .is_ok()
+        );
+    }
+
+    #[tokio::test]
+    async fn del_unseq_map_test() {
+        start_network();
+        assert!(map_tests::del_unseq_map_test().await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn map_deletions_should_cost_put_price() {
+        start_network();
+        assert!(map_tests::map_deletions_should_cost_put_price()
+            .await
+            .is_ok());
+    }
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -90,7 +90,7 @@ impl Network {
                 .unwrap();
             nodes.push((command_tx, handle));
         }
-        thread::sleep(std::time::Duration::from_secs(80));
+        thread::sleep(std::time::Duration::from_secs(30));
         Self { nodes }
     }
 }
