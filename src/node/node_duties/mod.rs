318eb0ade5810bc03579805429661fa4680c16c8
oetyng <oetyng@gmail.com>
2021-01-26 11:33:16 +0100

    merge fix

--- src/node/node_duties/mod.rs
+++ src/node/node_duties/mod.rs
@@ -6,8 +6,8 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-pub mod messaging;
 mod elder_constellation;
+pub mod messaging;
 mod msg_analysis;
 mod network_events;
 
