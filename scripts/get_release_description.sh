a38e9b5108d59c35a6674bdf47db72a7792fbbc1
Stephen Coyle <stephen.coyle@maidsafe.net>
2021-01-28 11:31:11 +0000

    chore(cd): only generate 1 tag per deployment
    The CD pipeline has inadvertently been generating 2 tags for every deployment `x.x.x` and `vx.x.x` - we only need the `vx.x.x` so reworked the release process to only generate this.

--- scripts/get_release_description.sh
+++ scripts/get_release_description.sh
@@ -10,6 +10,10 @@ fi
 read -r -d '' release_description << 'EOF'
 Implements a Safe Network Node.
 
+## Changelog:
+CHANGELOG_TEXT
+
+
 ## SHA-256 checksums for release versions:
 ```
 Linux
@@ -30,6 +34,8 @@ tar.gz: TAR_WIN_CHECKSUM
 * [Safe Browser](https://github.com/maidsafe/sn_browser/releases/)
 EOF
 
+# To include the changelog, which has newlines `\n`, we need to use sed to replace the newlines with `\\n`
+changelog_text=$(awk '/# \[/{c++;p=1}{if(c==2){exit}}p;' CHANGELOG.md | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n/g')
 zip_linux_checksum=$(sha256sum \
     "./deploy/prod/sn_node-$version-x86_64-unknown-linux-musl.zip" | \
     awk '{ print $1 }')
@@ -49,10 +55,12 @@ tar_win_checksum=$(sha256sum \
     "./deploy/prod/sn_node-$version-x86_64-pc-windows-msvc.tar.gz" | \
     awk '{ print $1 }')
 
-release_description=$(sed "s/ZIP_LINUX_CHECKSUM/$zip_linux_checksum/g" <<< "$release_description")
-release_description=$(sed "s/ZIP_MACOS_CHECKSUM/$zip_macos_checksum/g" <<< "$release_description")
-release_description=$(sed "s/ZIP_WIN_CHECKSUM/$zip_win_checksum/g" <<< "$release_description")
-release_description=$(sed "s/TAR_LINUX_CHECKSUM/$tar_linux_checksum/g" <<< "$release_description")
-release_description=$(sed "s/TAR_MACOS_CHECKSUM/$tar_macos_checksum/g" <<< "$release_description")
-release_description=$(sed "s/TAR_WIN_CHECKSUM/$tar_win_checksum/g" <<< "$release_description")
+# Need to use something like `=` instead of the usual `\` in the commands below because the changelog has `\` characters`
+release_description=$(sed "s=CHANGELOG_TEXT=$changelog_text=g" <<< "$release_description")
+release_description=$(sed "s=ZIP_LINUX_CHECKSUM=$zip_linux_checksum=g" <<< "$release_description")
+release_description=$(sed "s=ZIP_MACOS_CHECKSUM=$zip_macos_checksum=g" <<< "$release_description")
+release_description=$(sed "s=ZIP_WIN_CHECKSUM=$zip_win_checksum=g" <<< "$release_description")
+release_description=$(sed "s=TAR_LINUX_CHECKSUM=$tar_linux_checksum=g" <<< "$release_description")
+release_description=$(sed "s=TAR_MACOS_CHECKSUM=$tar_macos_checksum=g" <<< "$release_description")
+release_description=$(sed "s=TAR_WIN_CHECKSUM=$tar_win_checksum=g" <<< "$release_description")
 echo "$release_description"
