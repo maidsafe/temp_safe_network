27726eeb063500b48116d680659434429771045d
oetyng <oetyng@gmail.com>
2021-02-21 01:51:23 +0100

    fix: set response elected flag after electing best of the rest

--- src/connection_manager.rs
+++ src/connection_manager.rs
@@ -347,6 +347,7 @@ impl ConnectionManager {
                     received_errors,
                     &mut has_elected_a_response,
                 );
+                has_elected_a_response = true;
             }
         }
 
