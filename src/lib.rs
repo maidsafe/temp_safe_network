92b446da714367dd4f9fd7e70afb146c4e23cbd3
Josh Wilson <joshuef@gmail.com>
2021-06-14 16:41:04 +0200

    feat: Rerorg to monorepo and single crate

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+// Copyright 2021 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+
+//! Implementation of the "Node" node for the SAFE Network.
+
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
+    html_favicon_url = "https://maidsafe.net/img/favicon.ico",
+    test(attr(forbid(warnings)))
+)]
+// For explanation of lint checks, run `rustc -W help`.
+#![warn(
+    // TODO: add missing debug implementations for structs?
+    // missing_debug_implementations,
+    unsafe_code,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+)]
+// For quick_error
+#![recursion_limit = "256"]
+
+mod messaging;
+mod routing;
+pub mod node;
+pub mod client;
+
