d8df40ae8a76c6e4d15b579492ab03c707ac9a79
Josh Wilson <joshuef@gmail.com>
2021-07-25 06:44:38 +0200

    tests: fix config smoke test

--- src/node/config_handler.rs
+++ src/node/config_handler.rs
@@ -453,7 +453,7 @@ fn smoke() {
     // NOTE: IF this value is being changed due to a change in the config,
     // the change in config also be handled in Config::merge()
     // and in examples/config_handling.rs
-    let expected_size = 496;
+    let expected_size = 504;
 
     assert_eq!(std::mem::size_of::<Config>(), expected_size);
 }
