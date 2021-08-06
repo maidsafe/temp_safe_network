eaf498b23469c6d9174c458e1437ec0a0c200d8c
Chris Connelly <chris@connec.co.uk>
2021-08-06 10:56:32 +0100

    chore: Remove commented code

--- src/node/metadata/elder_stores.rs
+++ src/node/metadata/elder_stores.rs
@@ -6,7 +6,6 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-// use super::chunk_records::ChunkRecords;
 use crate::messaging::data::DataExchange;
 use crate::node::{network::Network, Error, Result};
 use crate::routing::Prefix;
