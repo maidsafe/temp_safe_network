cbfe5450e4a84b6970050903799c3def64c3d182
bochaco <gabrielviganotti@gmail.com>
2021-10-22 12:04:42 -0300

    fix(scripts): change regex in has_split.sh to properly match prefix logs

--- scripts/has_split.sh
+++ scripts/has_split.sh
@@ -9,12 +9,20 @@ fi
 echo "Checking logfiles to check for split"
 log_dir="$HOME/.safe/node/local-test-network"
 # -u needed here to search log dirs
-prefix_count=$(rg "Prefix\(0\)" "$log_dir" -u | wc -l)
+prefix_empty_count=$(rg "prefix=\(\)" "$log_dir" -u | wc -l)
+prefix_0_count=$(rg "prefix=\(0\)" "$log_dir" -u | wc -l)
+prefix_1_count=$(rg "prefix=\(1\)" "$log_dir" -u | wc -l)
 
-if ! [ $prefix_count -gt 0 ]
+echo "Prefix() found $prefix_empty_count times."
+echo "Prefix(0) found $prefix_0_count times."
+echo "Prefix(1) found $prefix_1_count times."
+
+if ! [[ $prefix_0_count -gt 0 && $prefix_1_count -gt 0 ]]
     then
         echo "No split, try changing NODE_COUNT!"
         exit 100
+    else
+	echo "Successful split!"
 fi
 
-echo "Prefix(0) found $prefix_count times. Successful split!"
+
