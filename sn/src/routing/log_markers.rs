c67beeb08cf0c93d7519c1e8f55007d2a5546c40
Josh Wilson <joshuef@gmail.com>
2021-10-24 13:28:17 +0200

    chore: tweak join retry backoff timing

--- sn/src/routing/log_markers.rs
+++ sn/src/routing/log_markers.rs
@@ -48,6 +48,7 @@ pub enum LogMarker {
     SendResourceProofChallenge,
     SendJoinAsRelocatedResponse,
     SendJoinRejected,
+    ReceivedJoinApproved,
     SendJoinRetryNotCorrectKey,
     SendJoinRetryFirstSectionAgeIssue,
     SendJoinRetryNotAdult,
