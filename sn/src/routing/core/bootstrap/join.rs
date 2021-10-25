ba7c4c609150839825a4992c08e3bdd00b698269
Josh Wilson <joshuef@gmail.com>
2021-10-25 08:37:58 +0200

    fix: get more splits (at the cost of more join msgs)

--- sn/src/routing/core/bootstrap/join.rs
+++ sn/src/routing/core/bootstrap/join.rs
@@ -130,8 +130,8 @@ impl<'a> Join<'a> {
         // use some backoff here as we try and join
         let mut our_backoff = ExponentialBackoff {
             initial_interval: Duration::from_millis(50),
-            max_interval: Duration::from_secs(3),
-            max_elapsed_time: Some(Duration::from_secs(180)),
+            max_interval: Duration::from_secs(1),
+            max_elapsed_time: Some(Duration::from_secs(20)),
             ..Default::default()
         };
 
