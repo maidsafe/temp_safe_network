696fd28a0b3d8d2e59c30c129c0ad7597b7326e4
bochaco <gabrielviganotti@gmail.com>
2021-10-25 11:23:04 -0300

    chore(CI): additional info to be reported by has_split script

--- resources/scripts/has_split.sh
+++ resources/scripts/has_split.sh
@@ -12,17 +12,19 @@ log_dir="$HOME/.safe/node/local-test-network"
 prefix_empty_count=$(rg "prefix=\(\)" "$log_dir" -u | wc -l)
 prefix_0_count=$(rg "prefix=\(0\)" "$log_dir" -u | wc -l)
 prefix_1_count=$(rg "prefix=\(1\)" "$log_dir" -u | wc -l)
+prefix_0_attempt_count=$(rg "Prefix\(0\)" "$log_dir" -u | wc -l)
+prefix_1_attempt_count=$(rg "Prefix\(1\)" "$log_dir" -u | wc -l)
 
 echo "Prefix() found $prefix_empty_count times."
 echo "Prefix(0) found $prefix_0_count times."
 echo "Prefix(1) found $prefix_1_count times."
+echo "Attempts to split to Prefix(0) found $prefix_0_attempt_count times."
+echo "Attempts to split to Prefix(1) found $prefix_1_attempt_count times."
 
 if ! [[ $prefix_0_count -gt 0 && $prefix_1_count -gt 0 ]]
     then
         echo "No split, try changing NODE_COUNT!"
         exit 100
     else
-	echo "Successful split!"
+        echo "Successful split!"
 fi
-
-
