a8bf3ad898eacce3b35e22af0730739dd4cf85b1
Chris O'Neil <chriso83@protonmail.com>
2021-10-29 23:26:46 +0100

    refactor: unit test node run-baby-fleming command
    I wanted to extend the `run-baby-fleming` command to add a new network to the networks list, but
    before doing so, I looked into providing test coverage to the node commands. Currently there are no
    integration tests for these commands. The `node` commands don't really depend on having an existing
    network, so they are much more susceptible to being unit tested.
    
    The one barrier to unit testing was the fact that the command uses `sn_launch_tool` from the `sn`
    crate, and this actually launches nodes. The solution was to introduce a `NetworkLauncher` trait and
    provide a fake implementation of this trait for use in the unit tests. We use the fake launcher to
    assert that the `sn_launch_tool` is called in the correct fashion. The concrete implementation for
    use with the application was simply just a little wrapper around `sn_launch_tool`.
    
    As part of this refactor, the `run-baby-fleming` command is changed as follows:
    * The `node_path` argument was renamed to `node_dir_path`, to be clearer about the fact that this
      option is expected to be a directory path and not a file path.
    * The `test` flag argument was removed, as we're not using the authentication service just now.
    * The `verbose` flag argument was removed, as it only applied to the authentication service test,
      which was removed.
    
    Due to the authentication service no longer being in use, the code related to performing a test
    using the service was removed.
    
    The unit tests provided here should give coverage for all the scenarios in `run-baby-fleming`.

--- sn_cli/src/operations/node.rs
+++ sn_cli/src/operations/node.rs
@@ -9,55 +9,24 @@
 
 #[cfg(feature = "self-update")]
 use super::helpers::download_and_install_github_release_asset;
+use crate::operations::config::NetworkLauncher;
 use color_eyre::{eyre::bail, eyre::eyre, eyre::WrapErr, Result};
-use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use std::{
-    collections::{BTreeSet, HashMap},
+    collections::BTreeSet,
     fs::create_dir_all,
     io::{self, Write},
     net::SocketAddr,
     path::PathBuf,
     process::{Command, Stdio},
-    thread,
-    time::Duration,
 };
 use structopt::StructOpt;
 use tracing::debug;
 
 #[cfg(not(target_os = "windows"))]
-const SN_NODE_EXECUTABLE: &str = "sn_node";
+pub(crate) const SN_NODE_EXECUTABLE: &str = "sn_node";
 
 #[cfg(target_os = "windows")]
-const SN_NODE_EXECUTABLE: &str = "sn_node.exe";
-
-fn run_safe_cmd(
-    args: &[&str],
-    envs: Option<HashMap<String, String>>,
-    ignore_errors: bool,
-    verbosity: u8,
-) -> Result<()> {
-    let env: HashMap<String, String> = envs.unwrap_or_default();
-
-    let msg = format!("Running 'safe' with args {:?} ...", args);
-    if verbosity > 1 {
-        println!("{}", msg);
-    }
-    debug!("{}", msg);
-
-    let _child = Command::new("safe")
-        .args(args)
-        .envs(&env)
-        .stdout(Stdio::inherit())
-        .stderr(if ignore_errors {
-            Stdio::null()
-        } else {
-            Stdio::inherit()
-        })
-        .spawn()
-        .wrap_err_with(|| format!("Failed to run 'safe' with args '{:?}'", args))?;
-
-    Ok(())
-}
+pub(crate) const SN_NODE_EXECUTABLE: &str = "sn_node.exe";
 
 /// Tries to print the version of the node binary pointed to
 pub fn node_version(node_path: Option<PathBuf>) -> Result<()> {
@@ -114,20 +83,20 @@ pub fn node_install(node_path: Option<PathBuf>, version: Option<String>) -> Resu
 }
 
 pub fn node_run(
-    node_path: Option<PathBuf>,
+    network_launcher: &mut Box<impl NetworkLauncher>,
+    node_directory_path: PathBuf,
     nodes_dir: &str,
-    verbosity: u8,
-    interval: &str,
+    interval: u64,
     num_of_nodes: &str,
     ip: Option<String>,
-    test: bool,
 ) -> Result<()> {
-    let node_path = get_node_bin_path(node_path)?;
-
-    let arg_node_path = node_path.join(SN_NODE_EXECUTABLE).display().to_string();
+    let arg_node_path = node_directory_path
+        .join(SN_NODE_EXECUTABLE)
+        .display()
+        .to_string();
     debug!("Running node from {}", arg_node_path);
 
-    let nodes_dir = node_path.join(nodes_dir);
+    let nodes_dir = node_directory_path.join(nodes_dir);
     if !nodes_dir.exists() {
         println!("Creating '{}' folder", nodes_dir.display());
         create_dir_all(nodes_dir.clone())
@@ -136,7 +105,7 @@ pub fn node_run(
     let arg_nodes_dir = nodes_dir.display().to_string();
     println!("Storing nodes' generated data at {}", arg_nodes_dir);
 
-    // Let's create an args array to pass to the network launcher tool
+    let int_arg = interval.clone().to_string();
     let mut sn_launch_tool_args = vec![
         "sn_launch_tool",
         "--node-path",
@@ -144,12 +113,10 @@ pub fn node_run(
         "--nodes-dir",
         &arg_nodes_dir,
         "--interval",
-        interval,
+        &int_arg,
         "--num-nodes",
         num_of_nodes,
     ];
-
-    let interval_as_int = &interval.parse::<u64>().unwrap();
 
     if let Some(ref launch_ip) = ip {
         sn_launch_tool_args.push("--ip");
@@ -158,59 +125,7 @@ pub fn node_run(
         sn_launch_tool_args.push("--local");
     }
 
-    debug!(
-        "Running network launch tool with args: {:?}",
-        sn_launch_tool_args
-    );
-
-    // We can now call the tool with the args
-    println!("Launching local Safe network...");
-    sn_launch_tool::Launch::from_iter_safe(&sn_launch_tool_args)
-        .map_err(|e| eyre!(e))
-        .and_then(|launch| launch.run())
-        .wrap_err("Error launching node")?;
-
-    let interval_duration = Duration::from_secs(interval_as_int * 15);
-    thread::sleep(interval_duration);
-
-    let ignore_errors = true;
-    let report_errors = false;
-
-    if test {
-        println!("Setting up authenticator against local Safe network...");
-
-        // stop authd
-        let stop_auth_args = vec!["auth", "stop"];
-        run_safe_cmd(&stop_auth_args, None, ignore_errors, verbosity)?;
-
-        let between_command_interval = Duration::from_secs(interval_as_int * 5);
-        thread::sleep(between_command_interval);
-
-        // start authd
-        let start_auth_args = vec!["auth", "start"];
-        run_safe_cmd(&start_auth_args, None, report_errors, verbosity)?;
-
-        thread::sleep(between_command_interval);
-
-        // Q: can we assume network is correct here? Or do we need to do networks switch?
-        let passphrase: String = thread_rng().sample_iter(&Alphanumeric).take(15).collect();
-        let password: String = thread_rng().sample_iter(&Alphanumeric).take(15).collect();
-
-        // setup env for create / unlock
-        let mut env = HashMap::new();
-        env.insert("SAFE_AUTH_PASSPHRASE".to_string(), passphrase);
-        env.insert("SAFE_AUTH_PASSWORD".to_string(), password);
-
-        // create a Safe
-        let create = vec!["auth", "create", "--test-coins"];
-
-        run_safe_cmd(&create, Some(env.clone()), report_errors, verbosity)?;
-        thread::sleep(between_command_interval);
-
-        // unlock the Safe
-        let unlock = vec!["auth", "unlock", "--self-auth"];
-        run_safe_cmd(&unlock, Some(env), report_errors, verbosity)?;
-    }
+    network_launcher.launch(sn_launch_tool_args, interval)?;
 
     Ok(())
 }
