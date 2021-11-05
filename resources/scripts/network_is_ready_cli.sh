b382bbabf1c108f923eb2e53e35c363da06b14cd
Josh Wilson <joshuef@gmail.com>
2021-11-05 13:52:32 +0100

    ci: just use ripgrep

--- resources/scripts/network_is_ready_cli.sh
+++ resources/scripts/network_is_ready_cli.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Waiting for a healthy network to be detected, as per the 'split_network_assert_health_check' test in sn/src/lib.rs "
-cd sn
-until ./has_split || $( sleep 5 && false ); do :; done
-cd ..
+until ./resources/scripts/has_split.sh || $( sleep 5 && false ); do :; done
