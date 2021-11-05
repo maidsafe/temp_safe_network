102cabc5895d075143dabd023b9d8a3148a3d0c4
Josh Wilson <joshuef@gmail.com>
2021-11-05 10:08:56 +0100

    chore: properly loop network_is_ready.sh

--- resources/scripts/network_is_ready.sh
+++ resources/scripts/network_is_ready.sh
@@ -2,5 +2,5 @@
 
 echo "Waiting for a haelthy network to be detected, as per the 'split_network_assert_health_check' test in sn/src/lib.rs "
 cd sn
-until cargo test --release --features=always-joinable,test-utils -- --ignored split_network_assert_health_check || sleep 5; do :; done
+until cargo test --release --features=always-joinable,test-utils -- --ignored split_network_assert_health_check || sleep 5 && false; do :; done
 cd ..
