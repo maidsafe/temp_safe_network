a9d6c17041c30674d75c2c385bcb889a324fd743
Josh Wilson <joshuef@gmail.com>
2021-11-05 10:39:50 +0100

    chore: loop network_is_ready.sh only until success

--- resources/scripts/network_is_ready.sh
+++ resources/scripts/network_is_ready.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Waiting for a haelthy network to be detected, as per the 'split_network_assert_health_check' test in sn/src/lib.rs "
+echo "Waiting for a healthy network to be detected, as per the 'split_network_assert_health_check' test in sn/src/lib.rs "
 cd sn
-until cargo test --release --features=always-joinable,test-utils -- --ignored split_network_assert_health_check || sleep 5 && false; do :; done
+until cargo test --lib --release --features=always-joinable,test-utils -- --ignored split_network_assert_health_check || $( sleep 5 && false ); do :; done
 cd ..
