c0a6369b06c95845a017f1328e043a368da3f9be
Chris O'Neil <chriso83@protonmail.com>
2021-11-16 15:40:17 +0000

    ci: use smart-release to auto bump version
    We change from using our own custom action that used the `standard-version` Node library to perform
    semantic version bumps and generate changelogs, to `cargo smart-release`.
    
    The `smart-release` tool uses the presence of 'conventional commits' to:
    * Identify crates in the workspace that have changes
    * Perform semantic version bumps
    * Update any dependent crates to use bumped version numbers
    * Generate changelogs
    
    The changes it makes are captured in a commit it generates, and for each crate that had a change, it
    also creates tags at this commit. We soft reset this commit so we can change the commit message to
    use `chore(release): `, re-commit, then update the tags to point to this new commit. After this, we
    push both the release commit and the tags.
    
    Previously we had separate workflows for the version bumping and tagging. The version bump commit
    was submitted in an automated PR, which was then merged, and kicked off the tagging workflow, which
    tagged and pushed the tags. We now don't have the generated PR and just push the changes directly.

---
+++ resources/scripts/bump_version.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+commit_message=""
+sn_version=""
+sn_api_version=""
+sn_cli_version=""
+safe_network_has_changes=false
+sn_api_has_changes=false
+sn_cli_has_changes=false
+
+function determine_which_crates_have_changes() {
+    local output
+    output=$(cargo smart-release \
+        --update-crates-index \
+        --no-push \
+        --no-publish \
+        --no-changelog-preview \
+        --allow-fully-generated-changelogs \
+        --no-changelog-github-release \
+        --no-isolate-dependencies-from-breaking-changes \
+        safe_network sn_api 2>&1)
+    if [[ $output == *"WOULD auto-bump dependent package 'safe_network'"* ]]; then
+        safe_network_has_changes=true
+    fi
+    if [[ $output == *"WOULD auto-bump dependent package 'sn_api'"* ]]; then
+        sn_api_has_changes=true
+    fi
+    if [[ $output == *"WOULD auto-bump dependent package 'sn_cli'"* ]]; then
+        sn_cli_has_changes=true
+    fi
+}
+
+function generate_version_bump_commit() {
+    cargo smart-release \
+        --update-crates-index \
+        --no-push \
+        --no-publish \
+        --no-changelog-preview \
+        --allow-fully-generated-changelogs \
+        --no-changelog-github-release \
+        --no-isolate-dependencies-from-breaking-changes \
+        --execute \
+        safe_network sn_api
+}
+
+function generate_new_commit_message() {
+    sn_version=$(grep "^version" < sn/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
+    sn_api_version=$(grep "^version" < sn_api/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
+    sn_cli_version=$(grep "^version" < sn_cli/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
+    commit_message="chore(release): "
+
+    if [[ $safe_network_has_changes == true ]]; then
+        commit_message="${commit_message}safe_network-${sn_version}/"
+    fi
+    if [[ $sn_api_has_changes == true ]]; then
+        commit_message="${commit_message}sn_api-${sn_api_version}/"
+    fi
+    if [[ $sn_cli_has_changes == true ]]; then
+        commit_message="${commit_message}sn_cli-${sn_cli_version}/"
+    fi
+    commit_message=${commit_message::-1} # strip off any trailing '/'
+}
+
+function amend_version_bump_commit() {
+    git reset --soft HEAD~1
+    git add --all
+    git commit -m "$commit_message"
+}
+
+function amend_tags() {
+    if [[ $safe_network_has_changes == true ]]; then git tag "safe_network-v${sn_version}" -f; fi
+    if [[ $sn_api_has_changes == true ]]; then git tag "sn_api-v${sn_api_version}" -f; fi
+    if [[ $sn_cli_has_changes == true ]]; then git tag "sn_cli-v${sn_cli_version}" -f; fi
+}
+
+determine_which_crates_have_changes
+generate_version_bump_commit
+generate_new_commit_message
+amend_version_bump_commit
+amend_tags
