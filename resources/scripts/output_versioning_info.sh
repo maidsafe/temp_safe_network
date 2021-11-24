d10d74ee89e11294d31f84310ede7ec261c3871f
Chris O'Neil <chriso83@protonmail.com>
2021-11-24 15:12:26 +0000

    ci: use different tag name for gh release
    In the last attempt to perform a release, the creation of the Github Release failed. Unfortunately
    there was no error output from the action, so I'm left guessing as to the cause. I suspect it might
    be because the tag name being used has a '/' in it. However, I'm not completely sure about this,
    because I created a release on my fork using the gh command line tool, and I used the same tag name
    as the run that had the error. The release creation was successful, but I notice it did return back
    a strange URL, so this is my best bet at the moment.

--- resources/scripts/output_versioning_info.sh
+++ resources/scripts/output_versioning_info.sh
@@ -1,31 +1,51 @@
 #!/usr/bin/env bash
 
+function build_release_name() {
+    if [[ $commit_message == *"safe_network"* ]]; then
+        gh_release_name="Safe Network v$sn_version/"
+    fi
+    if [[ $commit_message == *"sn_api"* ]]; then
+        gh_release_name="${gh_release_name}Safe API v$sn_api_version/"
+    fi
+    if [[ $commit_message == *"sn_cli"* ]]; then
+        gh_release_name="${gh_release_name}Safe CLI v$sn_cli_version/"
+    fi
+    gh_release_name=${gh_release_name::-1} # strip off any trailing '/' 
+}
+
+function build_release_tag_name() {
+    # This is to avoid having a '/' in the tag name, which Github don't seem to like.
+    # If the sn crate has had changes, we'll just use that as the tag name, otherwise
+    # use sn_api, otherwise sn_cli. It doesn't really matter too much, as this is just
+    # tag for the Github Release. The actual release tags will have already been created
+    # at version bumping, and there will be a tag for each of the crates.
+    if [[ $commit_message == *"safe_network"* ]]; then
+        gh_release_tag_name="safe_network-v$sn_version"
+    elif [[ $commit_message == *"sn_api"* ]]; then
+        gh_release_tag_name="sn_api-v$sn_api_version"
+    elif [[ $commit_message == *"sn_cli"* ]]; then
+        gh_release_tag_name="sn_cli-v$sn_cli_version"
+    else
+        echo "Unable to set the tag name for the Github Release"
+        echo "The commit message doesn't contain any expected text"
+        exit 1
+    fi
+}
+
+function output_version_info() {
+    echo "::set-output name=sn_version::$sn_version"
+    echo "::set-output name=sn_api_version::$sn_api_version"
+    echo "::set-output name=sn_cli_version::$sn_cli_version"
+    echo "::set-output name=gh_release_name::$gh_release_name"
+    echo "::set-output name=gh_release_tag_name::$gh_release_tag_name"
+}
+
 gh_release_name=""
 gh_release_tag_name=""
 commit_message=$(git log --oneline --pretty=format:%s | head -n 1)
 sn_version=$(grep "^version" < sn/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
 sn_api_version=$(grep "^version" < sn_api/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
 sn_cli_version=$(grep "^version" < sn_cli/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
-
-if [[ $commit_message == *"safe_network"* ]]; then
-    gh_release_name="Safe Network v$sn_version/"
-    gh_release_tag_name="safe_network-v$sn_version/"
-fi
-if [[ $commit_message == *"sn_api"* ]]; then
-    gh_release_name="${gh_release_name}Safe API v$sn_api_version/"
-    gh_release_tag_name="${gh_release_tag_name}sn_api-v$sn_api_version/"
-fi
-if [[ $commit_message == *"sn_cli"* ]]; then
-    gh_release_name="${gh_release_name}Safe CLI v$sn_cli_version/"
-    gh_release_tag_name="${gh_release_tag_name}sn_cli-v$sn_cli_version/"
-fi
-
-# strip off any trailing '/' 
-gh_release_name=${gh_release_name::-1}
-gh_release_tag_name=${gh_release_tag_name::-1}
-
-echo "::set-output name=sn_version::$sn_version"
-echo "::set-output name=sn_api_version::$sn_api_version"
-echo "::set-output name=sn_cli_version::$sn_cli_version"
-echo "::set-output name=gh_release_name::$gh_release_name"
-echo "::set-output name=gh_release_tag_name::$gh_release_tag_name"
+build_release_name
+build_release_tag_name
+output_version_info
