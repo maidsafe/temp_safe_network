c284f0787afe0d079e53b79b3a9d74cad04c4b0e
Chris O'Neil <chriso83@protonmail.com>
2021-12-03 19:32:10 +0000

    feat(sn_cli)!: `nrs create` only creates topnames
    BREAKING CHANGE: this command should now be used to create new topnames, but *not* a subname and a
    new topname at the same time. The user is advised on this by way of suggestions in error messages.
    
    The user experience of this command was also improved in the following ways:
    * Validate URLs supplied with the `--link` argument *before* creating the topname. This is to ensure
      the user doesn't receive an error message after the topname was already created, which can
      potentially be quite confusing.
    * Suggest supplying valid XorUrls for the `--link` argument.
    * Suggest that links to versionable content must have their version hash specified on the `--link`
      argument.
    * Suggest the use of the `add` command with the `--create-top-name` argument to create a subname and
      a topname at the same time, or use the `create` command to first create the topname, then use the
      `add` command to create a subname.
    * Suggest trying a different name if the chosen topname is already registered.
    * The grammar of a couple of error messages in the API were updated for improved readability.
    
    Some behaviour was removed:
    * Since dry run support is still in progress for NRS, the `--dry-run` code was removed.
    * The `--direct` argument was removed, since it only really applies to subnames, which you can no
      longer supply with the `create` command. This is a BREAKING CHANGE.
    
    The initial intention was to fix up the failing NRS tests for the CLI. These failures were due
    to the fact that the API for NRS had changed, which was resulting in the commands behaving a bit
    differently than they used to. The main difference being the fact that the NRS API doesn't allow the
    creation of subnames along with a topname.
    
    I took the opportunity to introduce new tests that should cover all the scenarios for the `create`
    command.

--- sn_cli/tests/cli_nrs.rs
+++ sn_cli/tests/cli_nrs.rs
@@ -8,17 +8,16 @@
 // Software.
 
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
 use color_eyre::{eyre::eyre, Result};
 use predicates::prelude::*;
 use sn_api::{resolver::ContentType, DataAddress, Scope, Url, XorUrlBase};
 use sn_cmd_test_utilities::util::{
     get_random_nrs_string, parse_nrs_create_output, safe_cmd, safe_cmd_stdout, safeurl_from,
-    upload_test_folder, CLI, SAFE_PROTOCOL,
+    upload_path, upload_test_folder, CLI, SAFE_PROTOCOL,
 };
 use std::process::Command;
 use xor_name::XorName;
-
-const PRETTY_NRS_CREATION_RESPONSE: &str = "New NRS Map";
 
 fn gen_fake_target() -> Result<String> {
     let xorname = XorName(*b"12345678901234567890123456789012");
@@ -38,18 +37,160 @@ fn gen_fake_target() -> Result<String> {
     .map_err(|e| eyre!("Failed to encode URL: {}", e))
 }
 
+///
+/// `nrs create` subcommand
+///
+/// Note: these CLI tests will *not* verify that NRS has linked to the correct content. For
+/// example, if you create an NRS link to <something>/README.md, we're not going to perform a `cat`
+/// to make sure README.md has been linked to, and not, say, test.md. This should be covered by
+/// other tests in the system that cover the `nrs_associate` API. It's reasonable to make the
+/// assumption in these tests that `nrs_associate` links to the correct content.
+
 #[test]
-fn calling_safe_nrs_create_pretty() -> Result<()> {
-    let test_name = get_random_nrs_string();
-    let fake_target = gen_fake_target()?;
-    let mut cmd = Command::cargo_bin(CLI).map_err(|e| eyre!(e.to_string()))?;
-    cmd.args(&vec!["nrs", "create", &test_name, "-l", &fake_target])
+fn nrs_create_should_create_a_topname() -> Result<()> {
+    let topname = get_random_nrs_string();
+    safe_cmd(["nrs", "create", &topname], Some(0))?
         .assert()
-        .stdout(predicate::str::contains(PRETTY_NRS_CREATION_RESPONSE))
-        .stdout(predicate::str::contains(SAFE_PROTOCOL).count(3))
-        .stdout(predicate::str::contains(fake_target).count(1))
-        .stdout(predicate::str::contains("+").count(1))
-        .success();
+        .stdout(predicate::str::contains(format!(
+            "New NRS Map created for \"safe://{}\"",
+            topname
+        )));
+    Ok(())
+}
+
+#[test]
+fn nrs_create_should_create_a_topname_with_a_versioned_content_link() -> Result<()> {
+    let with_trailing_slash = true;
+    let tmp_data_path = assert_fs::TempDir::new()?;
+    tmp_data_path.copy_from("../resources/testdata", &["**"])?;
+    let (files_container_xor, _processed_files, _) =
+        upload_path(&tmp_data_path, with_trailing_slash)?;
+    let mut url = Url::from_url(&files_container_xor)?;
+    url.set_path("test.md");
+
+    let topname = get_random_nrs_string();
+    safe_cmd(
+        ["nrs", "create", &topname, "--link", &url.to_string()],
+        Some(0),
+    )?
+    .assert()
+    .stdout(predicate::str::contains(format!(
+        "New NRS Map created for \"safe://{}\"",
+        topname
+    )))
+    .stdout(predicate::str::contains(format!(
+        "The entry points to {}",
+        url.to_string()
+    )));
+    Ok(())
+}
+
+#[test]
+fn nrs_create_should_create_a_topname_with_an_immutable_content_link() -> Result<()> {
+    let with_trailing_slash = true;
+    let tmp_data_path = assert_fs::TempDir::new()?;
+    tmp_data_path.copy_from("../resources/testdata", &["**"])?;
+    let (_files_container_xor, processed_files, _) =
+        upload_path(&tmp_data_path, with_trailing_slash)?;
+    let test_md_entry = processed_files.iter().last().unwrap();
+    let test_md_blob_link = test_md_entry.1.to_owned().1;
+    let url = Url::from_url(&test_md_blob_link)?;
+    println!("processed_files = {:?}", processed_files);
+
+    let topname = get_random_nrs_string();
+    safe_cmd(
+        ["nrs", "create", &topname, "--link", &url.to_string()],
+        Some(0),
+    )?
+    .assert()
+    .stdout(predicate::str::contains(format!(
+        "New NRS Map created for \"safe://{}\"",
+        topname
+    )))
+    .stdout(predicate::str::contains(format!(
+        "The entry points to {}",
+        url.to_string()
+    )));
+    Ok(())
+}
+
+#[test]
+fn nrs_create_should_return_an_error_if_a_subname_is_specified() -> Result<()> {
+    let name = format!("a.{}", get_random_nrs_string());
+    safe_cmd(["nrs", "create", &name], Some(1))?
+        .assert()
+        .stderr(predicate::str::contains(
+            "The create command can only create a topname, \
+            it cannot create subnames.",
+        ))
+        .stderr(predicate::str::contains(
+            "Please use the nrs add command with the --create-top-name \
+            argument to create a topname and add a subname at the same time.",
+        ))
+        .stderr(predicate::str::contains(
+            "Alternatively, create the topname first with the create command, \
+            then use the add command to create the subname.",
+        ));
+    Ok(())
+}
+
+#[test]
+fn nrs_create_should_return_an_error_if_an_invalid_link_is_specified() -> Result<()> {
+    let topname = get_random_nrs_string();
+    safe_cmd(["nrs", "create", &topname, "--link", "invalid"], Some(1))?
+        .assert()
+        .stderr(predicate::str::contains(format!(
+            "Could not create topname {}. The supplied link was not a valid XorUrl.",
+            topname
+        )))
+        .stderr(predicate::str::contains(
+            "Run the command again with a valid XorUrl for the --link argument.",
+        ));
+    Ok(())
+}
+
+#[test]
+fn nrs_create_should_return_an_error_if_link_to_versioned_content_has_no_version() -> Result<()> {
+    let with_trailing_slash = true;
+    let tmp_data_path = assert_fs::TempDir::new()?;
+    tmp_data_path.copy_from("../resources/testdata", &["**"])?;
+    let (files_container_xor, _processed_files, _) =
+        upload_path(&tmp_data_path, with_trailing_slash)?;
+    let mut url = Url::from_url(&files_container_xor)?;
+    url.set_path("test.md");
+    url.set_content_version(None);
+
+    let topname = get_random_nrs_string();
+    safe_cmd(
+        ["nrs", "create", &topname, "--link", &url.to_string()],
+        Some(1),
+    )?
+    .assert()
+    .stderr(predicate::str::contains(
+        "The destination you're trying to link to is versionable content. \
+            When linking to versionable content, you must supply a version hash on the XorUrl. \
+            The requested topname was not created.",
+    ))
+    .stderr(predicate::str::contains(
+        "Please run the command again with the version hash appended to the link. \
+            The link should have the form safe://<xorurl>?v=<versionhash>.",
+    ));
+    Ok(())
+}
+
+#[test]
+fn nrs_create_should_return_an_error_if_the_topname_already_exists() -> Result<()> {
+    let topname = get_random_nrs_string();
+    safe_cmd(["nrs", "create", &topname], Some(0))?;
+    safe_cmd(["nrs", "create", &topname], Some(1))?
+        .assert()
+        .stderr(predicate::str::contains(format!(
+            "Could not create topname {}. That name is already taken.",
+            topname
+        )))
+        .stderr(predicate::str::contains(
+            "Try the command again with a different name.",
+        ));
     Ok(())
 }
 
@@ -100,27 +241,6 @@ fn calling_safe_nrs_with_y_but_name_doesnt_exist() -> Result<()> {
     .stdout(predicate::str::contains("+").count(1))
     .success();
 
-    Ok(())
-}
-
-#[test]
-#[ignore = "extend create command to use eyre suggestion"]
-fn calling_safe_nrs_twice_w_name_fails() -> Result<()> {
-    let test_name = get_random_nrs_string();
-    let fake_target = gen_fake_target()?;
-
-    safe_cmd(
-        ["nrs", "create", &test_name, "-l", &fake_target, "--json"],
-        Some(0),
-    )?;
-
-    let mut cmd = Command::cargo_bin(CLI).map_err(|e| eyre!(e.to_string()))?;
-    cmd.args(&vec!["nrs", "create", &test_name, "-l", &fake_target])
-        .assert()
-        .stderr(predicate::str::contains(
-            "NRS name already exists. Please use 'nrs add' command to add sub names to it",
-        ))
-        .failure();
     Ok(())
 }
 
