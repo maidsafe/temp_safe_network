6f351415d0b00bffa46de625d2959d7fff78ca5f
Josh Wilson <joshuef@gmail.com>
2021-12-03 19:34:48 +0100

    tidier tests

--- resources/scripts/network_is_ready.sh
+++ resources/scripts/network_is_ready.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Waiting for a healthy network to be detected, as per the 'split_network_assert_health_check' test in sn/src/lib.rs "
-until cargo test  -p safe_network --lib --release --features=always-joinable,test-utils -- --ignored split_network_assert_health_check || $( sleep 15 && false ); do :; done
+until cargo test --lib --release --features=always-joinable,test-utils -- --ignored split_network_assert_health_check || $( sleep 15 && false ); do :; done
