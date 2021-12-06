12c2b5c34a8a10874fad05df688c239945e69202
Josh Wilson <joshuef@gmail.com>
2021-12-06 18:16:29 +0100

    60minutes

--- resources/scripts/network_is_ready_cli.sh
+++ resources/scripts/network_is_ready_cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Waiting for a healthy network to be detected, as per the 'split_network_assert_health_check' test in sn/src/lib.rs "
-until ./resources/scripts/nodes_finished_joining.sh || $( sleep 5 && false ); do :; done
+until ./resources/scripts/nodes_finished_joining.sh || $( sleep 30 && false ); do :; done
 until ./resources/scripts/has_split.sh || $( sleep 5 && false ); do :; done
