3e38ea6c0be72100d093b96cad5d5d4ff986352a
Josh Wilson <joshuef@gmail.com>
2021-12-08 07:46:48 +0100

    update health check var

--- resources/scripts/nodes_finished_joining.sh
+++ resources/scripts/nodes_finished_joining.sh
@@ -7,7 +7,7 @@ then
 fi
 
 DEFAULT_NODE_COUNT=33
-NODE_COUNT="${SN_NODE_COUNT:-$DEFAULT_NODE_COUNT}"
+NODE_COUNT="${NODE_COUNT:-$DEFAULT_NODE_COUNT}"
 
 # It's better to use the network health test in rust as it's type safe.
 # This is needed for windows though at the moment due to the logfile locking...
