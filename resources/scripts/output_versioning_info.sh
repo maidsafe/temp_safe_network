2f2522b8b8e21cc04faac052e089b158757e81f2
Chris O'Neil <chriso83@protonmail.com>
2021-12-17 02:29:58 +0000

    ci: release tag_name to include 3 crate versions
    Rather than just having the `safe_network` crate version, the tag name field will now include the
    version numbers of the 3 crates. This will enable us to be able to install a specific version of the
    CLI. This tag name should always be unique, since there will always be at least 1 crate with a
    change in it.
    
    The title of the release will also include the names and versions of each of the components, even if
    one crate didn't change.
    
    The commit message will still just include the crates that changed, so that we know which publishing
    stages to run.
    
    Also fixes a little bug here with one of the asset hashes not being set correctly.

--- resources/scripts/output_versioning_info.sh
+++ resources/scripts/output_versioning_info.sh
@@ -1,35 +1,15 @@
 #!/usr/bin/env bash
 
 function build_release_name() {
-    if [[ $commit_message == *"safe_network"* ]]; then
-        gh_release_name="Safe Network v$sn_version/"
-    fi
-    if [[ $commit_message == *"sn_api"* ]]; then
-        gh_release_name="${gh_release_name}Safe API v$sn_api_version/"
-    fi
-    if [[ $commit_message == *"sn_cli"* ]]; then
-        gh_release_name="${gh_release_name}Safe CLI v$sn_cli_version/"
-    fi
-    gh_release_name=${gh_release_name::-1} # strip off any trailing '/' 
+    gh_release_name="Safe Network v$sn_version/"
+    gh_release_name="${gh_release_name}Safe API v$sn_api_version/"
+    gh_release_name="${gh_release_name}Safe CLI v$sn_cli_version"
 }
 
 function build_release_tag_name() {
-    # This is to avoid having a '/' in the tag name, which Github don't seem to like.
-    # If the sn crate has had changes, we'll just use that as the tag name, otherwise
-    # use sn_api, otherwise sn_cli. It doesn't really matter too much, as this is just
-    # tag for the Github Release. The actual release tags will have already been created
-    # at version bumping, and there will be a tag for each of the crates.
-    if [[ $commit_message == *"safe_network"* ]]; then
-        gh_release_tag_name="safe_network-v$sn_version"
-    elif [[ $commit_message == *"sn_api"* ]]; then
-        gh_release_tag_name="sn_api-v$sn_api_version"
-    elif [[ $commit_message == *"sn_cli"* ]]; then
-        gh_release_tag_name="sn_cli-v$sn_cli_version"
-    else
-        echo "Unable to set the tag name for the Github Release"
-        echo "The commit message doesn't contain any expected text"
-        exit 1
-    fi
+    gh_release_tag_name="v$sn_version-"
+    gh_release_tag_name="v$sn_api_version-"
+    gh_release_tag_name="v$sn_cli_version"
 }
 
 function output_version_info() {
