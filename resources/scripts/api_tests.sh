33bc539db3c88fb02977c6199497cd5ebb1c0cc7
Josh Wilson <joshuef@gmail.com>
2021-12-20 11:58:28 +0100

    ci: run all tests in sequence

--- resources/scripts/api_tests.sh
+++ resources/scripts/api_tests.sh
@@ -6,7 +6,7 @@ function run_api_tests() {
     export TEST_BOOTSTRAPPING_PEERS=$(cat ~/.safe/node/node_connection_info.config)
 
     # TODO: enable doc tests, for now they require work
-    cd sn_api && cargo test --lib --release
+    cd sn_api && cargo test --lib --release -- --test-threads=1
     cd -
 }
 
