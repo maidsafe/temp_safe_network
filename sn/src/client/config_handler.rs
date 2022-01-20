ef12d9515edcac02228307e847f6c08ff250b919
Josh Wilson <joshuef@gmail.com>
2022-01-20 07:11:44 +0100

    WIP: testing

--- sn/src/client/config_handler.rs
+++ sn/src/client/config_handler.rs
@@ -25,9 +25,9 @@ const DEFAULT_LOCAL_ADDR: (Ipv4Addr, u16) = (Ipv4Addr::UNSPECIFIED, 0);
 /// Default amount of time to wait for responses to queries before giving up and returning an error.
 pub const DEFAULT_QUERY_TIMEOUT: Duration = Duration::from_secs(120);
 /// Default idle timeout on the elder connection
-const DEFAULT_IDLE_TIMEOUT: Duration = Duration::from_secs(120);
+const DEFAULT_IDLE_TIMEOUT: Duration = Duration::from_secs(10);
 /// Default keep alive for elder comms
-const DEFAULT_KEEP_ALIVE: Duration = Duration::from_secs(30);
+const DEFAULT_KEEP_ALIVE: Duration = Duration::from_secs(5);
 /// Default amount of time to wait (to keep the client alive) after sending a command. This allows AE messages to be parsed/resent.
 /// Larger PUT operations may need larger ae wait time
 pub const DEFAULT_AE_WAIT: Duration = Duration::from_secs(0);
