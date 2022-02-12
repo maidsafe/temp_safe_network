5ebd18d73d327acea12209285cc8587339c0e3af
oetyng <oetyng@gmail.com>
2022-02-12 22:29:53 +0100

    chore: co-locate related modules - backpressure and msg count are made into submodules of comm

--- sn/src/node/core/comm/mod.rs
+++ sn/src/node/core/comm/mod.rs
@@ -6,7 +6,11 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use super::{msg_count::MsgCount, BackPressure};
+mod back_pressure;
+mod msg_count;
+
+use back_pressure::BackPressure;
+use msg_count::MsgCount;
 
 use crate::messaging::{system::LoadReport, MsgId, WireMsg};
 use crate::node::error::{Error, Result};
