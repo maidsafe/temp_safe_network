6e4f771864b24acd0e528367d86832d2ef8c78c6
Chris O'Neil <chriso83@protonmail.com>
2022-03-25 20:33:21 +0000

    ci: add sn_dysfunction to release process
    The release workflow is extended to include the dysfunction crate. Add dysfunction to:
    
    * Version bumping
    * Version outputs for release process
    * The release changelog
    * Publishing as the first crate
    
    A basic README was also added to the dysfunction crate as this seems to be a prerequisite for a
    publish.

--- resources/scripts/output_versioning_info.sh
+++ resources/scripts/output_versioning_info.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 
 function build_release_name() {
-    gh_release_name="Safe Network v$sn_version/"
-    gh_release_name="${gh_release_name}Safe API v$sn_api_version/"
-    gh_release_name="${gh_release_name}Safe CLI v$sn_cli_version"
+  gh_release_name="Safe Node Dysfunction v$sn_dysfunction_version/"
+  gh_release_name="${gh_release_name}Safe Network v$sn_version/"
+  gh_release_name="${gh_release_name}Safe API v$sn_api_version/"
+  gh_release_name="${gh_release_name}Safe CLI v$sn_cli_version"
 }
 
 function build_release_tag_name() {
-    gh_release_tag_name="$sn_version-"
-    gh_release_tag_name="${gh_release_tag_name}$sn_api_version-"
-    gh_release_tag_name="${gh_release_tag_name}$sn_cli_version"
+  gh_release_tag_name="$sn_dysfunction_version-"
+  gh_release_tag_name="${gh_release_tag_name}$sn_version-"
+  gh_release_tag_name="${gh_release_tag_name}$sn_api_version-"
+  gh_release_tag_name="${gh_release_tag_name}$sn_cli_version"
 }
 
 function output_version_info() {
-    echo "::set-output name=sn_version::$sn_version"
-    echo "::set-output name=sn_api_version::$sn_api_version"
-    echo "::set-output name=sn_cli_version::$sn_cli_version"
-    echo "::set-output name=gh_release_name::$gh_release_name"
-    echo "::set-output name=gh_release_tag_name::$gh_release_tag_name"
+  echo "::set-output name=sn_dysfunction_version::$sn_dysfunction_version"
+  echo "::set-output name=sn_version::$sn_version"
+  echo "::set-output name=sn_api_version::$sn_api_version"
+  echo "::set-output name=sn_cli_version::$sn_cli_version"
+  echo "::set-output name=gh_release_name::$gh_release_name"
+  echo "::set-output name=gh_release_tag_name::$gh_release_tag_name"
 }
 
 gh_release_name=""
 gh_release_tag_name=""
 commit_message=$(git log --oneline --pretty=format:%s | head -n 1)
+sn_dysfunction_version=$( \
+  grep "^version" < sn_dysfunction/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
 sn_version=$(grep "^version" < sn/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
 sn_api_version=$(grep "^version" < sn_api/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
 sn_cli_version=$(grep "^version" < sn_cli/Cargo.toml | head -n 1 | awk '{ print $3 }' | sed 's/\"//g')
