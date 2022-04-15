8077a98a417f7e8180b95cfdac0840e0212f864d
github-action-benchmark <github@users.noreply.github.com>
2022-04-15 11:31:48 +0000

    add Safe Network Benchmarks (cargo) benchmark result for 697442816bcf96193a52955c97997a1972237bff

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1650022308941,
+  "repoUrl": "https://github.com/maidsafe/safe_network",
+  "entries": {
+    "Safe Network Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "697442816bcf96193a52955c97997a1972237bff",
+          "message": "ci: another git hook removal attempt",
+          "timestamp": "2022-04-15T13:02:44+02:00",
+          "tree_id": "2a44948a1c42a2095b0433fa06357e3b43b1db01",
+          "url": "https://github.com/maidsafe/safe_network/commit/697442816bcf96193a52955c97997a1972237bff"
+        },
+        "date": 1650022308342,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "upload-sampling/upload 3072b",
+            "value": 30701240334,
+            "range": "± 12996145696",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 1mb",
+            "value": 4699648042,
+            "range": "± 15612645960",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 10mb",
+            "value": 23625858546,
+            "range": "± 16965174814",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
