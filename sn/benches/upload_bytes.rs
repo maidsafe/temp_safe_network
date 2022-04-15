8a550f45d24fe44c2680f1f6819e3ba887383fcc
Josh Wilson <joshuef@gmail.com>
2022-04-15 15:06:25 +0200

    ci(bench): Increase target time. Add upload only benches

--- sn/benches/upload_bytes.rs
+++ sn/benches/upload_bytes.rs
@@ -9,15 +9,14 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use eyre::Result;
 use safe_network::client::{
-    utils::test_utils::read_network_conn_info,
-    Client, ClientConfig, Error,
+    utils::test_utils::read_network_conn_info, Client, ClientConfig, Error,
 };
 use sn_interface::types::utils::random_bytes;
 use sn_interface::types::Scope;
 use tokio::runtime::Runtime;
 
 /// This bench requires a network already set up
-async fn upload_bytes(size: usize) -> Result<(), Error> {
+async fn upload_and_read_bytes(size: usize) -> Result<(), Error> {
     let (genesis_key, bootstrap_nodes) = read_network_conn_info().unwrap();
     let bytes = random_bytes(size);
     let config = ClientConfig::new(None, None, genesis_key, None, None, None, None).await;
@@ -31,33 +30,75 @@ async fn upload_bytes(size: usize) -> Result<(), Error> {
 
     Ok(())
 }
+/// This bench requires a network already set up
+async fn upload_only(size: usize) -> Result<(), Error> {
+    let (genesis_key, bootstrap_nodes) = read_network_conn_info().unwrap();
+    let bytes = random_bytes(size);
+    let config = ClientConfig::new(None, None, genesis_key, None, None, None, None).await;
+    let client = Client::new(config, bootstrap_nodes, None).await?;
+    let address = client.upload(bytes.clone(), Scope::Public).await?;
+
+    assert_eq!(received_bytes, bytes);
+
+    Ok(())
+}
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("upload-sampling");
+    // measure over around 9s in general
+    group.measurement_time(std::time::Duration::from_secs(90));
 
     let runtime = Runtime::new().unwrap();
     group.sample_size(10);
+
+    // upload and read
+    group.bench_function("upload and read 3072b", |b| {
+        b.to_async(&runtime).iter(|| async {
+            match upload_and_read_bytes(3072).await {
+                Ok(_) => {}
+                Err(error) => println!("3072b upload and read bench failed with {:?}", error),
+            }
+        });
+    });
+    group.bench_function("upload and read 1mb", |b| {
+        b.to_async(&runtime).iter(|| async {
+            match upload_and_read_bytes(1024 * 1024).await {
+                Ok(_) => {}
+                Err(error) => println!("1mb upload and read bench failed with {:?}", error),
+            }
+        });
+    });
+    group.bench_function("upload and read 10mb", |b| {
+        b.to_async(&runtime).iter(|| async {
+            match upload_and_read_bytes(1024 * 1024 * 10).await {
+                Ok(_) => {}
+                Err(error) => println!("10mb upload and read bench failed with {:?}", error),
+            }
+        });
+    });
+
+    // only upload
     group.bench_function("upload 3072b", |b| {
         b.to_async(&runtime).iter(|| async {
-            match upload_bytes(3072).await {
+            match upload(3072).await {
                 Ok(_) => {}
-                Err(error) => println!("bench failed with {:?}", error),
+                Err(error) => println!("3072b upload bench failed with {:?}", error),
             }
         });
     });
     group.bench_function("upload 1mb", |b| {
         b.to_async(&runtime).iter(|| async {
-            match upload_bytes(1024 * 1024).await {
+            match upload(1024 * 1024).await {
                 Ok(_) => {}
-                Err(error) => println!("bench failed with {:?}", error),
+                Err(error) => println!("1mb upload bench failed with {:?}", error),
             }
         });
     });
     group.bench_function("upload 10mb", |b| {
         b.to_async(&runtime).iter(|| async {
-            match upload_bytes(1024 * 1024 * 10).await {
+            match upload(1024 * 1024 * 10).await {
                 Ok(_) => {}
-                Err(error) => println!("bench failed with {:?}", error),
+                Err(error) => println!("10mb upload bench failed with {:?}", error),
             }
         });
     });
