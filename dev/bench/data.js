5a59d57a7242eb436d1f77105450056a32bacacd
github-action-benchmark <github@users.noreply.github.com>
2022-04-19 16:46:47 +0000

    add Safe Network Benchmarks (cargo) benchmark result for e9550c10e7cf5ff5778e32d8eeeddea09824ecec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1650265765952,
+  "lastUpdate": 1650386807978,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "Safe Network Benchmarks": [
@@ -101,6 +101,66 @@ window.BENCHMARK_DATA = {
             "name": "upload-sampling/upload 10mb",
             "value": 4575696139,
             "range": "± 237044270",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshuef@gmail.com",
+            "name": "Josh Wilson",
+            "username": "joshuef"
+          },
+          "committer": {
+            "email": "joshuef@gmail.com",
+            "name": "joshuef",
+            "username": "joshuef"
+          },
+          "distinct": true,
+          "id": "e9550c10e7cf5ff5778e32d8eeeddea09824ecec",
+          "message": "fix: network_split example node start interval increased\n\nthis helps solidify network startup.",
+          "timestamp": "2022-04-19T18:06:08+02:00",
+          "tree_id": "c9e8e8317c1f3f3deb0ba99650deaecdbbe5478f",
+          "url": "https://github.com/maidsafe/safe_network/commit/e9550c10e7cf5ff5778e32d8eeeddea09824ecec"
+        },
+        "date": 1650386805741,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "upload-sampling/upload and read 3072b",
+            "value": 10595772190,
+            "range": "± 11492923238",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 1mb",
+            "value": 3854360116,
+            "range": "± 156066345",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 10mb",
+            "value": 9454732251,
+            "range": "± 232399915",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 3072b",
+            "value": 10040193644,
+            "range": "± 2219062592",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 1mb",
+            "value": 4095784163,
+            "range": "± 2088158105",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 10mb",
+            "value": 4710870422,
+            "range": "± 240679584",
             "unit": "ns/iter"
           }
         ]
