107bf9704ec25d9f0157ac38ff05353605da51e7
Chris O'Neil <chriso83@protonmail.com>
2022-05-12 15:35:53 +0100

    fix: publishing script not exiting correctly
    In the last release run, publishing of crates were successful with the retry loop; however, the
    script didn't exit correctly on the successful publish. The loop then continued until the retries
    were exceeded.
    
    The problem was the use of the sub shell. I use this from habit whenever a script is changing
    directories, as the change only applies inside the sub shell, meaning the outer script will retain
    its current directory, which is often desirable behaviour. The use of `exit 0` inside the sub shell
    only exits the sub shell, not the entire script, as was intended.
    
    I've now removed the use of the sub shell; it wasn't really a necessary protection for this scenario
    anyway.

--- resources/scripts/publish_crate_with_retries.sh
+++ resources/scripts/publish_crate_with_retries.sh
@@ -6,20 +6,18 @@ if [[ -z "$crate_name" ]]; then
   exit 1
 fi
 
+cd $crate_name
 count=1
 max_retries=15
 while [[ $count -le $max_retries ]]
 do
-  (
-    echo "Attempt $count to publish $crate_name..."
-    cd $crate_name
-    cargo publish --allow-dirty
-    exit_code=$?
-    if [[ $exit_code -eq 0 ]]; then
-      echo "Successfully published $crate_name"
-      exit 0
-    fi
-  )
+  echo "Attempt $count to publish $crate_name..."
+  cargo publish --allow-dirty
+  exit_code=$?
+  if [[ $exit_code -eq 0 ]]; then
+    echo "Successfully published $crate_name"
+    exit 0
+  fi
   ((count++))
   echo "Publishing failed. Will sleep for 5 seconds then retry."
   sleep 5
