23802f8e357831b0166307934ca19658d9107039
Chris O'Neil <chriso83@protonmail.com>
2022-06-09 21:00:54 +0100

    feat!: extend wallet_deposit for owned dbcs
    BREAKING CHANGE: the `wallet_deposit` function now provides an `Option<bls::SecretKey>` argument for
    providing a secret key when depositing an owned DBC.
    
    The owned DBC is changed to a bearer by providing the secret key, and the resulting DBC is then
    stored in the wallet. This means you don't need to provide the secret key at reissue time, where
    there is an issue with mapping input DBCs to secret keys.
    
    A couple of misc changes:
    * Extra test cases were added here to cover assigning the name to the deposit.
    * More instances of "Wallet" were converted to "wallet", because "wallet" is not a proper noun.
    
    As an unrelated change, this commit also provides more test coverage for `keys show` command. This
    isn't a hugely important command, but I just wanted to ensure we don't break support for BLS keys.
    
    These are integration tests because you really need to parse the console output, which you can't do
    with unit tests.
    
    This should probably have been its own commit, but it was accidentally added via an amend, then the
    API changes were also amended on top, so it was too difficult to break out.

--- sn_cli/tests/cli_keys.rs
+++ sn_cli/tests/cli_keys.rs
@@ -9,7 +9,7 @@
 use assert_cmd::prelude::*;
 use color_eyre::{eyre::eyre, Result};
 use predicates::prelude::*;
-use sn_api::{PublicKey, SafeUrl};
+use sn_api::SafeUrl;
 use sn_cmd_test_utilities::util::{parse_keys_create_output, safe_cmd_stdout, CLI, SAFE_PROTOCOL};
 use std::process::Command;
 
@@ -27,12 +27,85 @@ fn calling_safe_keys_create_pretty() -> Result<()> {
 }
 
 #[test]
-fn calling_safe_keys_create() -> Result<()> {
+fn keys_create_with_json_should_output_keys_and_url() -> Result<()> {
     let pk_cmd_result = safe_cmd_stdout(["keys", "create", "--json"], Some(0))?;
-    let (_, (pk_hex, _)): (SafeUrl, (String, String)) = parse_keys_create_output(&pk_cmd_result)?;
-    let result = PublicKey::bls_from_hex(&pk_hex);
-    // At the moment there is a problem parsing a BLS SecretKey from hex, so for now we'll just
-    // parse the public one.
-    assert!(result.is_ok());
+    let (_, (pk_hex, sk_hex)): (SafeUrl, (String, String)) =
+        parse_keys_create_output(&pk_cmd_result)?;
+    // You could write these using `assert!(result.is_ok())` but the problem is you don't get the
+    // failure message, so we do it in this more verbose fashion. We don't actually care what the
+    // parsed values are, just that the outputs from the command get parsed correctly.
+    match bls::PublicKey::from_hex(&pk_hex.trim()) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(eyre!(e));
+        }
+    }
+    match bls::SecretKey::from_hex(&sk_hex.trim()) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(eyre!(e));
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn keys_show_should_output_url_and_public_key() -> Result<()> {
+    let output = safe_cmd_stdout(["keys", "show", "--json"], Some(0))?;
+    let lines: Vec<&str> = output.split("\n").collect();
+    let (_, url) = lines[1]
+        .split_once(':')
+        .ok_or_else(|| eyre!("the output should contain a URL"))?;
+    let (_, pk_hex) = lines[2]
+        .split_once(':')
+        .ok_or_else(|| eyre!("the output should contain a public key in hex"))?;
+
+    match SafeUrl::from_url(url.trim()) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(eyre!(e));
+        }
+    }
+    match bls::PublicKey::from_hex(&pk_hex.trim()) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(eyre!(e));
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn keys_show_should_output_url_public_key_and_secret_key() -> Result<()> {
+    let output = safe_cmd_stdout(["keys", "show", "--show-sk", "--json"], Some(0))?;
+    let lines: Vec<&str> = output.split("\n").collect();
+    let (_, url) = lines[1]
+        .split_once(':')
+        .ok_or_else(|| eyre!("the output should contain a URL"))?;
+    let (_, pk_hex) = lines[2]
+        .split_once(':')
+        .ok_or_else(|| eyre!("the output should contain a public key in hex"))?;
+    let (_, sk_hex) = lines[3]
+        .split_once(':')
+        .ok_or_else(|| eyre!("the output should contain a secret key in hex"))?;
+
+    match SafeUrl::from_url(url.trim()) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(eyre!(e));
+        }
+    }
+    match bls::PublicKey::from_hex(&pk_hex.trim()) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(eyre!(e));
+        }
+    }
+    match bls::SecretKey::from_hex(&sk_hex.trim()) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(eyre!(e));
+        }
+    }
     Ok(())
 }
