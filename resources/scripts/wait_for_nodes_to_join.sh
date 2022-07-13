78b6ae113e648d15cf6b09dc097649e5bab74206
Josh Wilson <joshuef@gmail.com>
2022-07-13 16:37:18 +0200

    chore: increase wait on ci & decrease delay before node retry

--- resources/scripts/wait_for_nodes_to_join.sh
+++ resources/scripts/wait_for_nodes_to_join.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Waiting for all nodes to join..."
-until ./resources/scripts/all_nodes_joined.sh || $( sleep 5 && false ); do :; done
+until ./resources/scripts/all_nodes_joined.sh || $( sleep 15 && false ); do :; done
