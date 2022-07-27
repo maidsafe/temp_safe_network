e152f423c37b7e818fef7c456e936a13a1a4a2eb
oetyng <oetyng@gmail.com>
2022-07-27 22:28:21 +0200

    chore: run periodic checks on time Instead of running when connection arrives.

--- sn_node/src/node/mod.rs
+++ sn_node/src/node/mod.rs
@@ -233,7 +233,7 @@ mod core {
             info!("Creating DysfunctionDetection checks");
             let node_dysfunction_detector = DysfunctionDetection::new(
                 network_knowledge
-                    .adults()
+                    .members()
                     .iter()
                     .map(|peer| peer.name())
                     .collect::<Vec<XorName>>(),
