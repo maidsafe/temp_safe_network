c44fe93b0acbf3edd4760e9ea0c3706f95cf9935
Josh Wilson <joshuef@gmail.com>
2022-08-04 17:59:51 +0200

    chore: remove CmdCtrl.clone
    We move Cmd handling into main flowCtrl loop to avoid
    a clone and its knock on consequences of Arc/Rw

--- sn_node/src/node/error.rs
+++ sn_node/src/node/error.rs
@@ -27,6 +27,8 @@ pub type Result<T, E = Error> = std::result::Result<T, E>;
 #[derive(Debug, Error)]
 #[allow(missing_docs)]
 pub enum Error {
+    #[error("CmdCtrl has stopped.")]
+    CmdCtrlStopped,
     // We shortcircuit and ignore older len prefixes to avoid that
     // during a split DKG messages are still ongoing post-split
     // and are sent to the neighbouring section, which causes an AE loop as
