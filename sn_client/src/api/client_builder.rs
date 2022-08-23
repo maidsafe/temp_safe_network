011e952d08e529a78724f1679b1bd18425c5d405
Josh Wilson <joshuef@gmail.com>
2022-08-23 09:48:27 +0200

    chore: reduce query timeout noe we;re faster in general

--- sn_client/src/api/client_builder.rs
+++ sn_client/src/api/client_builder.rs
@@ -38,7 +38,7 @@ pub const ENV_AE_WAIT: &str = "SN_AE_WAIT";
 /// Bind by default to all network interfaces on a OS assigned port
 pub const DEFAULT_LOCAL_ADDR: (Ipv4Addr, u16) = (Ipv4Addr::UNSPECIFIED, 0);
 /// Default timeout to use before timing out queries and commands
-pub const DEFAULT_QUERY_CMD_TIMEOUT: Duration = Duration::from_secs(120);
+pub const DEFAULT_QUERY_CMD_TIMEOUT: Duration = Duration::from_secs(30);
 /// Default timeout for waiting for potential Anti-Entropy messages
 pub const DEFAULT_ACK_WAIT: Duration = Duration::from_secs(10);
 
