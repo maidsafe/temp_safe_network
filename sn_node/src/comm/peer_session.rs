0855fd3e3d4366fcca9f774f8cb7eee2a994b82f
Josh Wilson <joshuef@gmail.com>
2022-09-06 13:35:39 +0200

    chore: be dumb about SendMsg cmd.
    Separating out service msgs here means some may never be sent
    under heavy loads

--- sn_node/src/comm/peer_session.rs
+++ sn_node/src/comm/peer_session.rs
@@ -29,7 +29,7 @@ use tokio::{sync::mpsc, time::Instant};
 
 /// These retries are how may _new_ connection attempts do we make.
 /// If we fail all of these, HandlePeerFailedSend will be triggered
-/// for section nodes, which in turn kicks off a ConnectivityTest
+/// for section nodes, which in turn kicks off Dysfunction tracking
 const MAX_SENDJOB_RETRIES: usize = 3;
 const DEFAULT_DESIRED_RATE: f64 = 10.0; // 10 msgs / s
 
@@ -188,6 +188,7 @@ impl PeerSessionWorker {
     }
 
     async fn send(&mut self, mut job: SendJob) -> SessionStatus {
+        let id = job.msg_id;
         if job.retries > MAX_SENDJOB_RETRIES {
             job.reporter.send(SendStatus::MaxRetriesReached);
             return SessionStatus::Ok;
@@ -217,14 +218,14 @@ impl PeerSessionWorker {
                 return SessionStatus::Terminating;
             }
             Err(err) => {
-                warn!("Transient error while attempting to send, re-enqueing job {err:?}");
+                warn!("Transient error while attempting to send, re-enqueing job {id:?} {err:?}");
                 job.reporter
                     .send(SendStatus::TransientError(format!("{err:?}")));
 
                 job.retries += 1;
 
                 if let Err(e) = self.queue.send(SessionCmd::Send(job)).await {
-                    warn!("Failed to re-enqueue job after transient error {e:?}");
+                    warn!("Failed to re-enqueue job {id:?} after transient error {e:?}");
                     return SessionStatus::Terminating;
                 }
             }
