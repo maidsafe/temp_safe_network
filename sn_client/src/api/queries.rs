c8a1ed5e333637ee7d1b7a90b71f15f3e622d2b0
Josh Wilson <joshuef@gmail.com>
2022-09-15 19:43:09 +0200

    chore(client): write query responses as they come in
    as opposed to requiring a channel to be in place.

--- sn_client/src/api/queries.rs
+++ sn_client/src/api/queries.rs
@@ -6,7 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use std::collections::BTreeSet;
+// use std::collections::BTreeSet;
 
 use super::Client;
 use crate::{connections::QueryResult, errors::Error};
@@ -20,8 +20,9 @@ use sn_interface::{
     types::{Peer, PublicKey, Signature},
 };
 
+use backoff::{backoff::Backoff, ExponentialBackoff};
 use bytes::Bytes;
-use rand::Rng;
+use tokio::time::Duration;
 use tracing::{debug, info_span};
 
 impl Client {
@@ -58,12 +59,12 @@ impl Client {
             variant: query,
         };
 
-        let mut rng = rand::rngs::OsRng;
+        // let mut rng = rand::rngs::OsRng;
         // Add jitter so not all clients retry at the same rate. This divider will knock on to the overall retry window
         // and should help prevent elders from being conseceutively overwhelmed
-        let jitter = rng.gen_range(1.0..1.5);
-        let attempt_timeout = self.query_timeout.div_f32(retry_count as f32 + jitter);
-        trace!("Setting up query retry, interval is: {:?}", attempt_timeout);
+        // let jitter = rng.gen_range(1.0..1.5);
+        // let attempt_timeout = self.query_timeout.div_f32(retry_count as f32 + jitter);
+        trace!("Setting up query retry");
 
         let span = info_span!("Attempting a query");
         let _ = span.enter();
@@ -71,89 +72,63 @@ impl Client {
         let dst = query.variant.dst_name();
         // should we force a fresh connection to the nodes?
         let force_new_link = false;
-        let mut data_not_found_count = BTreeSet::default();
+        // let mut data_not_found_count = BTreeSet::default();
+
+        let op_limit = self.query_timeout;
+
+        let mut backoff = ExponentialBackoff {
+            initial_interval: Duration::from_millis(500),
+            max_interval: Duration::from_secs(20),
+            max_elapsed_time: Some(op_limit),
+            randomization_factor: 1.5,
+            ..Default::default()
+        };
+
+        // this seems needed for custom settings to take effect
+        backoff.reset();
+
         loop {
             let msg = ServiceMsg::Query(query.clone());
             let serialised_query = WireMsg::serialize_msg_payload(&msg)?;
             let signature = self.keypair.sign(&serialised_query);
             debug!(
-                "Attempting {:?} (attempt #{}) with a query timeout of {:?}",
-                query, attempts, attempt_timeout
+                "Attempting {:?} (attempt #{}) will force new: {force_new_link}",
+                query, attempts
             );
 
             // grab up to date destination section from our local network knowledge
             let (section_pk, elders) = self.session.get_query_elders(dst).await?;
 
-            let res = tokio::time::timeout(
-                attempt_timeout,
-                self.send_signed_query_to_section(
+            let res = self
+                .send_signed_query_to_section(
                     query.clone(),
                     client_pk,
                     serialised_query.clone(),
                     signature.clone(),
                     Some((section_pk, elders.clone())),
                     force_new_link,
-                ),
-            )
-            .await;
-
-            match res {
-                Ok(Ok(query_result)) => {
-                    if query_result.is_data_not_found() {
-                        let _existed = data_not_found_count.insert(query.adult_index);
-                        // if we've received DataNotFound from each adult_index,
-                        // we can return that error
-                        if data_not_found_count.len() == data_copy_count() {
-                            return Ok(query_result);
-                        }
-                    } else {
-                        return Ok(query_result);
-                    }
-
-                    warn!(
-                        "{query:?} DataNotFound returned from adult_index: {:?}",
-                        query.adult_index
-                    )
-                }
-                Ok(Err(err)) if attempts > self.max_retries => {
-                    debug!(
-                        "Retries ({}) all failed returning no response for {:?}",
-                        self.max_retries, query
-                    );
-                    break Err(Error::NoResponseAfterRetrying {
-                        query,
-                        attempts,
-                        last_error: Box::new(err),
-                    });
-                }
-                Err(_) if attempts > self.max_retries => {
-                    // this should be due to our tokio time out, rather than an error
-                    // returned by `send_signed_query_to_section`
-                    debug!(
-                        "Retries ({}) all failed returning no response for {:?}",
-                        self.max_retries, query
-                    );
-                    break Err(Error::NoResponseAfterRetrying {
-                        query,
-                        attempts,
-                        last_error: Box::new(Error::NoResponse(elders)),
-                    });
-                }
-                _ => {
-                    if attempts > self.max_retries / 3 {
-                        // any further attempts should use fresh links in case of issues
-                        // force_new_link = true;
-                    }
-                }
-            }
+                )
+                .await;
 
             attempts += 1;
 
+            if let Ok(result) = res {
+                debug!("{query:?} sent and received okay");
+                return Ok(result);
+            }
             // In the next attempt, try the next adult, further away.
             query.adult_index += 1;
             // There should not be more than a certain amount of adults holding copies of the data. Retry the closest adult again.
             if query.adult_index >= data_copy_count() {
                 query.adult_index = 0;
+            }
+
+            if let Some(delay) = backoff.next_backoff() {
+                debug!("Sleeping for {delay:?} before trying query {query:?} again");
+                tokio::time::sleep(delay).await;
+            } else {
+                // we're done trying
+                return res;
             }
         }
     }
