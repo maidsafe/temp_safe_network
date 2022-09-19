78502bd263a0f267d67f051dcbe5fd016072d5b3
Josh Wilson <joshuef@gmail.com>
2022-09-19 11:50:37 +0200

    ci: no retries in ci profile

--- sn_client/src/api/file_apis.rs
+++ sn_client/src/api/file_apis.rs
@@ -353,6 +353,7 @@ mod tests {
         init_logger();
         let file = random_bytes(MIN_ENCRYPTABLE_BYTES);
 
+        panic!("nooo");
         use crate::api::data::encrypt_large;
         let (first_address, mut first_chunks) = encrypt_large(file.clone())?;
 
