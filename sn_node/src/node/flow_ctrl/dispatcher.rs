af05a439aaaab8bf176b5e159b5eb2e3b9822504
Josh Wilson <joshuef@gmail.com>
2022-09-19 17:42:13 +0200

    feat(node): make sessions return if they're empty, and if so, clean them up then

--- sn_node/src/node/flow_ctrl/dispatcher.rs
+++ sn_node/src/node/flow_ctrl/dispatcher.rs
@@ -47,8 +47,7 @@ impl Dispatcher {
 
         match cmd {
             Cmd::CleanupPeerLinks => {
-                let members = { self.node.read().await.network_knowledge.section_members() };
-                self.comm.cleanup_peers(members).await;
+                self.comm.cleanup_peers().await;
                 Ok(vec![])
             }
             Cmd::SendMsg {
