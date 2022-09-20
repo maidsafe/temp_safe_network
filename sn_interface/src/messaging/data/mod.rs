486e09cb3d75aa0e1bd31bbaeab3096a68ee2690
Josh Wilson <joshuef@gmail.com>
2022-09-20 13:30:21 +0200

    chore(client): deem spentproof shares < 5 data not found

--- sn_interface/src/messaging/data/mod.rs
+++ sn_interface/src/messaging/data/mod.rs
@@ -237,6 +237,10 @@ impl QueryResponse {
     /// Returns true if the result returned is DataNotFound
     pub fn is_data_not_found(&self) -> bool {
         use QueryResponse::*;
+        if let SpentProofShares((Ok(vec), _)) = self {
+            return vec.len() < 5;
+        }
+
         matches!(
             self,
             GetChunk(Err(Error::DataNotFound(_)))
