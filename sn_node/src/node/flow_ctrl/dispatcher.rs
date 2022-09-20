6d5803e312228a0d85b57e2dd2bf345c025ebb3e
bochaco <gabrielviganotti@gmail.com>
2022-09-20 14:34:01 -0300

    refactor(messaging)!: removing op id from query response
    - Use the query msg id to generate the operation id to track the response from Adults
    - Remove peers from pending data queries when response was obtained from Adults
    - Removing correlation id from SystemMsg node query/response
    - Redefine system::NodeQueryResponse type just as an alias to data::QueryResponse

--- sn_node/src/node/flow_ctrl/dispatcher.rs
+++ sn_node/src/node/flow_ctrl/dispatcher.rs
@@ -103,6 +103,7 @@ impl Dispatcher {
                 Ok(vec![])
             }
             Cmd::AddToPendingQueries {
+                msg_id,
                 operation_id,
                 origin,
                 target_adult,
@@ -120,11 +121,15 @@ impl Dispatcher {
                     .pending_data_queries
                     .get_mut(&(operation_id, origin.name()))
                 {
-                    let _ = peers.insert(origin);
+                    trace!(
+                        "Adding to pending data queries for op id: {:?}",
+                        operation_id
+                    );
+                    let _ = peers.insert((msg_id, origin));
                 } else {
                     let _prior_value = node.pending_data_queries.set(
                         (operation_id, target_adult),
-                        BTreeSet::from([origin]),
+                        BTreeSet::from([(msg_id, origin)]),
                         None,
                     );
                 }
