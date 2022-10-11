cacfc6d01490729ba49bba8f580376b0d970f6c7
Benno Zeeman <bzeeman@live.nl>
2022-10-11 16:43:26 +0200

    feat: use bi stream from client; process in Node

--- sn_node/src/node/flow_ctrl/tests/mod.rs
+++ sn_node/src/node/flow_ctrl/tests/mod.rs
@@ -128,6 +128,7 @@ async fn membership_churn_starts_on_join_request_from_relocated_node() -> Result
                 Cmd::ValidateMsg {
                     origin: relocated_node.peer(),
                     wire_msg,
+                    send_stream: None,
                 },
                 &dispatcher,
             )
@@ -528,6 +529,7 @@ async fn ae_msg_from_the_future_is_handled() -> Result<()> {
                 Cmd::ValidateMsg {
                     origin: old_node.peer(),
                     wire_msg,
+                    send_stream: None,
                 },
                 &dispatcher,
             )
@@ -593,6 +595,7 @@ async fn untrusted_ae_msg_errors() -> Result<()> {
                     Cmd::ValidateMsg {
                         origin: sender.peer(),
                         wire_msg,
+                        send_stream: None,
                     },
                     &dispatcher,
                 )
