55ef52c7864bebeffcb65eaf372c2d47c6d9941a
RolandSherwin <RolandSherwin@protonmail.com>
2022-10-12 12:49:28 +0530

    tests(section_peers): unit tests for `SectionPeers`

--- sn_interface/src/network_knowledge/section_peers.rs
+++ sn_interface/src/network_knowledge/section_peers.rs
@@ -193,11 +193,221 @@ impl SectionPeers {
         proof_chain: &SectionsDAG,
         last_key: &bls::PublicKey,
     ) -> Result<()> {
-        let mut last_section_keys = proof_chain.get_ancestors(last_key)?;
-        last_section_keys.truncate(ELDER_CHURN_EVENTS_TO_PRUNE_ARCHIVE - 1);
-        last_section_keys.push(*last_key);
+        let mut latest_section_keys = proof_chain.get_ancestors(last_key)?;
+        latest_section_keys.truncate(ELDER_CHURN_EVENTS_TO_PRUNE_ARCHIVE - 1);
+        latest_section_keys.push(*last_key);
         self.archive
-            .retain(|_, node_state| last_section_keys.contains(&node_state.sig.public_key));
+            .retain(|_, node_state| latest_section_keys.contains(&node_state.sig.public_key));
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{SectionPeers, SectionsDAG};
+    use crate::{
+        messaging::system::{MembershipState, RelocateDetails, SectionSigned},
+        network_knowledge::{
+            sections_dag::tests::assert_lists,
+            test_utils::{gen_addr, section_signed},
+            NodeState,
+        },
+        types::{Peer, SecretKeySet},
+    };
+    use eyre::Result;
+    use rand::thread_rng;
+    use xor_name::{XorName, XOR_NAME_LEN};
+
+    #[test]
+    fn retain_archived_members_of_the_latest_sections_while_pruning() -> Result<()> {
+        let mut rng = thread_rng();
+        let section_peers = SectionPeers::default();
+
+        // adding node set 1
+        let sk_1 = SecretKeySet::random(None).secret_key().clone();
+        let nodes_1 = gen_random_signed_node_states(1, MembershipState::Left, &sk_1)?;
+        nodes_1.iter().for_each(|node| {
+            section_peers.update(node.clone());
+        });
+        let mut proof_chain = SectionsDAG::new(sk_1.public_key());
+        // 1 should be retained
+        section_peers.prune_members_archive(&proof_chain, &sk_1.public_key())?;
+        assert_lists(
+            section_peers
+                .archive
+                .iter()
+                .map(|item| item.value().clone()),
+            nodes_1.clone(),
+        )?;
+
+        // adding node set 2 as MembershipState::Relocated
+        let sk_2 = SecretKeySet::random(None).secret_key().clone();
+        let relocate = RelocateDetails {
+            previous_name: XorName::random(&mut rng),
+            dst: XorName::random(&mut rng),
+            dst_section_key: bls::SecretKey::random().public_key(),
+            age: 10,
+        };
+        let nodes_2 = gen_random_signed_node_states(
+            1,
+            MembershipState::Relocated(Box::new(relocate)),
+            &sk_2,
+        )?;
+        nodes_2.iter().for_each(|node| {
+            section_peers.update(node.clone());
+        });
+        let sig = bincode::serialize(&sk_2.public_key()).map(|bytes| sk_1.sign(&bytes))?;
+        proof_chain.insert(&sk_1.public_key(), sk_2.public_key(), sig)?;
+        // 1 -> 2 should be retained
+        section_peers.prune_members_archive(&proof_chain, &sk_2.public_key())?;
+        assert_lists(
+            section_peers
+                .archive
+                .iter()
+                .map(|item| item.value().clone()),
+            nodes_1.iter().cloned().chain(nodes_2.clone()),
+        )?;
+
+        // adding node set 3
+        let sk_3 = SecretKeySet::random(None).secret_key().clone();
+        let nodes_3 = gen_random_signed_node_states(1, MembershipState::Left, &sk_3)?;
+        nodes_3.iter().for_each(|node| {
+            section_peers.update(node.clone());
+        });
+        let sig = bincode::serialize(&sk_3.public_key()).map(|bytes| sk_2.sign(&bytes))?;
+        proof_chain.insert(&sk_2.public_key(), sk_3.public_key(), sig)?;
+        // 1 -> 2 -> 3 should be retained
+        section_peers.prune_members_archive(&proof_chain, &sk_3.public_key())?;
+        assert_lists(
+            section_peers
+                .archive
+                .iter()
+                .map(|item| item.value().clone()),
+            nodes_1
+                .iter()
+                .cloned()
+                .chain(nodes_2.clone())
+                .chain(nodes_3.clone()),
+        )?;
+
+        // adding node set 4
+        let sk_4 = SecretKeySet::random(None).secret_key().clone();
+        let nodes_4 = gen_random_signed_node_states(1, MembershipState::Left, &sk_4)?;
+        nodes_4.iter().for_each(|node| {
+            section_peers.update(node.clone());
+        });
+        let sig = bincode::serialize(&sk_4.public_key()).map(|bytes| sk_3.sign(&bytes))?;
+        proof_chain.insert(&sk_3.public_key(), sk_4.public_key(), sig)?;
+        //  2 -> 3 -> 4 should be retained
+        section_peers.prune_members_archive(&proof_chain, &sk_4.public_key())?;
+        assert_lists(
+            section_peers
+                .archive
+                .iter()
+                .map(|item| item.value().clone()),
+            nodes_2
+                .iter()
+                .cloned()
+                .chain(nodes_3.clone())
+                .chain(nodes_4),
+        )?;
+
+        // adding node set 5 as a branch to 3
+        // 1 -> 2 -> 3 -> 4
+        //              |
+        //              -> 5
+        let sk_5 = SecretKeySet::random(None).secret_key().clone();
+        let nodes_5 = gen_random_signed_node_states(1, MembershipState::Left, &sk_5)?;
+        nodes_5.iter().for_each(|node| {
+            section_peers.update(node.clone());
+        });
+        let sig = bincode::serialize(&sk_5.public_key()).map(|bytes| sk_3.sign(&bytes))?;
+        proof_chain.insert(&sk_3.public_key(), sk_5.public_key(), sig)?;
+        // 2 -> 3 -> 5 should be retained
+        section_peers.prune_members_archive(&proof_chain, &sk_5.public_key())?;
+        assert_lists(
+            section_peers
+                .archive
+                .iter()
+                .map(|item| item.value().clone()),
+            nodes_2.iter().cloned().chain(nodes_3).chain(nodes_5),
+        )?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn archived_members_should_not_be_moved_to_joined_state() -> Result<()> {
+        let section_peers = SectionPeers::default();
+        let sk = SecretKeySet::random(None).secret_key().clone();
+        let node_left = gen_random_signed_node_states(1, MembershipState::Left, &sk)?[0].clone();
+        assert!(section_peers.update(node_left.clone()));
+
+        let node_joined = section_signed(&sk, NodeState::joined(*node_left.peer(), None))?;
+        assert!(!section_peers.update(node_joined));
+
+        assert_lists(
+            section_peers
+                .archive
+                .iter()
+                .map(|item| item.value().clone()),
+            [node_left],
+        )?;
+        assert!(section_peers.members().is_empty());
+        Ok(())
+    }
+
+    #[test]
+    fn members_should_be_updated_to_reflect_higher_age() -> Result<()> {
+        let mut rng = thread_rng();
+        let section_peers = SectionPeers::default();
+
+        let sk = SecretKeySet::random(None).secret_key().clone();
+        let addr = gen_addr();
+        let mut name = XorName::random(&mut rng);
+        // initially set a low age for the node
+        name.0[XOR_NAME_LEN - 1] = 5;
+        let peer = Peer::new(name, addr);
+        let node = NodeState::joined(peer, None);
+        let node = section_signed(&sk, node)?;
+        assert!(section_peers.update(node.clone()));
+
+        // increase the age of the node
+        name.0[XOR_NAME_LEN - 1] = 10;
+        let peer = Peer::new(name, addr);
+        let node_older = NodeState::joined(peer, Some(node.name()));
+        let node_older = section_signed(&sk, node_older)?;
+        assert!(section_peers.update(node_older));
+
+        let member = section_peers.members().into_iter().collect::<Vec<_>>()[0].clone();
+        assert_eq!(member.age(), 10);
+
+        Ok(())
+    }
+
+    // Test helpers
+    // generate node states signed by a section's sk
+    fn gen_random_signed_node_states(
+        num_nodes: usize,
+        membership_state: MembershipState,
+        secret_key: &bls::SecretKey,
+    ) -> Result<Vec<SectionSigned<NodeState>>> {
+        let mut rng = thread_rng();
+        let mut signed_node_states = Vec::new();
+        for _ in 0..num_nodes {
+            let addr = gen_addr();
+            let name = XorName::random(&mut rng);
+            let peer = Peer::new(name, addr);
+            let node_state = match membership_state {
+                MembershipState::Joined => NodeState::joined(peer, None),
+                MembershipState::Left => NodeState::left(peer, None),
+                MembershipState::Relocated(ref details) => {
+                    NodeState::relocated(peer, None, (**details).clone())
+                }
+            };
+            let sig = section_signed(secret_key, node_state)?;
+            signed_node_states.push(sig);
+        }
+        Ok(signed_node_states)
     }
 }
