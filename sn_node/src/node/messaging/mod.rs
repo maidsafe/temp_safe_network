8368c402bf5b305279b44d8c28cbb497c4bec333
Benno Zeeman <bzeeman@live.nl>
2022-10-13 15:08:54 +0200

    feat: send client msg back on bidi stream

--- sn_node/src/node/messaging/mod.rs
+++ sn_node/src/node/messaging/mod.rs
@@ -126,6 +126,7 @@ impl MyNode {
                             Error::CannotHandleQuery(query.clone()),
                             origin,
                             msg_id,
+                            send_stream,
                             #[cfg(feature = "traceroute")]
                             wire_msg.traceroute(),
                         );
@@ -158,6 +159,7 @@ impl MyNode {
                         msg,
                         auth,
                         origin,
+                        send_stream.clone(),
                         #[cfg(feature = "traceroute")]
                         wire_msg.traceroute(),
                     )
@@ -170,6 +172,7 @@ impl MyNode {
                             err,
                             origin,
                             msg_id,
+                            send_stream,
                             #[cfg(feature = "traceroute")]
                             wire_msg.traceroute(),
                         );
