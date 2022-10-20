d2a1e5341fbaaa6ec06038f2d64f0e95d88170ac
bochaco <gabrielviganotti@gmail.com>
2022-10-20 12:55:30 -0300

    refactor(client): verify each Chunk individually in the `upload_and_verify` API
    - This shouldn't only reduce the amount of memory used when uploading large files,
    but it shall also be more precise as to when a Chunk is ready for verification after
    uploading it, and which chunk specifically failed the verification.
    - Sending a DataCmd now is not retried but only make sure it returns within
    the configured cmd timeout window.
    - Using `expect()` in some client file tests to make sure Rust return useful info when errors
    occur, there is an issue with Rust test when Err is returned not showing enough failure info.

--- sn_client/src/api/cmds.rs
+++ sn_client/src/api/cmds.rs
@@ -12,120 +12,27 @@ use crate::Error;
 use sn_interface::{
     messaging::{
         data::{ClientMsg, DataCmd},
-        ClientAuth, MsgId, WireMsg,
+        ClientAuth, WireMsg,
     },
     types::{PublicKey, Signature},
 };
 
-use backoff::{backoff::Backoff, ExponentialBackoff};
 use bytes::Bytes;
 use xor_name::XorName;
 
 impl Client {
-    /// Send a Cmd to the network and await a response.
-    /// Cmds are not retried.
-    #[instrument(skip(self), level = "debug")]
-    pub async fn send_cmd_without_retry(&self, cmd: DataCmd) -> Result<(), Error> {
-        self.send_cmd_with_retry(cmd, false).await
-    }
-
     /// Sign data using the client keypair
     pub fn sign(&self, data: &[u8]) -> Signature {
         self.keypair.sign(data)
     }
 
-    // Send a Cmd to the network and await a response.
-    // Cmds are automatically retried if an error is returned
-    // This function is a private helper.
-    #[instrument(skip(self), level = "debug")]
-    async fn send_cmd_with_retry(&self, cmd: DataCmd, retry: bool) -> Result<(), Error> {
-        let client_pk = self.public_key();
-        let dst_name = cmd.dst_name();
-
-        let debug_cmd = format!("{:?}", cmd);
-
-        let serialised_cmd = {
-            let msg = ClientMsg::Cmd(cmd);
-            WireMsg::serialize_msg_payload(&msg)?
-        };
-        let signature = self.sign(&serialised_cmd);
-
-        let op_limit = self.cmd_timeout;
-        let max_interval = self.max_backoff_interval;
-
-        let mut backoff = ExponentialBackoff {
-            initial_interval: max_interval / 2,
-            max_interval,
-            max_elapsed_time: Some(op_limit),
-            randomization_factor: 0.5,
-            ..Default::default()
-        };
-
-        let msg_id = MsgId::new();
-
-        // this seems needed for custom settings to take effect
-        backoff.reset();
-
-        let span = info_span!("Attempting a cmd with total timeout of {op_limit:?}");
-        let _ = span.enter();
-
-        let mut attempt = 1;
-        let force_new_link = false;
-        loop {
-            debug!(
-                "Attempting {:?} (attempt #{}), forcing new: {force_new_link}",
-                debug_cmd, attempt
-            );
-
-            let res = self
-                .send_signed_cmd(
-                    dst_name,
-                    client_pk,
-                    msg_id,
-                    serialised_cmd.clone(),
-                    signature.clone(),
-                    force_new_link,
-                )
-                .await;
-
-            // force_new_link = true;
-
-            // no retries wanted, so we bail on first response
-            if !retry {
-                break res;
-            }
-
-            if let Ok(cmd_result) = res {
-                debug!("{debug_cmd} sent okay");
-                break Ok(cmd_result);
-            }
-
-            trace!(
-                "Failed response on {debug_cmd} cmd attempt #{attempt}: {:?}",
-                res
-            );
-
-            attempt += 1;
-
-            if let Some(delay) = backoff.next_backoff() {
-                debug!("Sleeping for {delay:?} before trying attempt {attempt} cmd {debug_cmd:?} again");
-                tokio::time::sleep(delay).await;
-            } else {
-                debug!("backoff done affter #{:?} attempts", attempt - 1);
-                // we're done trying
-                break res;
-            }
-        }
-    }
-
     /// Send a signed `DataCmd` to the network.
-    /// This is to be part of a public API, for the user to
+    /// This is part of the public API, for the user to
     /// provide the serialised and already signed cmd.
     pub async fn send_signed_cmd(
         &self,
         dst_address: XorName,
         client_pk: PublicKey,
-        msg_id: MsgId,
         serialised_cmd: Bytes,
         signature: Signature,
         force_new_link: bool,
@@ -135,24 +42,56 @@ impl Client {
             signature,
         };
 
-        self.session
-            .send_cmd(
-                dst_address,
-                auth,
-                serialised_cmd,
-                msg_id,
-                force_new_link,
-                #[cfg(feature = "traceroute")]
-                self.public_key(),
-            )
-            .await
+        tokio::time::timeout(self.cmd_timeout, async {
+            self.session
+                .send_cmd(
+                    dst_address,
+                    auth,
+                    serialised_cmd,
+                    force_new_link,
+                    #[cfg(feature = "traceroute")]
+                    self.public_key(),
+                )
+                .await
+        })
+        .await
+        .map_err(|_| Error::CmdAckValidationTimeout(dst_address))?
     }
 
-    /// Send a DataCmd to the network and await a response.
-    /// Cmds are automatically retried using exponential backoff if an error is returned.
-    /// This function is a helper private to this module.
+    /// Public API to send a `DataCmd` to the network.
+    /// The provided `DataCmd` is serialised and signed with the
+    /// keypair this Client instance has been setup with.
     #[instrument(skip_all, level = "debug", name = "client-api send cmd")]
-    pub(crate) async fn send_cmd(&self, cmd: DataCmd) -> Result<(), Error> {
-        self.send_cmd_with_retry(cmd, true).await
+    pub async fn send_cmd(&self, cmd: DataCmd) -> Result<(), Error> {
+        let client_pk = self.public_key();
+        let dst_name = cmd.dst_name();
+
+        let debug_cmd = format!("{:?}", cmd);
+        debug!("Attempting {:?}", debug_cmd);
+
+        let serialised_cmd = {
+            let msg = ClientMsg::Cmd(cmd);
+            WireMsg::serialize_msg_payload(&msg)?
+        };
+        let signature = self.sign(&serialised_cmd);
+        let force_new_link = false;
+
+        let res = self
+            .send_signed_cmd(
+                dst_name,
+                client_pk,
+                serialised_cmd,
+                signature,
+                force_new_link,
+            )
+            .await;
+
+        if res.is_ok() {
+            debug!("{debug_cmd} sent okay: {:?}", res);
+        } else {
+            trace!("Failed response on {debug_cmd} cmd: {:?}", res);
+        }
+
+        res
     }
 }
