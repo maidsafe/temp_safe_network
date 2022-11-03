d75b788a2bd9ad15160aef65efc3d94ced2671b5
Josh Wilson <joshuef@gmail.com>
2022-11-03 11:04:57 +0900

    test: remove two spend tests that ar eno longer valid in this flow

--- sn_node/src/node/cfg/config_handler.rs
+++ sn_node/src/node/cfg/config_handler.rs
@@ -463,7 +463,7 @@ fn smoke() {
     // NOTE: IF this value is being changed due to a change in the config,
     // the change in config also be handled in Config::merge()
     // and in examples/config_handling.rs
-    let expected_size = 424;
+    let expected_size = 432;
 
     assert_eq!(std::mem::size_of::<Config>(), expected_size);
 }
