b09329be395afc79c01c8276a83db02fbd8feded
Josh Wilson <joshuef@gmail.com>
2022-12-14 11:26:10 +0100

    ci: enable 40/100mb tests

--- sn_client/src/api/file_apis.rs
+++ sn_client/src/api/file_apis.rs
@@ -758,7 +758,6 @@ mod tests {
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    #[ignore = "too heavy for CI"]
     async fn store_and_read_40mb() {
         init_logger();
         let _outer_span = tracing::info_span!("store_and_read_40mb").entered();
@@ -767,7 +766,6 @@ mod tests {
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    #[ignore = "too heavy for CI"]
     async fn store_and_read_100mb() {
         init_logger();
         let _outer_span = tracing::info_span!("store_and_read_100mb").entered();
