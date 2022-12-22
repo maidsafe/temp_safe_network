48df82437361969da25e4c212a5a64e597605ac6
github-action-benchmark <github@users.noreply.github.com>
2022-12-22 10:16:04 +0000

    add Safe Network Benchmarks (cargo) benchmark result for 24e9b561f2396b944d7b02d2da453c7d9998d55d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671700030173,
+  "lastUpdate": 1671704164718,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "Safe Network Benchmarks": [
@@ -46696,6 +46696,150 @@ window.BENCHMARK_DATA = {
             "name": "read-sampling/chunk keys/4000",
             "value": 389777396,
             "range": "± 16628618",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26634292+bors[bot]@users.noreply.github.com",
+            "name": "bors[bot]",
+            "username": "bors[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "24e9b561f2396b944d7b02d2da453c7d9998d55d",
+          "message": "Merge #1916\n\n1916: fix(log): build tracing `Subscriber` without custom macros + small fixes r=RolandSherwin a=RolandSherwin\n\n- fix(log): use vector of `Layers` to build the `Subscriber`\r\n  - Replaces the macros with a vector containing boxed Layers\r\n- chore(testnet): enable otlp feature flag\r\n- fix(log): prevent panic when we have multiple tracing subscribers\r\n  - When we have the `otlp` feature enabled, we effectively have\r\n  multiple subscribers consuming the logs.\r\n  - Under rare circumstances, we get a panic with the following msg,\r\n  `Format: was already formatted once`\r\n  - Turns out it's because the `itertools::format()` is being called\r\n  multiple times\r\n  - Was not able to reproduce the issue with a minimal setup, but this\r\n  gets rid of the panic.\r\n- chore(log): remove unused event formatting option\r\n  - the `.event_format()` overrides the `.with_thread_names()` option,\r\n  hence remove it\n\nCo-authored-by: RolandSherwin <RolandSherwin@protonmail.com>",
+          "timestamp": "2022-12-22T08:51:00Z",
+          "tree_id": "63ac0fc75f03e8f2a41cbc0a923c9f9ae72ddfde",
+          "url": "https://github.com/maidsafe/safe_network/commit/24e9b561f2396b944d7b02d2da453c7d9998d55d"
+        },
+        "date": 1671704161382,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "serialize/serialize for sending",
+            "value": 2335419,
+            "range": "± 348857",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 3072b",
+            "value": 92926273,
+            "range": "± 2617211",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 1mb",
+            "value": 109342168,
+            "range": "± 5565683",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 10mb",
+            "value": 436053408,
+            "range": "± 9617095",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 3072b",
+            "value": 75445427,
+            "range": "± 1870938",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 1mb",
+            "value": 87390371,
+            "range": "± 1930631",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 10mb",
+            "value": 353117711,
+            "range": "± 6019342",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "register-edit-sampling/register_edits/1000",
+            "value": 24294531206,
+            "range": "± 249425285",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/100",
+            "value": 49447061,
+            "range": "± 14460866",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/1000",
+            "value": 1821553801,
+            "range": "± 175567907",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/4000",
+            "value": 8957985120,
+            "range": "± 1214138310",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/100",
+            "value": 290492449,
+            "range": "± 27540856",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/1000",
+            "value": 2763002045,
+            "range": "± 152059812",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/4000",
+            "value": 7422936725,
+            "range": "± 931523470",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/100",
+            "value": 46993312,
+            "range": "± 4108898",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/1000",
+            "value": 455792230,
+            "range": "± 23602190",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/4000",
+            "value": 1770658478,
+            "range": "± 73613849",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/100",
+            "value": 12337690,
+            "range": "± 537236",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/1000",
+            "value": 98906839,
+            "range": "± 5799441",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/4000",
+            "value": 335790082,
+            "range": "± 16485673",
             "unit": "ns/iter"
           }
         ]
