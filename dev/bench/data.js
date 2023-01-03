3e93a1705f5856d79d2a72d31c50b1832aa2ed0a
github-action-benchmark <github@users.noreply.github.com>
2023-01-03 07:12:29 +0000

    add Safe Network Benchmarks (cargo) benchmark result for a8227e8b3bda5f51d1de8bd39e9d7bba5705a93a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1672690873861,
+  "lastUpdate": 1672729949630,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "Safe Network Benchmarks": [
@@ -50296,6 +50296,150 @@ window.BENCHMARK_DATA = {
             "name": "read-sampling/chunk keys/4000",
             "value": 376202442,
             "range": "± 12096254",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26634292+bors[bot]@users.noreply.github.com",
+            "name": "bors[bot]",
+            "username": "bors[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "a8227e8b3bda5f51d1de8bd39e9d7bba5705a93a",
+          "message": "Merge #1941\n\n1941: Refactor proposals r=grumbach a=grumbach\n\n<!--\r\nThanks for contributing to the project! We recommend you check out our \"Guide to contributing\" page if you haven't already: https://github.com/maidsafe/QA/blob/master/CONTRIBUTING.md\r\n\r\nWrite your comment below this line: -->\r\n\r\nRefactor the `NodeMsg::Propose` into 4 distinct messages:\r\n- `NodeMsg::RequestHandover`: when nodes finish DKG and request a handover to the current elders (node->elder)\r\n- `NodeMsg::SectionHandoverPromotion`: when elders tell those nodes that they are promoted as elder (elder->node)\r\n- `NodeMsg::SectionSplitPromotion`: when elders tell those nodes that they are promoted as elder in either side of a split (elder->node)\r\n- `NodeMsg::ProposeSectionState`: when elders decide to kick nodes or accept new nodes within a section (elder->elder)\r\n\r\nThis distinction makes explicit:\r\n- who is signing\r\n- who is receiving/aggregating the signatures\r\n\r\nAll the changes are in the [refactor: proposal into multiple distinct messages](https://github.com/maidsafe/safe_network/pull/1941/commits/692135f6c803926403cf46ba3b2b747660b04d1d) commit.\r\n\r\nThe other commits are tidying up and moving code around: large diffs of nothingness!\n\nCo-authored-by: grumbach <anselmega@gmail.com>",
+          "timestamp": "2023-01-03T05:30:21Z",
+          "tree_id": "68d0683a08fb87e1273378f1ccaa12b197d7359c",
+          "url": "https://github.com/maidsafe/safe_network/commit/a8227e8b3bda5f51d1de8bd39e9d7bba5705a93a"
+        },
+        "date": 1672729946693,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "serialize/serialize for sending",
+            "value": 2417157,
+            "range": "± 12107",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 3072b",
+            "value": 85164609,
+            "range": "± 1550466",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 1mb",
+            "value": 105985366,
+            "range": "± 4260668",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 10mb",
+            "value": 394883611,
+            "range": "± 2115047",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 3072b",
+            "value": 70151138,
+            "range": "± 757835",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 1mb",
+            "value": 88206585,
+            "range": "± 1432304",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 10mb",
+            "value": 323993270,
+            "range": "± 2316852",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "register-edit-sampling/register_edits/1000",
+            "value": 23484166386,
+            "range": "± 264043480",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/100",
+            "value": 44741789,
+            "range": "± 14644559",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/1000",
+            "value": 2017971456,
+            "range": "± 182513217",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/4000",
+            "value": 7849837134,
+            "range": "± 584400500",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/100",
+            "value": 217783999,
+            "range": "± 10534824",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/1000",
+            "value": 1876002051,
+            "range": "± 192131166",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/4000",
+            "value": 7163802036,
+            "range": "± 558719111",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/100",
+            "value": 45950233,
+            "range": "± 1113190",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/1000",
+            "value": 436533066,
+            "range": "± 4119911",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/4000",
+            "value": 1689564584,
+            "range": "± 33467883",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/100",
+            "value": 11606764,
+            "range": "± 148348",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/1000",
+            "value": 96031347,
+            "range": "± 3735449",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/4000",
+            "value": 331445728,
+            "range": "± 3725825",
             "unit": "ns/iter"
           }
         ]
