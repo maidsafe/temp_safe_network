a7d2b19a05c0348840a2f933214f5b23dad2d3a0
Josh Wilson <joshuef@gmail.com>
2023-02-03 07:58:17 +0100

    chore(node): use parking lot and setup deadlock detection

--- sn_node/src/bin/sn_node/main.rs
+++ sn_node/src/bin/sn_node/main.rs
@@ -61,6 +61,33 @@ fn main() -> Result<()> {
         (rt, guard)
     };
 
+    {
+        use parking_lot::deadlock;
+        use std::thread;
+
+        // Create a background thread which checks for deadlocks every 3s
+        let _handle = thread::spawn(move || loop {
+            thread::sleep(Duration::from_secs(3));
+            let deadlocks = deadlock::check_deadlock();
+            if deadlocks.is_empty() {
+                continue;
+            }
+
+            println!("{} deadlocks detected", deadlocks.len());
+            warn!("{} deadlocks detected", deadlocks.len());
+            for (i, threads) in deadlocks.iter().enumerate() {
+                println!("Deadlock #{i}");
+                warn!("Deadlock #{i}");
+                for t in threads {
+                    println!("Thread Id {:#?}", t.thread_id());
+                    warn!("Thread Id {:#?}", t.thread_id());
+                    println!("{:#?}", t.backtrace());
+                    warn!("{:#?}", t.backtrace());
+                }
+            }
+        });
+    }
+
     loop {
         println!("Node started");
         create_runtime_and_node(&config)?;
