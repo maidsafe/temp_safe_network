892fde7ae9ce98f58bdde9d57031765a46a55220
github-action-benchmark <github@users.noreply.github.com>
2023-02-20 08:44:23 +0000

    add Safe Network Benchmarks (cargo) benchmark result for eb4f37255646015f5c6a6145b9a476afded1bf59

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676617058960,
+  "lastUpdate": 1676882663438,
   "repoUrl": "https://github.com/maidsafe/safe_network",
   "entries": {
     "Safe Network Benchmarks": [
@@ -71140,6 +71140,150 @@ window.BENCHMARK_DATA = {
             "name": "read-sampling/chunk keys/4000",
             "value": 372580225,
             "range": "± 3086966",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "26634292+bors[bot]@users.noreply.github.com",
+            "name": "bors[bot]",
+            "username": "bors[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "eb4f37255646015f5c6a6145b9a476afded1bf59",
+          "message": "Merge #2113\n\n2113: feat(client): sn_client to not have any default cmd/query timeout value set r=joshuef a=bochaco\n\nsn_client is now set to send msgs to the network without any default timeout values, the user of the lib can still set them with SN_CMD_TIMEOUT and SN_QUERY_TIMEOUT env vars according to its own requirements.\n\nCo-authored-by: bochaco <gabrielviganotti@gmail.com>",
+          "timestamp": "2023-02-20T06:47:41Z",
+          "tree_id": "9383109f3884a0a46254a16e5f39fe23bf8dcb2e",
+          "url": "https://github.com/maidsafe/safe_network/commit/eb4f37255646015f5c6a6145b9a476afded1bf59"
+        },
+        "date": 1676882659925,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "serialize/serialize for sending",
+            "value": 2325343,
+            "range": "± 1427",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 3072b",
+            "value": 78260916,
+            "range": "± 1721113",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 1mb",
+            "value": 93567699,
+            "range": "± 2617256",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload and read 10mb",
+            "value": 381534053,
+            "range": "± 1937082",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 3072b",
+            "value": 66064437,
+            "range": "± 314444",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 1mb",
+            "value": 79837824,
+            "range": "± 649943",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "upload-sampling/upload 10mb",
+            "value": 314494777,
+            "range": "± 1845938",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "register-edit-sampling/register_edits/1000",
+            "value": 23311027181,
+            "range": "± 54065391",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/100",
+            "value": 45438637,
+            "range": "± 17936989",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/1000",
+            "value": 2826229200,
+            "range": "± 500050572",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/register_writes/4000",
+            "value": 10407232378,
+            "range": "± 1618139708",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/100",
+            "value": 342469717,
+            "range": "± 73947851",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/1000",
+            "value": 1491175735,
+            "range": "± 310846918",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "write-sampling/chunk writes/4000",
+            "value": 5746344838,
+            "range": "± 167659709",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/100",
+            "value": 45496615,
+            "range": "± 493077",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/1000",
+            "value": 434759368,
+            "range": "± 1941890",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/register_keys/4000",
+            "value": 1694676924,
+            "range": "± 6770123",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/100",
+            "value": 11784197,
+            "range": "± 143174",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/1000",
+            "value": 97230212,
+            "range": "± 3619315",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read-sampling/chunk keys/4000",
+            "value": 336372068,
+            "range": "± 2012097",
             "unit": "ns/iter"
           }
         ]
