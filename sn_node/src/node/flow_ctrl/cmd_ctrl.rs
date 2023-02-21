3695987a112228e9215d6746446133156fe6c047
Josh Wilson <joshuef@gmail.com>
2023-02-21 10:14:20 +0100

    chore(node): pull MyNode up out of dispatcher

--- sn_node/src/node/flow_ctrl/cmd_ctrl.rs
+++ sn_node/src/node/flow_ctrl/cmd_ctrl.rs
@@ -8,7 +8,7 @@
 
 use crate::node::{
     flow_ctrl::{cmds::Cmd, dispatcher::Dispatcher, RejoinReason},
-    Error,
+    Error, MyNode,
 };
 
 use std::sync::{
@@ -37,13 +37,10 @@ impl CmdCtrl {
         }
     }
 
-    pub(crate) fn node(&self) -> Arc<RwLock<crate::node::MyNode>> {
-        self.dispatcher.node()
-    }
-
     /// Processes the passed in cmd on a new task
     pub(crate) async fn process_cmd_job(
         &self,
+        node: Arc<RwLock<MyNode>>,
         cmd: Cmd,
         mut id: Vec<usize>,
         node_identifier: XorName,
@@ -61,7 +58,7 @@ impl CmdCtrl {
         #[cfg(feature = "statemap")]
         sn_interface::statemap::log_state(node_identifier.to_string(), cmd.statemap_state());
 
-        match dispatcher.process_cmd(cmd).await {
+        match dispatcher.process_cmd(cmd, node).await {
             Ok(cmds) => {
                 let _handle = tokio::task::spawn(async move {
                     for (child_nr, cmd) in cmds.into_iter().enumerate() {
