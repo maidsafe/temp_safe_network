6631dbb0e982c2449d8be841fedf21951254384d
Josh Wilson <joshuef@gmail.com>
2023-03-01 16:56:57 +0100

    chore(node): membership validation logic allows relocations even when joined...

--- sn_interface/src/network_knowledge/node_state/mod.rs
+++ sn_interface/src/network_knowledge/node_state/mod.rs
@@ -93,7 +93,7 @@ impl NodeState {
         }
     }
 
-    pub fn validate(
+    pub fn validate_node_state(
         &self,
         prefix: &Prefix,
         members: &BTreeMap<XorName, Self>,
@@ -111,16 +111,19 @@ impl NodeState {
             MembershipState::Joined => {
                 if members.contains_key(&name) {
                     warn!("Rejecting join from existing member {name}");
-                    Err(Error::ExistingMemberConflict)
+                    Err(Error::ExistingMemberNameConflict)
                 } else if !section_has_room_for_node(name, prefix, members.keys().copied()) {
                     warn!("Rejecting join since we are at capacity");
                     Err(Error::TryJoinLater)
-                } else if let Some(existing_node) = members
-                    .values()
-                    .find(|n| n.peer().addr() == self.peer().addr())
-                {
+                } else if let Some(existing_node) = members.values().find(|n| {
+                    // reject if we find a node that is marked as currently 'Joined' AND has same socket.
+                    // (This check is needed to allow nodes that have been Relocated to rejoin this section
+                    // if they've been sent to our section again)
+                    n.peer().addr() == self.peer().addr()
+                        && matches!(n.state, MembershipState::Joined)
+                }) {
                     warn!("Rejecting join since we have an existing node with this address: {existing_node:?}");
-                    Err(Error::ExistingMemberConflict)
+                    Err(Error::ExistingMemberSocketAddrConflict)
                 } else if archived.contains(&name) {
                     Err(Error::ArchivedNodeRejoined)
                 } else {
