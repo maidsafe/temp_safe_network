2740c6dc5272334106dda5846d1aaf9fadf8119b
Chris O'Neil <chriso83@protonmail.com>
2023-03-10 18:28:05 +0000

    feat: reworked version of testnet binary
    The `testnet` binary was reworked to remove its dependency on `sn_launch_tool`. Since it will now be
    used as a replacement for the `node run-baby-fleming` command, the interface was updated to make it
    a bit more usable. Now it does not attempt to build the node by default: the `--build-node` flag is
    used for that. The other arguments that were added should be fairly self explanatory and can be seen
    on the diff.
    
    All the support from the original binary is retained in this rework, including the use of flamegraph
    and joining an existing network.
    
    There is a module in the crate that exposes a couple of structs that can be used for launching a
    testnet: `TestnetBuilder` and `Testnet`. The functions on `Testnet` should have almost full code
    coverage, but since the builder is a simple utility, I didn't bother putting tedious tests around
    it. Since the bulk of the work has been unit tested I also haven't added any integration tests for
    testing the code in the `main` module for the binary, since it's pretty simple and doesn't really
    seem worth the additional testing infrastructure.
    
    The merge CI workflow was updated to use the reworked version of the binary. Since we already build
    the node in the previous step, we just pass the path to it, rather than attempt to rebuild.
    
    The script for verifying node joins was modified to check for IPs matching "0.0.0.0" in addition to
    "127.0.0.1", which was being caused by the use of the `--local-addr` argument. It also now considers
    the total number of nodes to be the genesis node plus the number of non-genesis nodes. In addition,
    the script was updated to split long commands over multiple lines, use newer-style Bash syntax and
    consistent indentation size.

--- resources/scripts/all_nodes_joined.sh
+++ resources/scripts/all_nodes_joined.sh
@@ -1,47 +1,59 @@
 #!/usr/bin/env bash
 
-if ! command -v rg &> /dev/null
-then
-    echo "ripgrep could not be found and is required"
-    exit 1
+if ! command -v rg &> /dev/null; then
+  echo "ripgrep could not be found and is required"
+  exit 1
 fi
 
 DEFAULT_NODE_COUNT=30
 DEFAULT_LOG_DIR="$HOME/.safe/node/local-test-network"
 
 NODE_COUNT="${NODE_COUNT:-$DEFAULT_NODE_COUNT}"
+TOTAL=$(($NODE_COUNT + 1)) # Total is the number of nodes + genesis.
 
 log_dir="${LOG_DIR:-$DEFAULT_LOG_DIR}"
 echo
 echo "Checking nodes log files to verify all nodes have joined. Logs path: $log_dir"
 
 # -u needed here to search log dirs
-nodes=$(rg ".*connection info:.*" "$log_dir" -g "*.log*" -u | rg "(sn-node-.*).sn_node\.log.*(.{6}\(\d{8}\)).*(127\.0\.0\.1:\d{5})" -or '$2->$3 $1' --sort path)
-nodes_ips=$(rg "connection info:.*" "$log_dir" -g "*.log*" -u | rg "(.{6}\(\d{8}\)).*(127\.0\.0\.1:\d{5})" -or '$1->$2' | sort)
+# The new testnet tool counts nodes as separate from the genesis node, so we
+# exclude the genesis node from being matched.
+nodes=$(\
+  rg ".*connection info:.*" "$log_dir" -g "*.log*" -u | \
+  rg "(sn-node-.*).sn_node\.log.*(.{6}\(\d{8}\)).*((127\.0\.0\.1|0\.0\.0\.0):\d{5})" \
+    -or '$2->$3 $1' --sort path | \
+  sed "s/0\.0\.0\.0:/127\.0\.0\.1:/g") # transform 0.0.0.0 matches back to 127.0.0.1
+nodes_ips=$(\
+  rg "connection info:.*" "$log_dir" -g "*.log*" -u | \
+  rg "(.{6}\(\d{8}\)).*((127\.0\.0\.1|0\.0\.0\.0):\d{5})" -or '$1->$2' | \
+  sed "s/0\.0\.0\.0:/127\.0\.0\.1:/g" | \
+  sort)
 nodes_ips_count=$(echo "$nodes_ips" | wc -l)
 
 echo
 echo "Number of nodes: $nodes_ips_count"
 
-if [[ $nodes_ips_count -ne $NODE_COUNT ]]
-    then
-        echo "Unexpected number of joined nodes. Expected $NODE_COUNT, we have $nodes_ips_count:"
-        echo "$nodes"
-        exit 100
-    else
-        echo "All nodes have joined. Nodes names and IPs:"
-        echo "$nodes"
+if [[ $nodes_ips_count -ne $TOTAL ]]; then
+  echo "Unexpected number of joined nodes. Expected $TOTAL, we have $nodes_ips_count:"
+  echo "$nodes"
+  exit 100
+else
+  echo "All nodes have joined. Nodes names and IPs:"
+  echo "$nodes"
 fi
 
 # We'll use the logs from the nodes that joined, to obtain the
 # list of members in the network knowledge they share with AE messages.
-members=$(rg ".* msg: AntiEntropy\(AntiEntropy \{.* kind: Update \{ members: \{(.*)\} \}\)" -or '$1' "$log_dir" -g "*.log*" | rg "(?:NodeState\((.{6}\(\d{8}\)).., (127\.0\.0\.1:\d{5}), Joined)+" -or '$1->$2' | sort -u)
+members=$(\
+  rg ".* msg: AntiEntropy\(AntiEntropy \{.* kind: Update \{ members: \{(.*)\} \}\)" \
+    -or '$1' "$log_dir" -g "*.log*" | \
+  rg "(?:NodeState\((.{6}\(\d{8}\)).., ((127\.0\.0\.1|0\.0\.0\.0):\d{5}), Joined)+" -or '$1->$2' | \
+  sort -u)
 members_count=$(echo "$members" | wc -l)
 
 echo
-if [[ $members_count -ne $NODE_COUNT ]]
-then
-  echo "Unexpected number of nodes in network knowledge. Expected $NODE_COUNT, we have $members_count:"
+if [[ $members_count -ne $TOTAL ]]; then
+  echo "Unexpected number of nodes in network knowledge. Expected $TOTAL, we have $members_count:"
   echo "$members"
 else
   echo "Number of nodes found in network knowledge: $members_count"
@@ -51,26 +63,22 @@ echo
 echo "Checking which nodes in network knowledge match the list of joined nodes..."
 
 invalid_member_found=false
-for m in $members
-do
-    if grep -q "$m" <<< "$nodes_ips"
-    then
-      echo "Node $m is a valid member"
-    else
-      echo "Node $m in network knowledge was not found in the list of joined nodes"
-      invalid_member_found=true
-    fi
+for m in $members; do
+  if grep -q "$m" <<< "$nodes_ips"; then
+    echo "Node $m is a valid member"
+  else
+    echo "Node $m in network knowledge was not found in the list of joined nodes"
+    invalid_member_found=true
+  fi
 done
 
 echo
-if $invalid_member_found
-then
+if $invalid_member_found; then
   echo "At least one member in the network knowledge was found invalid"
   exit 100
 else
-  if [[ $members_count -lt $NODE_COUNT ]]
-  then
-    echo "Some joined nodes ($NODE_COUNT) not found in the network knowledge ($members_count)"
+  if [[ $members_count -lt $TOTAL ]]; then
+    echo "Some joined nodes ($TOTAL) not found in the network knowledge ($members_count)"
     exit 100
   else
     echo "All good!, members in the network knowledge match the list of joined nodes!"
