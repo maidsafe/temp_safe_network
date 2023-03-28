0b27d12ef3ea4b04e829c6e31790b00d520542a1
oetyng <oetyng@gmail.com>
2023-03-28 15:55:23 +0200

    chore: fix review comments of PR 2249

--- sn_interface/src/types/keys/secret_key.rs
+++ sn_interface/src/types/keys/secret_key.rs
@@ -97,11 +97,11 @@ pub fn bls_secret_from_hex<T: AsRef<[u8]>>(hex: T) -> Result<bls::SecretKey> {
             "Couldn't parse BLS secret key bytes from hex. The provided string must represent exactly {} bytes.",
             bls::SK_SIZE
         )))?;
-    let pk = bls::SecretKey::from_bytes(bytes_fixed_len).map_err(|err| {
+    let sk = bls::SecretKey::from_bytes(bytes_fixed_len).map_err(|err| {
         Error::FailedToParse(format!(
             "Couldn't parse BLS secret key from fixed-length byte array: {err}",
         ))
     })?;
 
-    Ok(pk)
+    Ok(sk)
 }
