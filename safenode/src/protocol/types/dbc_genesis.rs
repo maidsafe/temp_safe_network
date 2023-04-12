4646c897ae58735e728f1dc730577d506ffd0ef0
oetyng <oetyng@gmail.com>
2023-04-12 21:46:25 +0200

    chore: remove commented out code - This is fee related stuff that will be added in later.

--- safenode/src/protocol/types/dbc_genesis.rs
+++ safenode/src/protocol/types/dbc_genesis.rs
@@ -91,14 +91,6 @@ pub fn create_genesis_dbc(genesis_main_key: &MainKey) -> GenesisResult<Dbc> {
     Ok(genesis_dbc)
 }
 
-// Copyright 2023 MaidSafe.net limited.
-//
-// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
-// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
-// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied. Please review the Licences for the specific language governing
-// permissions and limitations relating to use of the SAFE Network Software.
-
 pub(super) type Result<T> = std::result::Result<T, Error>;
 
 /// Error type returned by the API
@@ -136,28 +128,11 @@ pub fn send_tokens(
     dbcs: Vec<(Dbc, DerivedKey)>,
     recipients: Vec<(Token, DbcIdSource)>,
     change_to: PublicAddress,
-    // reason: DbcReason,
-    // priority: SpendPriority,
 ) -> Result<ReissueOutputs> {
-    // let mut attempts = 0;
-    // let mut max_required = Token::zero();
-
     // We need to select the necessary number of dbcs from those that we were passed.
     // This will also account for any fees.
     let reissue_inputs = select_inputs(dbcs, recipients, change_to)?;
-
     reissue(reissue_inputs)
-    // // then we can reissue
-    // match reissue(reissue_inputs).await {
-    //     Ok(outputs) => return Ok(outputs),
-    //     Err(Error::FeeTooLow(required)) => {
-    //         max_required = required;
-    //         attempts += 1;
-    //     }
-    //     error => return error,
-    // }
-
-    // Err(Error::FeeTooLow(max_required))
 }
 
 /// The input dbcs will be spent on the network, and the resulting
@@ -173,10 +148,6 @@ fn reissue(
         input_dbcs,
         outputs,
         change: (change, change_to),
-        // #[cfg(not(feature = "data-network"))]
-        // fees_paid,
-        // #[cfg(not(feature = "data-network"))]
-        // all_fee_cipher_params,
     } = reissue_inputs;
 
     let mut inputs = vec![];
@@ -206,62 +177,13 @@ fn reissue(
         tx_builder = tx_builder.add_output(change, dbc_id_src);
     }
 
-    // let inputs_spent_proofs: BTreeSet<SpentProof> = input_dbcs
-    //     .iter()
-    //     .flat_map(|dbc| dbc.inputs_spent_proofs.clone())
-    //     .collect();
-    // let inputs_spent_transactions: BTreeSet<DbcTransaction> = input_dbcs
-    //     .iter()
-    //     .flat_map(|dbc| dbc.inputs_spent_transactions.clone())
-    //     .collect();
-
     // Finalize the tx builder to get the dbc builder.
     let dbc_builder = tx_builder
         .build(Hash::default(), &mut rng)
         .map_err(Error::DbcError)?;
 
-    // // Get fee outputs to generate the fee ciphers.
-    // #[cfg(not(feature = "data-network"))]
-    // let outputs = dbc_builder
-    //     .revealed_outputs
-    //     .iter()
-    //     .map(|output| (output.public_key, output.revealed_amount))
-    //     .collect();
-
     let inputs = dbc_builder.signed_spends();
     let spent_dbcs = inputs.iter().map(|spent| spent.dbc_id()).cloned().collect();
-
-    // // Spend all the input DBCs, collecting the spent proof shares for each of them
-    // for (public_key, tx) in inputs {
-    //     // // Generate the fee ciphers.
-    //     // #[cfg(not(feature = "data-network"))]
-    //     // let input_fee_ciphers = {
-    //     //     let fee_cipher_params = all_fee_cipher_params
-    //     //         .get(&public_key)
-    //     //         .ok_or(Error::DbcReissueError("Missing fee!".to_string()))?;
-    //     //     fee_ciphers(&outputs, fee_cipher_params)?
-    //     // };
-
-    //     let tx_hash = Hash::from(tx.hash());
-    //     // TODO: spend DBCs concurrently spawning tasks
-    //     let mut attempts = 0;
-    //     loop {
-    //         attempts += 1;
-    //         client
-    //             .spend_dbc(
-    //                 public_key,
-    //                 tx.clone(),
-    //                 reason,
-    //                 inputs_spent_proofs.clone(),
-    //                 inputs_spent_transactions.clone(),
-    //                 #[cfg(not(feature = "data-network"))]
-    //                 input_fee_ciphers.clone(),
-    //             )
-    //             .await?;
-
-    //         let signed_spend = client.get_signed_spend(public_key).await?;
-    //     }
-    // }
 
     // Perform verifications of input TX and spentproofs,
     // as well as building the output DBCs.
@@ -281,8 +203,6 @@ fn reissue(
         outputs: output_dbcs,
         change: change_dbc,
         spent_dbcs,
-        // #[cfg(not(feature = "data-network"))]
-        // fees_paid,
     })
 }
 
@@ -311,105 +231,9 @@ pub(crate) fn select_inputs(
         })?;
 
     let mut change_amount = total_output_amount;
-    // let mut rng = rng::thread_rng();
-    // #[cfg(not(feature = "data-network"))]
-    // let mut all_fee_cipher_params = BTreeMap::new();
-    // #[cfg(not(feature = "data-network"))]
-    // let mut fees_paid = Token::zero();
 
     for (dbc, derived_key) in inputs {
-        // let revealed_amount = match dbc.revealed_amount(derived_key).map_err(Error::DbcError) {
-        //     Ok(revealed) => revealed,
-        //     Err(error) => {
-        //         error!(
-        //             "Could not get the amount from dbc {:?}, wrong derived key used! {error}",
-        //             dbc.id(),
-        //         );
-        //         continue;
-        //     }
-        // };
-
         let input_key = dbc.id();
-
-        #[cfg(not(feature = "data-network"))]
-        {
-            // // ------------ fee part start ----------------
-            // #[cfg(not(feature = "data-network"))]
-            // let fee_per_input = {
-            //     // Each section will have elder_count() instances to pay individually (for now, later they will be more).
-            //     let elder_fees = match client.get_section_fees(input_key, priority).await {
-            //         Ok(fees) => fees,
-            //         Err(error) => {
-            //             error!("Could not get fees for input dbc: {input_key:?}: {error}");
-            //             continue;
-            //         }
-            //     };
-            //     let num_responses = elder_fees.len();
-            //     let required_responses = supermajority(elder_count());
-            //     if required_responses > num_responses {
-            //         warn!("Not enough elders contacted for the section to spend the input. Got: {num_responses}, needed: {required_responses}");
-            //         continue;
-            //     }
-
-            //     // Fees that were not encrypted to us.
-            //     let mut invalid_fees = BTreeSet::new();
-            //     // As the Elders encrypt the amount to our public key, we need to decrypt it.
-            //     let mut decrypted_elder_fees = vec![];
-
-            //     for (elder, fee) in elder_fees {
-            //         match fee.content.decrypt_amount(&revealed_bearer.secret_key) {
-            //             Ok(amount) => decrypted_elder_fees.push(((elder, fee), amount)),
-            //             Err(error) => {
-            //                 error!("Decrypting the fee content from {elder} failed! {error}");
-            //                 let _ = invalid_fees.insert(fee.content.elder_reward_key);
-            //             }
-            //         }
-            //     }
-
-            //     let max_invalid_fees = elder_count() - required_responses;
-            //     if invalid_fees.len() > max_invalid_fees {
-            //         let valid_responses = num_responses - invalid_fees.len();
-            //         warn!("Not enough valid fees received from the section to spend the input. Found: {valid_responses}, needed: {required_responses}", );
-            //         continue;
-            //     }
-
-            //     // Total fee paid to all recipients in the section for this input.
-            //     let fee_per_input = decrypted_elder_fees
-            //         .iter()
-            //         .fold(Some(Token::zero()), |total, (_, fee)| {
-            //             total.and_then(|t| t.checked_add(*fee))
-            //         })
-            //         .ok_or_else(|| Error::DbcReissueError(
-            //             "Overflow occurred while summing the individual Elder's fees in order to calculate the total amount for the output DBCs."
-            //                 .to_string(),
-            //         ))?;
-
-            //     let mut fee_cipher_params = BTreeMap::new();
-
-            //     // Add elders to outputs and generate their fee ciphers.
-            //     decrypted_elder_fees
-            //         .iter()
-            //         .for_each(|((elder, required_fee), fee)| {
-            //             let owner = Owner::from(required_fee.content.elder_reward_key);
-            //             let owner_once = OwnerOnce::from_owner_base(owner, &mut rng);
-            //             outputs.push((*fee, owner_once.clone()));
-
-            //             let _ =
-            //                 fee_cipher_params.insert(elder.name(), (required_fee.clone(), owner_once));
-            //         });
-
-            //     let _ = all_fee_cipher_params.insert(input_key, fee_cipher_params);
-
-            //     fees_paid = fees_paid.checked_add(fee_per_input).ok_or_else(|| {
-            //         Error::DbcReissueError(
-            //             "Overflow occurred while summing all the input fees.".to_string(),
-            //         )
-            //     })?;
-
-            //     fee_per_input
-            // };
-            // // ---------------- fee part end ----------------
-        }
 
         let dbc_balance = match dbc.revealed_amount(&derived_key) {
             Ok(revealed_amount) => Token::from_nano(revealed_amount.value()),
@@ -430,26 +254,6 @@ pub(crate) fn select_inputs(
                     .to_string(),
             )
             })?;
-
-        #[cfg(not(feature = "data-network"))]
-        {
-            // // Output amount now increases a bit, as we have to cover the fee as well..
-            // total_output_amount = total_output_amount.checked_add(fee_per_input)
-            // .ok_or_else(|| {
-            //     Error::DbcReissueError(
-            //     "Overflow occurred while adding mint fee in order to calculate the total amount for the output DBCs."
-            //         .to_string(),
-            // )
-            // })?;
-            // // ..and so does `change_amount` (that we subtract from to know if we've covered `total_output_amount`).
-            // change_amount = change_amount.checked_add(fee_per_input)
-            // .ok_or_else(|| {
-            //     Error::DbcReissueError(
-            //     "Overflow occurred while adding mint fee in order to calculate the total amount for the output DBCs."
-            //         .to_string(),
-            // )
-            // })?;
-        }
 
         // If we've already combined input DBCs for the total output amount, then stop.
         match change_amount.checked_sub(dbc_balance) {
@@ -473,10 +277,6 @@ pub(crate) fn select_inputs(
         input_dbcs,
         outputs,
         change: (change_amount, change_to),
-        // #[cfg(not(feature = "data-network"))]
-        // fees_paid,
-        // #[cfg(not(feature = "data-network"))]
-        // all_fee_cipher_params,
     })
 }
 
@@ -489,8 +289,6 @@ fn verify_amounts(total_input_amount: Token, total_output_amount: Token) -> Resu
     Ok(())
 }
 
-// type ReissueCiphers = BTreeMap<PublicKey, BTreeMap<XorName, (RequiredFee, OwnerOnce)>>;
-
 ///
 #[derive(Debug)]
 pub struct ReissueInputs {
@@ -500,14 +298,6 @@ pub struct ReissueInputs {
     pub outputs: Vec<(Token, DbcIdSource)>,
     /// Any surplus amount after spending the necessary input dbcs.
     pub change: (Token, PublicAddress),
-    // /// Total fees to pay for spending the inputs.
-    // #[cfg(not(feature = "data-network"))]
-    // pub fees_paid: Token,
-    // /// This is the set of input dbc keys, each having a set of
-    // /// Elder names and their respective fee ciphers.
-    // /// Sent together with spends, so that Elders can verify their fee payments.
-    // #[cfg(not(feature = "data-network"))]
-    // pub all_fee_cipher_params: ReissueCiphers,
 }
 
 /// The results of reissuing dbcs.
@@ -519,7 +309,4 @@ pub struct ReissueOutputs {
     pub change: Option<Dbc>,
     /// The dbcs we spent when reissuing.
     pub spent_dbcs: BTreeSet<DbcId>,
-    // /// Total fees to paid for this tx.
-    // #[cfg(not(feature = "data-network"))]
-    // pub fees_paid: Token,
 }
