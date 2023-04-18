ffe9dfe50b7fcec30b5fe6103d033b042b1cb93f
oetyng <oetyng@gmail.com>
2023-04-18 12:45:08 +0200

    chore: doc updates

--- safenode/src/protocol/online_transfers/mod.rs
+++ safenode/src/protocol/online_transfers/mod.rs
@@ -62,18 +62,18 @@ pub struct CreatedDbc {
     pub amount: RevealedAmount,
 }
 
-/// A function for creating an offline transfer of tokens.
+/// A function for creating an online transfer of tokens.
 /// This is done by creating new dbcs to the recipients (and a change dbc if any)
 /// by selecting from the available input dbcs, and creating the necessary
-/// spends to do so.
+/// spends to do so. Additionally, fees for each of those inputs will be queried for
+/// and the nodes to pay the fee to will be included in the outputs.
 ///
-/// Those signed spends are found in each new dbc, and must be uploaded to the network
-/// for the transaction to take effect.
-/// The peers will validate each signed spend they receive, before accepting it.
-/// Once enough peers have accepted all the spends of the transaction, and serve
+/// The resulting signed spends are found in each new dbc, and must be uploaded to
+/// the network for the transaction to take effect.
+/// The nodes will validate each signed spend they receive, before accepting it.
+/// Once enough nodes have accepted all the spends of the transaction, and serve
 /// them upon request, the transaction will be completed.
 ///
-/// (Disabled for now: Transfer fees will be added if not in data-network.)
 /// (Disabled for now: DbcReason, can be added later.)
 #[allow(clippy::result_large_err)]
 pub async fn create_transfer(
@@ -96,9 +96,10 @@ pub(crate) async fn select_inputs(
     change_to: PublicAddress,
     client: &Client,
 ) -> Result<Inputs> {
-    // We'll combine one or more input DBCs and reissue:
-    // - one output DBC per recipient,
-    // - and a single DBC for the change - if any - which will be returned from this function.
+    // We'll combine one or more input dbcs and reissue:
+    // - one output dbc per recipient,
+    // - one output dbc for each node that will be paid a fee,
+    // - and a single dbc for the change - if any - which will be returned from this function.
     let mut dbcs_to_spend = vec![];
     let mut total_input_amount = Token::zero();
     let mut total_output_amount = recipients
@@ -123,7 +124,7 @@ pub(crate) async fn select_inputs(
         let dbc_balance = match dbc.revealed_amount(&derived_key) {
             Ok(revealed_amount) => Token::from_nano(revealed_amount.value()),
             Err(err) => {
-                warn!("Ignoring input Dbc (id: {dbc_id:?}) due to not having correct derived key: {err:?}");
+                warn!("Ignoring input dbc (id: {dbc_id:?}) due to not having correct derived key: {err:?}");
                 continue;
             }
         };
@@ -142,18 +143,18 @@ pub(crate) async fn select_inputs(
             let num_responses = node_fees.len();
             let required_responses = close_group_majority();
             if required_responses > num_responses {
-                warn!("Not enough elders contacted for the section to spend the input. Got: {num_responses}, needed: {required_responses}");
+                warn!("Not enough nodes contacted for the section to spend the input. Got: {num_responses}, needed: {required_responses}");
                 continue;
             }
 
             // Fees that were not encrypted to us.
             let mut invalid_fees = BTreeSet::new();
-            // As the Elders encrypt the amount to our public key, we need to decrypt it.
-            let mut decrypted_elder_fees = vec![];
+            // As the nodes encrypt the amount to the dbc id to be spent, we need to decrypt it.
+            let mut decrypted_node_fees = vec![];
 
             for (node_id, fee) in node_fees {
                 match fee.content.decrypt_amount(&derived_key) {
-                    Ok(amount) => decrypted_elder_fees.push(((node_id, fee), amount)),
+                    Ok(amount) => decrypted_node_fees.push(((node_id, fee), amount)),
                     Err(error) => {
                         error!("Decrypting the fee content from {node_id:?} failed! {error}");
                         let _ = invalid_fees.insert(fee.content.reward_address);
@@ -169,7 +170,7 @@ pub(crate) async fn select_inputs(
             }
 
             // Total fee paid to all recipients in the section for this input.
-            let fee_per_input = decrypted_elder_fees
+            let fee_per_input = decrypted_node_fees
                 .iter()
                 .fold(Some(Token::zero()), |total, (_, fee)| {
                     total.and_then(|t| t.checked_add(*fee))
@@ -182,7 +183,7 @@ pub(crate) async fn select_inputs(
             let mut fee_cipher_params = BTreeMap::new();
 
             // Add nodes to outputs and generate their fee ciphers.
-            decrypted_elder_fees
+            decrypted_node_fees
                 .iter()
                 .for_each(|((node_id, required_fee), fee)| {
                     let dbc_id_src = required_fee
@@ -205,7 +206,7 @@ pub(crate) async fn select_inputs(
         };
         // ---------------- fee part end ----------------
 
-        // Add this Dbc as input to be spent.
+        // Add this dbc as input to be spent.
         dbcs_to_spend.push((dbc, derived_key));
 
         // Input amount increases with the amount of the dbc.
@@ -217,13 +218,13 @@ pub(crate) async fn select_inputs(
             )
             })?;
 
-        #[cfg(not(feature = "data-network"))]
+        // ---------------- fee part start ----------------
         {
             // Output amount now increases a bit, as we have to cover the fee as well..
             total_output_amount = total_output_amount.checked_add(fee_per_input)
             .ok_or_else(|| {
                 Error::DbcReissueFailed(
-                "Overflow occurred while adding mint fee in order to calculate the total amount for the output DBCs."
+                "Overflow occurred while adding node fee in order to calculate the total amount for the output DBCs."
                     .to_string(),
             )
             })?;
@@ -231,13 +232,14 @@ pub(crate) async fn select_inputs(
             change_amount = change_amount.checked_add(fee_per_input)
             .ok_or_else(|| {
                 Error::DbcReissueFailed(
-                "Overflow occurred while adding mint fee in order to calculate the total amount for the output DBCs."
+                "Overflow occurred while adding node fee in order to calculate the total amount for the output DBCs."
                     .to_string(),
             )
             })?;
         }
+        // ---------------- fee part end ----------------
 
-        // If we've already combined input DBCs for the total output amount, then stop.
+        // If we've already combined input dbcs for the total output amount, then stop.
         match change_amount.checked_sub(dbc_balance) {
             Some(pending_output) => {
                 change_amount = pending_output;
@@ -262,7 +264,7 @@ pub(crate) async fn select_inputs(
     })
 }
 
-// Make sure total input amount gathered with input DBCs are enough for the output amount
+// Make sure total input amount gathered with input dbcs are enough for the output amount
 #[allow(clippy::result_large_err)]
 fn verify_amounts(total_input_amount: Token, total_output_amount: Token) -> Result<()> {
     if total_output_amount > total_input_amount {
@@ -319,7 +321,7 @@ fn crate_transfer_with(selected_inputs: Inputs) -> Result<Outputs> {
         .map_err(Error::Dbcs)?;
 
     // Perform validations of input tx and signed spends,
-    // as well as building the output DBCs.
+    // as well as building the output dbcs.
     let mut created_dbcs: Vec<_> = dbc_builder
         .build()
         .map_err(Error::Dbcs)?
@@ -353,7 +355,7 @@ async fn get_fees(dbc_id: DbcId, client: &Client) -> Result<BTreeMap<NodeId, Req
         .await
         .map_err(|e| Error::CouldNotGetFees(e.to_string()))?;
 
-    // We just want to receive at least supermajority of results, we don't care about any errors
+    // We just want to receive at least a majority of results, we don't care about any errors
     // so we log them, but return whatever results we get. If not enough for upper layer, it will error there.
     let results: BTreeMap<_, _> = responses
         .into_iter()
